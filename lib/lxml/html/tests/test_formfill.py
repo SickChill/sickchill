7738a091f23c6e759b35e82e92d9eab5b5cac5d8
echel0n <justin.tabish@gmail.com>
2014-03-28 22:36:59 -0700

    Revert "Added lxml to our libs"
    This reverts commit 32c029c3cf639a48c35daa003caa68731308670e.

--- lib/lxml/html/tests/test_formfill.py
+++
@@ -1,8 +0,0 @@
-import unittest, sys
-from lxml.tests.common_imports import make_doctest
-
-def test_suite():
-    suite = unittest.TestSuite()
-    if sys.version_info >= (2,4):
-        suite.addTests([make_doctest('test_formfill.txt')])
-    return suite
