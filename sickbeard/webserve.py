ac82e32b98fb25804e335515aa578111589fbb5b
Jason Valdron <jason@valdron.ca>
2014-04-22 19:58:43 -0300

    added indexer selectbox and timeout in search

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -1976,6 +1976,8 @@ class NewHomeAddShows:
         # check for indexer preset
         indexers = sickbeard.indexerApi().indexers if not int(indexer) else [int(indexer or 0)]
 
+        print repr(indexers)
+
         # Query Indexers for each search term and build the list of results
         for indexer in indexers:
             results = []
@@ -2132,6 +2134,7 @@ class NewHomeAddShows:
         t.provided_show_dir = show_dir
         t.other_shows = other_shows
         t.provided_indexer = int(indexer or 0)
+        t.indexers = sickbeard.indexerApi().indexers
 
         return _munge(t)
 
@@ -2148,7 +2151,7 @@ class NewHomeAddShows:
     @cherrypy.expose
     def addNewShow(self, whichSeries=None, indexerLang="en", rootDir=None, defaultStatus=None,
                    anyQualities=None, bestQualities=None, flatten_folders=None, subtitles=None,
-                   fullShowPath=None, other_shows=None, skipShow=None):
+                   fullShowPath=None, other_shows=None, skipShow=None, providedIndexer=None):
         """
         Receive tvdb id, dir, and other options and create a show from them. If extra show dirs are
         provided then it forwards back to newShow, if not it goes to /home.
