44d45ca760acf9305db15ad82e62f0d7823eaa53
Nils Vogels <bacardicoke@gmail.com>
2014-06-02 01:39:55 +0200

    Fixing UnboundLocalError when attempting to process nonexisting dir

--- sickbeard/processTV.py
+++ sickbeard/processTV.py
@@ -397,6 +397,7 @@ def delete_dir(processPath):
 
 
 def get_path_dir_files(dirName, nzbName, type):
+    path = ""
 
     if dirName == sickbeard.TV_DOWNLOAD_DIR and not nzbName or type == "manual":  #Scheduled Post Processing Active
         #Get at first all the subdir in the dirName
