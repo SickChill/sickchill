6c3f34fd8dba409c195b26a666a0a7f30c6870bc
echel0n <justin.tabish@gmail.com>
2014-06-24 17:37:03 -0700

    Possible fix for EpisodeNotFoundByAbsoluteNumberException errors

--- sickbeard/helpers.py
+++ sickbeard/helpers.py
@@ -702,7 +702,7 @@ def get_absolute_number_from_season_and_episode(show, season, episode):
 
 def get_all_episodes_from_absolute_number(show, indexer_id, absolute_numbers):
     if len(absolute_numbers) == 0:
-        raise EpisodeNotFoundByAbsoluteNumberException()
+        raise EpisodeNotFoundByAbsoluteNumberException
 
     episodes = []
     season = None
@@ -718,7 +718,7 @@ def get_all_episodes_from_absolute_number(show, indexer_id, absolute_numbers):
         if ep:
             episodes.append(ep.episode)
         else:
-            raise EpisodeNotFoundByAbsoluteNumberException()
+            raise EpisodeNotFoundByAbsoluteNumberException
         season = ep.season  # this will always take the last found seson so eps that cross the season border are not handeled well
 
     return (season, episodes)
