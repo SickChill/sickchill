bea3a13c2ba3b473fd685f9ede49ca2016c70ea7
echel0n <justin.tabish@gmail.com>
2014-07-08 20:39:31 -0700

    Switched update url for git repo to use non-ssl url.

--- sickbeard/versionChecker.py
+++ sickbeard/versionChecker.py
@@ -636,7 +636,7 @@ class SourceUpdateManager(UpdateManager):
         """
         Downloads the latest source tarball from github and installs it over the existing version.
         """
-        base_url = 'https://github.com/' + self.github_repo_user + '/' + self.github_repo
+        base_url = 'http://github.com/' + self.github_repo_user + '/' + self.github_repo
         tar_download_url = base_url + '/tarball/' + self.branch
         version_path = ek.ek(os.path.join, sickbeard.PROG_DIR, u'version.txt')
 
