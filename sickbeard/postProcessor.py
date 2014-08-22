27189d0406ba768126f1d618e778b62fca19c264
echel0n <sickrage.tv@gmail.com>
2014-08-21 23:18:35 -0700

    Fix for files being deleted from show folder during post-processing runs

--- sickbeard/postProcessor.py
+++ sickbeard/postProcessor.py
@@ -224,6 +224,7 @@ class PostProcessor(object):
                         self._log(u'Cannot change permissions of ' + cur_file, logger.WARNING)
 
                 ek.ek(os.remove, cur_file)
+
                 # do the library update for synoindex
                 notifiers.synoindex_notifier.deleteFile(cur_file)
 
@@ -855,6 +856,18 @@ class PostProcessor(object):
                 u"This download is marked a priority download so I'm going to replace an existing file if I find one",
                 logger.DEBUG)
 
+        # delete the existing file (and company)
+        for cur_ep in [ep_obj] + ep_obj.relatedEps:
+            try:
+                self._delete(cur_ep.location, associated_files=True)
+
+                # clean up any left over folders
+                if cur_ep.location:
+                    helpers.delete_empty_folders(ek.ek(os.path.dirname, cur_ep.location),
+                                                 keep_dir=ep_obj.show._location)
+            except (OSError, IOError):
+                raise exceptions.PostProcessingFailed("Unable to delete the existing files")
+
             # set the status of the episodes
             # for curEp in [ep_obj] + ep_obj.relatedEps:
             #    curEp.status = common.Quality.compositeStatus(common.SNATCHED, new_ep_quality)
@@ -965,17 +978,6 @@ class PostProcessor(object):
                 raise exceptions.PostProcessingFailed("Unable to move the files to their new home")
         except (OSError, IOError):
             raise exceptions.PostProcessingFailed("Unable to move the files to their new home")
-
-        # delete the existing file (and company)
-        for cur_ep in [ep_obj] + ep_obj.relatedEps:
-            try:
-                self._delete(cur_ep.location, associated_files=True)
-                # clean up any left over folders
-                if cur_ep.location:
-                    helpers.delete_empty_folders(ek.ek(os.path.dirname, cur_ep.location),
-                                                 keep_dir=ep_obj.show._location)
-            except (OSError, IOError):
-                raise exceptions.PostProcessingFailed("Unable to delete the existing files")
                 
         # download subtitles
         if sickbeard.USE_SUBTITLES and ep_obj.show.subtitles:
