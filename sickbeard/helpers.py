0822d15d36ac20ea9614aade9fc37b1ff533f779
Adam <adam111316@users.noreply.github.com>
2014-10-02 16:37:08 +0800

    Add sizing field on show page

--- sickbeard/helpers.py
+++ sickbeard/helpers.py
@@ -1379,3 +1379,39 @@ def clearCache(force=False):
                                 logger.log(u"Unable to clean " + cache_root + ": " + repr(e) + " / " + str(e),
                                            logger.WARNING)
                                 break
+
+def human(size):
+    """
+    format a size in bytes into a 'human' file size, e.g. bytes, KB, MB, GB, TB, PB
+    Note that bytes/KB will be reported in whole numbers but MB and above will have greater precision
+    e.g. 1 byte, 43 bytes, 443 KB, 4.3 MB, 4.43 GB, etc
+    """
+    if size == 1:
+        # because I really hate unnecessary plurals
+        return "1 byte"
+
+    suffixes_table = [('bytes', 0), ('KB', 0), ('MB', 1), ('GB', 2),('TB', 2), ('PB', 2)]
+
+    num = float(size)
+    for suffix, precision in suffixes_table:
+        if num < 1024.0:
+            break
+        num /= 1024.0
+
+    if precision == 0:
+        formatted_size = "%d" % num
+    else:
+        formatted_size = str(round(num, ndigits=precision))
+
+    return "%s %s" % (formatted_size, suffix)
+
+
+def get_size(start_path='.'):
+
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
