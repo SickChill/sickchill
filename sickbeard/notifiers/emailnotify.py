0030c87d044d45dfd36ca7e04249c64429388629
luxmoggy <luxmoggy@internet.lu>
2014-12-15 12:49:09 +0100

    Catch error when speical charactor crashes email send

--- sickbeard/notifiers/emailnotify.py
+++ sickbeard/notifiers/emailnotify.py
@@ -29,7 +29,7 @@ import sickbeard
 
 from sickbeard import logger, common
 from sickbeard import db
-from sickbeard.encodingKludge import toUnicode
+from sickbeard import encodingKludge as ek
 from sickbeard.exceptions import ex
 
 
@@ -51,7 +51,7 @@ class EmailNotifier:
         ep_name: The name of the episode that was snatched
         title: The title of the notification (optional)
         """
-        ep_name = toUnicode(ep_name)
+        ep_name = ek.ss(ep_name)
 
         if sickbeard.EMAIL_NOTIFY_ONSNATCH:
             show = self._parseEp(ep_name)
@@ -89,7 +89,7 @@ class EmailNotifier:
         ep_name: The name of the episode that was downloaded
         title: The title of the notification (optional)
         """
-        ep_name = toUnicode(ep_name)
+        ep_name = ek.ss(ep_name)
 
         if sickbeard.EMAIL_NOTIFY_ONDOWNLOAD:
             show = self._parseEp(ep_name)
@@ -127,7 +127,7 @@ class EmailNotifier:
         ep_name: The name of the episode that was downloaded
         lang: Subtitle language wanted
         """
-        ep_name = toUnicode(ep_name)
+        ep_name = ek.ss(ep_name)
 
         if sickbeard.EMAIL_NOTIFY_ONSUBTITLEDOWNLOAD:
             show = self._parseEp(ep_name)
@@ -207,7 +207,7 @@ class EmailNotifier:
             return False
 
     def _parseEp(self, ep_name):
-        ep_name = toUnicode(ep_name)
+        ep_name = ek.ss(ep_name)
 
         sep = " - "
         titles = ep_name.split(sep)
