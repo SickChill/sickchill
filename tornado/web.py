5e1298f801648204fa62456e86d02bf53f61d1c5
echel0n <sickrage.tv@gmail.com>
2014-12-18 16:54:39 -0800

    Fixed sickragetv/sickrage-issues#113

--- tornado/web.py
+++ tornado/web.py
@@ -2017,8 +2017,6 @@ class RedirectHandler(RequestHandler):
             (r"/oldpath", web.RedirectHandler, {"url": "/newpath"}),
         ])
     """
-    def __init__(self, *args, **kwargs):
-        super(RedirectHandler, self).__init__(*args, **kwargs)
 
     def initialize(self, url, permanent=True):
         self._url = url
