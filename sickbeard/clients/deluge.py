SickChill/sickchill

commit 646a3092813aa2bd22bbe908951a81a5b9ec7ebe

Fix deluge move completed default value

sickbeard/clients/deluge.py
@@ -97,8 +97,7 @@ class DelugeAPI(GenericClient):
     def _add_torrent_uri(self, result):
 
         post_data = json.dumps({"method": "core.add_torrent_magnet",
-                                "params": [result.url, {"move_completed": "true",
-                                                        "move_completed_path": sickbeard.TV_DOWNLOAD_DIR}],
+                                "params": [result.url],
                                 "id": 2
         })
         self._request(method='post', data=post_data)
@@ -110,9 +109,7 @@ class DelugeAPI(GenericClient):
     def _add_torrent_file(self, result):
 
         post_data = json.dumps({"method": "core.add_torrent_file",
-                                "params": [result.name + '.torrent', b64encode(result.content),
-                                           {"move_completed": "true",
-                                            "move_completed_path": sickbeard.TV_DOWNLOAD_DIR}],
+                                "params": [result.name + '.torrent', b64encode(result.content)],
                                 "id": 2
         })
         self._request(method='post', data=post_data)

commit 25421b0c3f1ee736749eea8de9c231725d7ab57f

Fix deluge client not working Bug introduced in https://github.com/SiCKRAGETV/SickRage/commit/646a3092813aa2bd22bbe908951a81a5b9ec7ebe

sickbeard/clients/deluge.py
@@ -97,7 +97,7 @@ class DelugeAPI(GenericClient):
     def _add_torrent_uri(self, result):
 
         post_data = json.dumps({"method": "core.add_torrent_magnet",
-                                "params": [result.url],
+                                "params": [result.url, {}],
                                 "id": 2
         })
         self._request(method='post', data=post_data)
@@ -109,7 +109,7 @@ class DelugeAPI(GenericClient):
     def _add_torrent_file(self, result):
 
         post_data = json.dumps({"method": "core.add_torrent_file",
-                                "params": [result.name + '.torrent', b64encode(result.content)],
+                                "params": [result.name + '.torrent', b64encode(result.content), {}],
                                 "id": 2
         })
         self._request(method='post', data=post_data)