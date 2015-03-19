62ae51a8f73f7fa8cb304e92b9567a0cde3a58a9
Anthony Ryan <anthonyryan1@gmail.com>
2015-03-19 14:35:55 -0400

    Add workaround for improperly cleaned scgi over unix domain socket URI

--- sickbeard/config.py
+++ sickbeard/config.py
@@ -28,6 +28,11 @@ from sickbeard import logger
 from sickbeard import naming
 from sickbeard import db
 
+# Address poor support for scgi over unix domain sockets
+# this is not nicely handled by python currently
+# http://bugs.python.org/issue23636
+urlparse.uses_netloc.append('scgi')
+
 naming_ep_type = ("%(seasonnumber)dx%(episodenumber)02d",
                   "s%(seasonnumber)02de%(episodenumber)02d",
                   "S%(seasonnumber)02dE%(episodenumber)02d",
