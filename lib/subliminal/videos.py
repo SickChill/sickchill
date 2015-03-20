9e6ba950a97611bb8f5bd1033c627d3b7f696a13
Alexandre Beloin <alexandre.beloin@gmail.com>
2015-03-20 17:43:06 -0400

    Fix subliminal not working properly on Windows.

--- lib/subliminal/videos.py
+++ lib/subliminal/videos.py
@@ -59,8 +59,12 @@ class Video(object):
         self._path = None
         self.hashes = {}
 
-        if isinstance(path, unicode):
-            path = path.encode('utf-8')
+        if sys.platform == 'win32':
+            if isinstance(path, str):
+                path = unicode(path.encode('utf-8'))
+        else:
+            if isinstance(path, unicode):
+                path = path.encode('utf-8')
 
         self.release = path
 
@@ -228,8 +232,12 @@ def scan(entry, max_depth=3, scan_filter=None, depth=0):
 
     """
 
-    if isinstance(entry, unicode):
-        entry = entry.encode('utf-8')
+    if sys.platform == 'win32':
+        if isinstance(entry, str):
+            entry = unicode(entry.encode('utf-8'))
+    else:
+        if isinstance(entry, unicode):
+            entry = entry.encode('utf-8')
 
     if depth > max_depth and max_depth != 0:  # we do not want to search the whole file system except if max_depth = 0
         return []
