a36912b1a2440efa4ce97c74150291903bf55e79
Alexandre Beloin <alexandre.beloin@gmail.com>
2015-04-13 08:56:58 -0400

    iptorrent: Add missing import

--- sickbeard/providers/iptorrents.py
+++ sickbeard/providers/iptorrents.py
@@ -24,6 +24,7 @@ import itertools
 
 import sickbeard
 import generic
+from sickbeard.common import MULTI_EP_RESULT, SEASON_RESULT
 from sickbeard.common import Quality
 from sickbeard import logger
 from sickbeard import tvcache
