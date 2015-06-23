915c8db3a6b9f3446e9734ef4acc132671361730
Dustyn Gibson <dustyn.gibson@gmail.com>
2015-06-23 01:58:28 -0700

    Change USER_AGENT back to SR now that problems are fixed. Hopefully we stop being blocked. Set user agent in feedcache/feedparser Better logging in generic provider Fix typo in scene_numbering ( Thanks @thezoggy ) Fix some paths in tests, and fix some status'/db structures in tests. Also commit to db between tests.

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -43,7 +43,7 @@ shutil.copyfile = lib.shutil_custom.copyfile_custom
 #=================
 # test globals
 #=================
-TESTDIR = os.path.abspath('.')
+TESTDIR = os.path.abspath(os.path.dirname(__file__))
 TESTDBNAME = "sickbeard.db"
 TESTCACHEDBNAME = "cache.db"
 TESTFAILEDDBNAME = "failed.db"
@@ -88,8 +88,8 @@ sickbeard.PROVIDER_ORDER = ["sick_beard_index"]
 sickbeard.newznabProviderList = providers.getNewznabProviderList("'Sick Beard Index|http://lolo.sickbeard.com/|0|5030,5040|0|eponly|0|0|0!!!NZBs.org|https://nzbs.org/||5030,5040,5060,5070,5090|0|eponly|0|0|0!!!Usenet-Crawler|https://www.usenet-crawler.com/||5030,5040,5060|0|eponly|0|0|0'")
 sickbeard.providerList = providers.makeProviderList()
 
-sickbeard.PROG_DIR = os.path.abspath('..')
-sickbeard.DATA_DIR = sickbeard.PROG_DIR
+sickbeard.PROG_DIR = os.path.abspath(os.path.join(TESTDIR, '..'))
+sickbeard.DATA_DIR = TESTDIR
 sickbeard.CONFIG_FILE = os.path.join(sickbeard.DATA_DIR, "config.ini")
 sickbeard.CFG = ConfigObj(sickbeard.CONFIG_FILE)
 
@@ -140,34 +140,36 @@ class SickbeardTestDBCase(unittest.TestCase):
         tearDown_test_episode_file()
         tearDown_test_show_dir()
 
-
 class TestDBConnection(db.DBConnection, object):
 
     def __init__(self, dbFileName=TESTDBNAME):
         dbFileName = os.path.join(TESTDIR, dbFileName)
         super(TestDBConnection, self).__init__(dbFileName)
 
-
 class TestCacheDBConnection(TestDBConnection, object):
-
-    def __init__(self, providerName):
+     def __init__(self, providerName):
         db.DBConnection.__init__(self, os.path.join(TESTDIR, TESTCACHEDBNAME))
 
         # Create the table if it's not already there
         try:
-            sql = "CREATE TABLE " + providerName + " (name TEXT, season NUMERIC, episodes TEXT, indexerid NUMERIC, url TEXT, time NUMERIC, quality TEXT);"
-            self.connection.execute(sql)
-            self.connection.commit()
-        except sqlite3.OperationalError, e:
-            if str(e) != "table " + providerName + " already exists":
+            if not self.hasTable(providerName):
+                sql = "CREATE TABLE [" + providerName + "] (name TEXT, season NUMERIC, episodes TEXT, indexerid NUMERIC, url TEXT, time NUMERIC, quality TEXT, release_group TEXT)"
+                self.connection.execute(sql)
+                self.connection.commit()
+        except Exception, e:
+            if str(e) != "table [" + providerName + "] already exists":
                 raise
+
+            # add version column to table if missing
+            if not self.hasColumn(providerName, 'version'):
+                self.addColumn(providerName, 'version', "NUMERIC", "-1")
 
         # Create the table if it's not already there
         try:
             sql = "CREATE TABLE lastUpdate (provider TEXT, time NUMERIC);"
             self.connection.execute(sql)
             self.connection.commit()
-        except sqlite3.OperationalError, e:
+        except Exception, e:
             if str(e) != "table lastUpdate already exists":
                 raise
 
@@ -195,31 +197,29 @@ def setUp_test_db():
 
 
 def tearDown_test_db():
-    """Deletes the test db
-        although this seams not to work on my system it leaves me with an zero kb file
-    """
+    from sickbeard.db import db_cons
+    for connection in db_cons:
+        db_cons[connection].commit()
+#        db_cons[connection].close()
 
-    #uncomment next line so leave the db intact between test and at the end
-    #return False
-
-    for current_db in [ TESTDBNAME, TESTCACHEDBNAME, TESTFAILEDDBNAME ]:
-        for file_name in [ os.path.join(TESTDIR, current_db), os.path.join(TESTDIR, current_db + '-journal') ]:
-            if os.path.exists(file_name):
-                try:
-                    os.remove(file_name)
-                except (IOError, OSError) as e:
-                    print 'ERROR: Failed to remove ' + file_name
-                    print ex(e)
-
+#    for current_db in [ TESTDBNAME, TESTCACHEDBNAME, TESTFAILEDDBNAME ]:
+#        file_name = os.path.join(TESTDIR, current_db)
+#        if os.path.exists(file_name):
+#            try:
+#                os.remove(file_name)
+#            except Exception as e:
+#                print 'ERROR: Failed to remove ' + file_name
+#                print ex(e)
 
 def setUp_test_episode_file():
     if not os.path.exists(FILEDIR):
         os.makedirs(FILEDIR)
 
     try:
-        with open(FILEPATH, 'w') as f:
+        with open(FILEPATH, 'wb') as f:
             f.write("foo bar")
-    except EnvironmentError:
+            f.flush()
+    except Exception:
         print "Unable to set up test episode"
         raise
 
