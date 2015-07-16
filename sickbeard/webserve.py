18683b526c71fe136e3afaa4ff2812742d40775a
Fernando <fernandog@users.noreply.github.com>
2015-07-16 12:39:09 -0300

    Add link to NEWS file

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -2153,7 +2153,7 @@ class HomeNews(Home):
     def index(self):
         t = PageTemplate(rh=self, file="news.tmpl")
         t.submenu = self.HomeMenu()
-        response = requests.get("https://raw.githubusercontent.com/SiCKRAGETV/SickRage/master/CHANGES.md", verify=False)
+        response = requests.get("https://raw.githubusercontent.com/SiCKRAGETV/SickRage/add-news/news.md", verify=False)
         import markdown2
         t.newsdata = markdown2.markdown(response.text)
         return t.respond()
