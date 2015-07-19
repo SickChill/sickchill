bb196f36019baa9b923e9cc5ed170b84c94db762
Dustyn Gibson <miigotu@gmail.com>
2015-07-19 05:41:43 -0700

    Fix notification when searchin subtitles through web api

--- sickbeard/webapi.py
+++ sickbeard/webapi.py
@@ -1100,11 +1100,10 @@ class CMD_SubtitleSearch(ApiCall):
             return _responds(RESULT_FAILURE, msg='Unable to find subtitles')
 
         # return the correct json value
-        if previous_subtitles != epObj.subtitles:
-            status = 'New subtitles downloaded: %s' % ' '.join([
-                "<img src='" + sickbeard.WEB_ROOT + "/images/flags/" + babelfish.language.Language(
-                    x).alpha2 + ".png' alt='" + babelfish.language.Language(x).name + "'/>" for x in
-                sorted(list(set(epObj.subtitles).difference(previous_subtitles)))])
+        newSubtitles = frozenset(ep_obj.subtitles).difference(previous_subtitles)
+        if newSubtitles:
+            newLangs = [subtitles.fromietf(newSub) for newSub in newSubtitles]
+            status = 'New subtitles downloaded: %s' % ', '.join([newLang.name for newLang in newLangs])
             response = _responds(RESULT_SUCCESS, msg='New subtitles found')
         else:
             status = 'No subtitles downloaded'
