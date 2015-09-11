52534f9b577d2099191683363675c74868f134c6
Fernando <fernandog@users.noreply.github.com>
2015-09-11 00:23:09 -0300

    Add bs3

--- sickbeard/providers/hdtorrents.py
+++ sickbeard/providers/hdtorrents.py
@@ -30,7 +30,7 @@ from sickbeard import helpers
 from sickbeard import show_name_helpers
 from sickbeard.exceptions import ex, AuthException
 import requests
-from bs4 import BeautifulSoup as soup
+from lib.BeautifulSoup import BeautifulSoup as soup
 from unidecode import unidecode
 from sickbeard.helpers import sanitizeSceneName
 from datetime import datetime
