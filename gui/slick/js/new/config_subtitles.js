7f380086aafb119ccdc40d99a18ca8c4d30391cd
Alexis Tyler <xo@wvvw.me>
2015-09-27 22:03:52 +0930

    Fixes config_subtitles

--- gui/slick/js/new/config_subtitles.js
+++ gui/slick/js/new/config_subtitles.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-    $("#subtitles_languages").tokenInput(
-            [${",\r\n".join("{id: \"" + lang.opensubtitles + "\", name: \"" + lang.name + "\"}" for lang in subtitles.subtitleLanguageFilter())}],
-            {
-                method: "POST",
-                hintText: "Write to search a language and select it",
-                preventDuplicates: true,
-                prePopulate: [${",\r\n".join("{id: \"" + subtitles.fromietf(lang).opensubtitles + "\", name: \"" + subtitles.fromietf(lang).name + "\"}" for lang in subtitles.wantedLanguages()) if subtitles.wantedLanguages() else ''}]
-            }
-        );
+    $("#subtitles_languages").tokenInput([$('meta[data-var="subtitles.subtitleLanguageFilter"]').data('content')], {
+        method: "POST",
+        hintText: "Write to search a language and select it",
+        preventDuplicates: true,
+        prePopulate: [$('meta[data-var="prePopulate"]').data('content')]
+    });
 });
 $('#config-components').tabs();
 $('#subtitles_dir').fileBrowser({ title: 'Select Subtitles Download Directory' });
