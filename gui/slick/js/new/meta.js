7164ace275fb673d525a3cf2402e223f5d5e06d1
Alexis Tyler <xo@wvvw.me>
2015-10-04 12:13:07 +1030

    Added warning to meta.js

--- gui/slick/js/new/meta.js
+++ gui/slick/js/new/meta.js
@@ -1,7 +1,12 @@
 function metaToBool(pyVar){
     var meta = $('meta[data-var="' + pyVar + '"]').data('content');
-    meta = (isNaN(meta) ? meta.toLowerCase() : meta.toString());
-    return !(meta === 'false' || meta === 'none' || meta === '0');
+    if(meta === undefined){
+        console.log(pyVar + ' is empty, did you forget to add this to main.mako?');
+        return meta;
+    } else {
+        meta = (isNaN(meta) ? meta.toLowerCase() : meta.toString());
+        return !(meta === 'false' || meta === 'none' || meta === '0');
+    }
 }
 
 function getMeta(pyVar){
