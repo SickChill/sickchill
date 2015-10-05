aeee484c8f5bd9e49637ab67f88645314ac13d4b
Dustyn Gibson <miigotu@gmail.com>
2015-10-05 10:11:09 -0700

    Clear cache/sessions and cache/indexers on restore

--- sickbeard/__init__.py
+++ sickbeard/__init__.py
@@ -728,14 +728,19 @@ def initialize(consoleLogging=True):
 
                 restoreCache(os.path.join(restoreDir, 'cache'), CACHE_DIR)
         except Exception as e:
-            logger.log(u"Restore: restoring cache failed: {0}".format(str(e)), logger.ERROR)
+            logger.log(u"Restore: restoring cache failed: {0}".format(ex(e)), logger.ERROR)
         finally:
             if os.path.exists(os.path.join(DATA_DIR, 'restore')):
                 try:
                     shutil.rmtree(os.path.join(DATA_DIR, 'restore'))
-                    shutil.rmtree(os.path.join(CACHE_DIR, 'mako'))
                 except Exception as e:
-                    logger.log(u"Restore: Unable to remove the restore directory: {0}".format(str(e)), logger.ERROR)
+                    logger.log(u"Restore: Unable to remove the restore directory: {0}".format(ex(e)), logger.ERROR)
+
+                for cleanupDir in ['mako', 'sessions', 'indexers']:
+                    try:
+                        shutil.rmtree(os.path.join(CACHE_DIR, cleanupDir))
+                    except Exception as e:
+                        logger.log(u"Restore: Unable to remove the cache/{0} directory: {1}".format(cleanupDir, ex(e)), logger.WARNING)
 
 
         GUI_NAME = check_setting_str(CFG, 'GUI', 'gui_name', 'slick')
