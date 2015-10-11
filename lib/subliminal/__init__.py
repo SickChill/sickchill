24d891e91cbac8038fc157cf38128617eb0cf659
dvizzacc <dariovizz@hotmail.it>
2015-10-11 18:16:53 +0200

    Updated subliminal to 1.1.0.dev0, added no_setup patch, added napiprojekt subtitles provider, fixed #3251

--- lib/subliminal/__init__.py
+++ lib/subliminal/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = 'subliminal'
-__version__ = '1.0.dev0'
+__version__ = '1.1.0.dev0'
 __author__ = 'Antoine Bertin'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015, Antoine Bertin'
@@ -12,7 +12,7 @@ from .api import (ProviderPool, check_video, provider_manager, download_best_sub
 from .cache import region
 from .exceptions import Error, ProviderError
 from .providers import Provider
-from .subtitle import Subtitle
+from .subtitle import Subtitle, compute_score
 from .video import SUBTITLE_EXTENSIONS, VIDEO_EXTENSIONS, Episode, Movie, Video, scan_video, scan_videos
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
