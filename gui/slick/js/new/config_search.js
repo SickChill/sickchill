442e9c72ab5c58e1da0bd1ea4a59eb78207febcd
Alison Winters <alisonatwork@gmail.com>
2015-10-31 19:29:52 +0100

    remove unnecessary init causing error on search settings screeen

--- gui/slick/js/new/config_search.js
+++ gui/slick/js/new/config_search.js
@@ -2,4 +2,3 @@ $('#config-components').tabs();
 $('#nzb_dir').fileBrowser({ title: 'Select .nzb black hole/watch location' });
 $('#torrent_dir').fileBrowser({ title: 'Select .torrent black hole/watch location' });
 $('#torrent_path').fileBrowser({ title: 'Select .torrent download location' });
-$('#tv_download_dir').fileBrowser({ title: 'Select TV download location' });
