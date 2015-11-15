e8e551b42045c580b2b80539b49b5b87b6bb8e81
labrys <labrys@useres.noreply.github.com>
2015-11-15 18:47:35 -0500

    PEP 8: Fix multiple imports on one line

--- sickbeard/notifiers/boxcar.py
+++ sickbeard/notifiers/boxcar.py
@@ -17,7 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
 
-import urllib, urllib2
+import urllib
+import urllib2
 import time
 
 import sickbeard
