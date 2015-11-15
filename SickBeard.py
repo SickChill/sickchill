226dea57fb503704c1d59f4e719b080554072328
Dustyn Gibson <miigotu@gmail.com>
2015-11-14 21:14:09 -0800

    Temporarily re-add the sys reload hack until remaining known encoding issues are fixed correctly, so that we can merge to master

--- SickBeard.py
+++ SickBeard.py
@@ -155,6 +155,18 @@ class SickRage(object):
             (sys.platform.startswith('win') and sys.getwindowsversion()[0] >= 6 and getattr(sys.stdout, 'device', sys.stdout).encoding.lower() in ('cp65001', 'charmap')):
             sickbeard.SYS_ENCODING = 'UTF-8'
 
+        # TODO: Continue working on making this unnecessary, this hack creates all sorts of hellish problems
+        if not hasattr(sys, "setdefaultencoding"):
+            reload(sys)
+
+        try:
+            # pylint: disable=E1101
+            # On non-unicode builds this will raise an AttributeError, if encoding type is not valid it throws a LookupError
+            sys.setdefaultencoding(sickbeard.SYS_ENCODING)
+        except Exception:
+            sys.exit("Sorry, you MUST add the SickRage folder to the PYTHONPATH environment variable\n" +
+                     "or find another way to force Python to use " + sickbeard.SYS_ENCODING + " for string encoding.")
+
         # Need console logging for SickBeard.py and SickBeard-console.exe
         self.consoleLogging = (not hasattr(sys, "frozen")) or (sickbeard.MY_NAME.lower().find('-console') > 0)
 
