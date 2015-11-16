7eccdc344b20695e2d27162be01ccc8cbad2af5f
labrys <labrys@useres.noreply.github.com>
2015-11-15 20:11:12 -0500

    PEP 8: Fix white space, blank lines, and indentation

--- sickbeard/notifiers/prowl.py
+++ sickbeard/notifiers/prowl.py
@@ -53,10 +53,10 @@ class ProwlNotifier(object):
             self._sendProwl(prowl_api=None, prowl_priority=None,
                             event=common.notifyStrings[common.NOTIFY_SUBTITLE_DOWNLOAD], message=ep_name + ": " + lang)
                             
-    def notify_git_update(self, new_version = "??"):
+    def notify_git_update(self, new_version="??"):
         if sickbeard.USE_PROWL:
-            update_text=common.notifyStrings[common.NOTIFY_GIT_UPDATE_TEXT]
-            title=common.notifyStrings[common.NOTIFY_GIT_UPDATE]
+            update_text = common.notifyStrings[common.NOTIFY_GIT_UPDATE_TEXT]
+            title = common.notifyStrings[common.NOTIFY_GIT_UPDATE]
             self._sendProwl(prowl_api=None, prowl_priority=None,
                             event=title, message=update_text + new_version)
 
