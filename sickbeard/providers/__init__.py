839d1fc8297b9947dad3c56f363ee2fb19ff7291
labrys <labrys@useres.noreply.github.com>
2015-11-15 20:56:26 -0500

    PEP 8: Move imports to top of file
    PEP 263: Add encoding declaration

--- sickbeard/providers/__init__.py
+++ sickbeard/providers/__init__.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 # Author: Nic Wolfe <nic@wolfeden.ca>
 # URL: http://code.google.com/p/sickbeard/
 #
@@ -16,6 +18,11 @@
 # You should have received a copy of the GNU General Public License
 # along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
 
+from os import sys
+from random import shuffle
+
+import sickbeard
+from sickbeard import logger
 from sickbeard.providers import btn, newznab, womble, thepiratebay, torrentleech, kat, iptorrents, torrentz, \
     omgwtfnzbs, scc, hdtorrents, torrentday, hdbits, hounddawgs, nextgen, speedcd, nyaatorrents, animenzb, bluetigers, cpasbien, fnt, xthor, torrentbytes, \
     freshontv, titansoftv, libertalia, morethantv, bitsoup, t411, tokyotoshokan, shazbat, rarbg, alpharatio, tntvillage, binsearch, torrentproject, extratorrent, \
@@ -67,13 +74,8 @@ __all__ = ['womble',
            'gftracker',
            'hdspace',
            'newpct'
-]
+           ]
 
-import sickbeard
-
-from sickbeard import logger
-from os import sys
-from random import shuffle
 
 def sortedProviderList(randomize=False):
     initialList = sickbeard.providerList + sickbeard.newznabProviderList + sickbeard.torrentRssProviderList
@@ -232,6 +234,7 @@ def getDefaultNewznabProviders():
            'NZBGeek|https://api.nzbgeek.info/||5030,5040|0|eponly|0|0|0!!!' + \
            'NZBs.org|https://nzbs.org/||5030,5040|0|eponly|0|0|0!!!' + \
            'Usenet-Crawler|https://www.usenet-crawler.com/||5030,5040|0|eponly|0|0|0'
+
 
 def getProviderModule(name):
     name = name.lower()
