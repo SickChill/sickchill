66c04bccd593bf4bf5ab7a8b52643d6f84e19742
labrys <labrys@useres.noreply.github.com>
2015-11-15 20:13:41 -0500

    PEP 8: Convert None comparisons from operators to 'is / is not None'

--- sickbeard/notifiers/pushalot.py
+++ sickbeard/notifiers/pushalot.py
@@ -62,7 +62,7 @@ class PushalotNotifier(object):
         if not sickbeard.USE_PUSHALOT and not force:
             return False
 
-        if pushalot_authorizationtoken == None:
+        if pushalot_authorizationtoken is None:
             pushalot_authorizationtoken = sickbeard.PUSHALOT_AUTHORIZATIONTOKEN
 
         logger.log(u"Pushalot event: " + event, logger.DEBUG)
