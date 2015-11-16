890a9e4230100c1c4fa1c678023289de9efe371d
labrys <labrys@useres.noreply.github.com>
2015-11-15 20:14:34 -0500

    PEP 8: Fix white space, blank lines, and indentation

--- sickbeard/notifiers/pushalot.py
+++ sickbeard/notifiers/pushalot.py
@@ -49,10 +49,10 @@ class PushalotNotifier(object):
                                event=common.notifyStrings[common.NOTIFY_SUBTITLE_DOWNLOAD],
                                message=ep_name + ": " + lang)
                                
-    def notify_git_update(self, new_version = "??"):
+    def notify_git_update(self, new_version="??"):
         if sickbeard.USE_PUSHALOT:
-            update_text=common.notifyStrings[common.NOTIFY_GIT_UPDATE_TEXT]
-            title=common.notifyStrings[common.NOTIFY_GIT_UPDATE]
+            update_text = common.notifyStrings[common.NOTIFY_GIT_UPDATE_TEXT]
+            title = common.notifyStrings[common.NOTIFY_GIT_UPDATE]
             self._sendPushalot(pushalot_authorizationtoken=None,
                                event=title, 
                                message=update_text + new_version)
