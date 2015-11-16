5053ddea669b0827b2a704ae45bb3bda3d1cfec0
labrys <labrys@useres.noreply.github.com>
2015-11-15 20:23:15 -0500

    Convert to new style class with explicit object subclassing

--- sickbeard/notifiers/synoindex.py
+++ sickbeard/notifiers/synoindex.py
@@ -26,7 +26,7 @@ from sickrage.helper.encoding import ek
 from sickrage.helper.exceptions import ex
 
 
-class synoIndexNotifier:
+class synoIndexNotifier(object):
     def notify_snatch(self, ep_name):
         pass
 
