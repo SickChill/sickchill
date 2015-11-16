e1a52242aa8e2e87f1e3948a53b25e623ef46ff1
labrys <labrys@useres.noreply.github.com>
2015-11-15 20:44:50 -0500

    Convert to new style class with explicit object subclassing

--- sickbeard/notifiers/tweet.py
+++ sickbeard/notifiers/tweet.py
@@ -31,7 +31,7 @@ import oauth2 as oauth
 import pythontwitter as twitter
 
 
-class TwitterNotifier:
+class TwitterNotifier(object):
     consumer_key = "vHHtcB6WzpWDG6KYlBMr8g"
     consumer_secret = "zMqq5CB3f8cWKiRO2KzWPTlBanYmV0VYxSXZ0Pxds0E"
 
@@ -52,10 +52,10 @@ class TwitterNotifier:
         if sickbeard.TWITTER_NOTIFY_ONSUBTITLEDOWNLOAD:
             self._notifyTwitter(common.notifyStrings[common.NOTIFY_SUBTITLE_DOWNLOAD] + ' ' + ep_name + ": " + lang)
 
-    def notify_git_update(self, new_version = "??"):
+    def notify_git_update(self, new_version="??"):
         if sickbeard.USE_TWITTER:
-            update_text=common.notifyStrings[common.NOTIFY_GIT_UPDATE_TEXT]
-            title=common.notifyStrings[common.NOTIFY_GIT_UPDATE]
+            update_text = common.notifyStrings[common.NOTIFY_GIT_UPDATE_TEXT]
+            title = common.notifyStrings[common.NOTIFY_GIT_UPDATE]
             self._notifyTwitter(title + " - " + update_text + new_version)
 
     def test_notify(self):
@@ -114,7 +114,6 @@ class TwitterNotifier:
             sickbeard.TWITTER_USERNAME = access_token['oauth_token']
             sickbeard.TWITTER_PASSWORD = access_token['oauth_token_secret']
             return True
-
 
     def _send_tweet(self, message=None):
 
