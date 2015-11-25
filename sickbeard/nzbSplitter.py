a9df1be7e227f0602d3984184b1e2f1270164105
labrys <labrys@users.noreply.github.com>
2015-11-25 04:15:45 -0500

    Clean up regexs and logic, refactor, and docstrings

--- sickbeard/nzbSplitter.py
+++ sickbeard/nzbSplitter.py
@@ -1,5 +1,6 @@
+# coding=utf-8
 # Author: Nic Wolfe <nic@wolfeden.ca>
-# URL: https://sickrage.github.io
+# URL: https://SickRage.GitHub.io
 # Git: https://github.com/SickRage/SickRage.git
 #
 # This file is part of SickRage.
@@ -17,192 +18,213 @@
 # You should have received a copy of the GNU General Public License
 # along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
 
+# pylint: disable=line-too-long
 
-import requests
+import requests  # pylint: disable=import-error
 import re
 
 try:
-    import xml.etree.cElementTree as etree
+    import xml.etree.cElementTree as ETree
 except ImportError:
-    import xml.etree.ElementTree as etree
+    import xml.etree.ElementTree as ETree
 
 from sickbeard import logger, classes, helpers
 from sickbeard.common import Quality
+from sickbeard.name_parser.parser import NameParser, InvalidNameException, InvalidShowException
 from sickrage.helper.encoding import ek, ss
 from sickrage.helper.exceptions import ex
 
-from name_parser.parser import NameParser, InvalidNameException, InvalidShowException
 
-
-def getSeasonNZBs(name, urlData, season):
+def get_season_nzbs(name, url_data, season):
     """
     Split a season NZB into episodes
 
     :param name: NZB name
-    :param urlData: URL to get data from
+    :param url_data: URL to get data from
     :param season: Season to check
     :return: dict of (episode files, xml matches)
     """
+
+    # TODO: clean up these regex'es, comment them, and make them all raw strings
+    regex_string = {
+        # Match the xmlns in an nzb
+        # Example:  nzbElement.getchildren()[1].tag == '{http://www.newzbin.com/DTD/2003/nzb}file'
+        #           regex match returns  'http://www.newzbin.com/DTD/2003/nzb'
+        'nzb_xmlns': r"{(http://[\w_\./]+nzb)}file",
+        'scene_name': '([\w\._\ ]+)[\. ]S%02d[\. ]([\w\._\-\ ]+)[\- ]([\w_\-\ ]+?)',  # pylint: disable=anomalous-backslash-in-string
+        'episode': '\.S%02d(?:[E0-9]+)\.[\w\._]+\-\w+',  # pylint: disable=anomalous-backslash-in-string
+    }
+
     try:
-        showXML = etree.ElementTree(etree.XML(urlData))
+        show_xml = ETree.ElementTree(ETree.XML(url_data))
     except SyntaxError:
-        logger.log(u"Unable to parse the XML of " + name + ", not splitting it", logger.ERROR)
-        return ({}, '')
-
-    filename = name.replace(".nzb", "")
-
-    nzbElement = showXML.getroot()
-
-    regex = '([\w\._\ ]+)[\. ]S%02d[\. ]([\w\._\-\ ]+)[\- ]([\w_\-\ ]+?)' % season
-
-    sceneNameMatch = re.search(regex, filename, re.I)
-    if sceneNameMatch:
-        showName, qualitySection, groupName = sceneNameMatch.groups()  # @UnusedVariable
-    else:
-        # Make sure we aren't missing valid results after changing name_parser and the quality detection
-        # Most of these will likely be invalid shows
-        logger.log(u"Unable to parse " + name + " into a scene name.", logger.DEBUG)
+        logger.log(u"Unable to parse the XML of " + name + ", not splitting it", logger.ERROR)  # pylint: disable=no-member
         return {}, ''
 
-    regex = '(' + re.escape(showName) + '\.S%02d(?:[E0-9]+)\.[\w\._]+\-\w+' % season + ')'
+    nzb_element = show_xml.getroot()
+
+    scene_name_match = re.search(regex_string['scene_name'] % season, filename, re.I)
+    if scene_name_match:
+        show_name = scene_name_match.groups()[0]
+    else:  # Make sure we aren't missing valid results after changing name_parser and the quality detection
+        # Most of these will likely be invalid shows
+        logger.log(u"Unable to parse " + name + " into a scene name.", logger.DEBUG)   # pylint: disable=no-member
+        return {}, ''
+
+    regex = '(' + re.escape(show_name) + regex_string['episode'] % season + ')'
     regex = regex.replace(' ', '.')
 
-    epFiles = {}
+    ep_files = {}
     xmlns = None
 
-    for curFile in nzbElement.getchildren():
-        xmlnsMatch = re.match("\{(http:\/\/[A-Za-z0-9_\.\/]+\/nzb)\}file", curFile.tag)
-        if not xmlnsMatch:
+    for cur_file in nzb_element.getchildren():
+        xmlns_match = re.match(regex_string['nzb_xmlns'], cur_file.tag)
+        if not xmlns_match:
             continue
         else:
-            xmlns = xmlnsMatch.group(1)
-        match = re.search(regex, curFile.get("subject"), re.I)
+            xmlns = xmlns_match.group(1)
+        match = re.search(regex, cur_file.get("subject"), re.I)
         if not match:
-            # print curFile.get("subject"), "doesn't match", regex
+            # regex couldn't match cur_file.get("subject")
             continue
-        curEp = match.group(1)
-        if curEp not in epFiles:
-            epFiles[curEp] = [curFile]
+        cur_ep = match.group(1)
+        if cur_ep not in ep_files:
+            ep_files[cur_ep] = [cur_file]
         else:
-            epFiles[curEp].append(curFile)
+            ep_files[cur_ep].append(cur_file)
+    # TODO: Decide what to do if we found multiple valid xmlns strings, should we only return the last???
+    return ep_files, xmlns
 
-    return (epFiles, xmlns)
 
+def create_nzb_string(file_elements, xmlns):
+    """
+    Extract extra info from file_elements.
 
-def createNZBString(fileElements, xmlns):
-    rootElement = etree.Element("nzb")
+    :param file_elements: to be processed
+    :param xmlns: the xml namespace to be used
+    :return: string containing all extra info extracted from the file_elements
+    """
+    root_element = ETree.Element("nzb")
     if xmlns:
-        rootElement.set("xmlns", xmlns)
+        root_element.set("xmlns", xmlns)
 
-    for curFile in fileElements:
-        rootElement.append(stripNS(curFile, xmlns))
+    for cur_file in file_elements:
+        root_element.append(strip_xmlns(cur_file, xmlns))
 
-    return etree.tostring(ss(rootElement))
+    return ETree.tostring(ss(root_element))
 
 
-def saveNZB(nzbName, nzbString):
+def save_nzb(nzb_name, nzb_string):
     """
     Save NZB to disk
 
-    :param nzbName: Filename/path to write to
-    :param nzbString: Content to write in file
+    :param nzb_name: Filename/path to write to
+    :param nzb_string: Content to write in file
     """
     try:
-        with ek(open, nzbName + ".nzb", 'w') as nzb_fh:
-            nzb_fh.write(nzbString)
+        with ek(open, nzb_name + ".nzb", 'w') as nzb_fh:
+            nzb_fh.write(nzb_string)
 
-    except EnvironmentError, e:
-        logger.log(u"Unable to save NZB: " + ex(e), logger.ERROR)
+    except EnvironmentError as error:
+        logger.log(u"Unable to save NZB: " + ex(error), logger.ERROR)  # pylint: disable=no-member
 
 
-def stripNS(element, ns):
-    element.tag = element.tag.replace("{" + ns + "}", "")
-    for curChild in element.getchildren():
-        stripNS(curChild, ns)
+def strip_xmlns(element, xmlns):
+    """
+    Remove the xml namespace from the element's children.
+
+    :param element: to be processed
+    :param xmlns: xml namespace to be removed
+    :return: processed element
+    """
+    element.tag = element.tag.replace("{" + xmlns + "}", "")
+    for cur_child in element.getchildren():
+        strip_xmlns(cur_child, xmlns)
 
     return element
 
 
-def splitResult(result):
+# TODO: change return to empty list on failure since it evaluates as False anyway
+def split_result(obj):
     """
-    Split result into seperate episodes
+    Split obj into separate episodes.
 
-    :param result: search result object
-    :return: False upon failure, a list of episode objects otherwise
+    :param obj: to search for results
+    :return: a list of episode objects or an empty list
     """
-    urlData = helpers.getURL(result.url, session=requests.Session(), needBytes=True)
-    if urlData is None:
-        logger.log(u"Unable to load url " + result.url + ", can't download season NZB", logger.ERROR)
-        return False
+    url_data = helpers.getURL(obj.url, session=requests.Session(), needBytes=True)
+    if url_data is None:
+        logger.log(u"Unable to load url " + obj.url + ", can't download season NZB", logger.ERROR)  # pylint: disable=no-member
+        return []
 
     # parse the season ep name
     try:
-        np = NameParser(False, showObj=result.show)
-        parse_result = np.parse(result.name)
+        parsed_obj = NameParser(False, showObj=obj.show).parse(obj.name)
     except InvalidNameException:
-        logger.log(u"Unable to parse the filename " + result.name + " into a valid episode", logger.DEBUG)
-        return False
+        logger.log(u"Unable to parse the filename " + obj.name + " into a valid episode", logger.DEBUG)  # pylint: disable=no-member
+        return []
     except InvalidShowException:
-        logger.log(u"Unable to parse the filename " + result.name + " into a valid show", logger.DEBUG)
-        return False
+        logger.log(u"Unable to parse the filename " + obj.name + " into a valid show", logger.DEBUG)  # pylint: disable=no-member
+        return []
 
     # bust it up
-    season = parse_result.season_number if parse_result.season_number is not None else 1
+    season = 1 if parsed_obj.season_number is None else parsed_obj.season_number
 
-    separateNZBs, xmlns = getSeasonNZBs(result.name, urlData, season)
+    separate_nzbs, xmlns = get_season_nzbs(obj.name, url_data, season)
 
-    resultList = []
+    result_list = []
 
-    for newNZB in separateNZBs:
-
-        logger.log(u"Split out " + newNZB + " from " + result.name, logger.DEBUG)
+    # TODO: Re-evaluate this whole section
+    #   If we have valid results and hit an exception, we ignore the results found so far.
+    #   Maybe we should return the results found or possibly continue with the next iteration of the loop
+    for new_nzb in separate_nzbs:
+        logger.log(u"Split out " + new_nzb + " from " + obj.name, logger.DEBUG)  # pylint: disable=no-member
 
         # parse the name
         try:
-            np = NameParser(False, showObj=result.show)
-            parse_result = np.parse(newNZB)
+            parsed_obj = NameParser(False, showObj=obj.show).parse(new_nzb)
         except InvalidNameException:
-            logger.log(u"Unable to parse the filename " + newNZB + " into a valid episode", logger.DEBUG)
-            return False
+            logger.log(u"Unable to parse the filename " + new_nzb + " into a valid episode", logger.DEBUG)  # pylint: disable=no-member
+            return []
         except InvalidShowException:
-            logger.log(u"Unable to parse the filename " + newNZB + " into a valid show", logger.DEBUG)
-            return False
+            logger.log(u"Unable to parse the filename " + new_nzb + " into a valid show", logger.DEBUG)  # pylint: disable=no-member
+            return []
 
         # make sure the result is sane
-        if (parse_result.season_number is not None and parse_result.season_number != season) or (
-                parse_result.season_number is None and season != 1):
-            logger.log(
-                u"Found " + newNZB + " inside " + result.name + " but it doesn't seem to belong to the same season, ignoring it",
-                logger.WARNING)
+        if (parsed_obj.season_number != season) or (parsed_obj.season_number is None and season != 1):
+            # pylint: disable=no-member
+            logger.log(u"Found " + new_nzb + " inside " + obj.name + " but it doesn't seem to belong to the same season, ignoring it",
+                       logger.WARNING)
             continue
-        elif len(parse_result.episode_numbers) == 0:
-            logger.log(
-                u"Found " + newNZB + " inside " + result.name + " but it doesn't seem to be a valid episode NZB, ignoring it",
-                logger.WARNING)
+        elif len(parsed_obj.episode_numbers) == 0:
+            # pylint: disable=no-member
+            logger.log(u"Found " + new_nzb + " inside " + obj.name + " but it doesn't seem to be a valid episode NZB, ignoring it",
+                       logger.WARNING)
             continue
 
-        wantEp = True
-        for epNo in parse_result.episode_numbers:
-            if not result.extraInfo[0].wantEpisode(season, epNo, result.quality):
-                logger.log(u"Ignoring result " + newNZB + " because we don't want an episode that is " +
-                           Quality.qualityStrings[result.quality], logger.INFO)
-                wantEp = False
+        want_ep = True
+        for ep_num in parsed_obj.episode_numbers:
+            if not obj.extraInfo[0].wantEpisode(season, ep_num, obj.quality):
+                # pylint: disable=no-member
+                logger.log(u"Ignoring result " + new_nzb + " because we don't want an episode that is " +
+                           Quality.qualityStrings[obj.quality], logger.INFO)
+                want_ep = False
                 break
-        if not wantEp:
+        if not want_ep:
             continue
 
         # get all the associated episode objects
-        epObjList = []
-        for curEp in parse_result.episode_numbers:
-            epObjList.append(result.extraInfo[0].getEpisode(season, curEp))
+        ep_obj_list = []
+        for cur_ep in parsed_obj.episode_numbers:
+            ep_obj_list.append(obj.extraInfo[0].getEpisode(season, cur_ep))
 
         # make a result
-        curResult = classes.NZBDataSearchResult(epObjList)
-        curResult.name = newNZB
-        curResult.provider = result.provider
-        curResult.quality = result.quality
-        curResult.extraInfo = [createNZBString(separateNZBs[newNZB], xmlns)]
+        cur_obj = classes.NZBDataSearchResult(ep_obj_list)
+        cur_obj.name = new_nzb
+        cur_obj.provider = obj.provider
+        cur_obj.quality = obj.quality
+        cur_obj.extraInfo = [create_nzb_string(separate_nzbs[new_nzb], xmlns)]
 
-        resultList.append(curResult)
+        result_list.append(cur_obj)
 
-    return resultList
+    return result_list
