fc750a678a763fc3f3677f5b6a6e09bc4a5f3bb9
Gaëtan Muller <m.gaetan89@gmail.com>
2015-11-26 20:26:52 +0100

    Move more functions form sickbeard.helpers to sickrage.helper.common and add more tests

--- tests/sickrage_tests/helper/common_tests.py
+++ tests/sickrage_tests/helper/common_tests.py
@@ -24,10 +24,64 @@ import sys
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../lib')))
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
-from sickrage.helper.common import remove_extension, replace_extension
+from sickrage.helper.common import is_torrent_or_nzb_file, pretty_file_size, remove_extension, replace_extension
 
 
 class CommonTests(TestCase):
+    def test_is_torrent_or_nzb_file(self):
+        tests = {
+            None: False,
+            '': False,
+            u'': False,
+            'filename': False,
+            u'filename': False,
+            '.nzb': True,
+            u'.nzb': True,
+            'file.nzb': True,
+            u'file.nzb': True,
+            'file.nzb.part': False,
+            u'file.nzb.part': False,
+            '.torrent': True,
+            u'.torrent': True,
+            'file.torrent': True,
+            u'file.torrent': True,
+            'file.torrent.part': False,
+            u'file.torrent.part': False,
+        }
+
+        for (filename, result) in tests.iteritems():
+            self.assertEqual(is_torrent_or_nzb_file(filename), result)
+
+    def test_pretty_file_size(self):
+        tests = {
+            None: '',
+            '': '',
+            u'': '',
+            '1024': '1.00 KB',
+            u'1024': '1.00 KB',
+            '1024.5': '',
+            u'1024.5': '',
+            -42.5: '-42.50 B',
+            -42: '-42.00 B',
+            0: '0.00 B',
+            25: '25.00 B',
+            25.5: '25.50 B',
+            2 ** 10: '1.00 KB',
+            50 * 2 ** 10 + 25: '50.02 KB',
+            2 ** 20: '1.00 MB',
+            100 * 2 ** 20 + 50 * 2 ** 10 + 25: '100.05 MB',
+            2 ** 30: '1.00 GB',
+            200 * 2 ** 30 + 100 * 2 ** 20 + 50 * 2 ** 10 + 25: '200.10 GB',
+            2 ** 40: '1.00 TB',
+            400 * 2 ** 40 + 200 * 2 ** 30 + 100 * 2 ** 20 + 50 * 2 ** 10 + 25: '400.20 TB',
+            2 ** 50: '1.00 PB',
+            800 * 2 ** 50 + 400 * 2 ** 40 + 200 * 2 ** 30 + 100 * 2 ** 20 + 50 * 2 ** 10 + 25: '800.39 PB',
+            2 ** 60: 2 ** 60,
+        }
+
+        for (size, result) in tests.iteritems():
+            self.assertEqual(pretty_file_size(size), result)
+
     def test_remove_extension(self):
         tests = {
             None: None,
