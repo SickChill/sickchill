a7fa114c3f011281f01db478e8445abe69960afe
labrys <labrys@useres.noreply.github.com>
2015-11-28 14:23:59 -0500

    Lint 10/10

--- tests/ssl_sni_tests.py
+++ tests/ssl_sni_tests.py
@@ -17,23 +17,38 @@
 # You should have received a copy of the GNU General Public License
 # along with SickRage.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys, os.path
+# pylint: disable=line-too-long
+
+"""
+Test SNI and SSL
+"""
+
+import sys
+import os.path
+import unittest
+
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib')))
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import unittest
-import requests
-
+import requests  # pylint: disable=import-error
 import sickbeard.providers as providers
-import certifi
+import certifi  # pylint: disable=import-error
 from sickrage.helper.exceptions import ex
 
 
-class SNI_Tests(unittest.TestCase):
+class SniTests(unittest.TestCase):
+    """
+    Test SNI
+    """
     self_signed_cert_providers = ["Womble's Index", "Libertalia"]
-    def test_SNI_URLS(self):
+
+    def test_sni_urls(self):
+        """
+        Test SNI urls
+        :return:
+        """
         print ''
-        #Just checking all providers - we should make this error on non-existent urls.
+        # Just checking all providers - we should make this error on non-existent urls.
         for provider in [provider for provider in providers.makeProviderList() if provider.name not in self.self_signed_cert_providers]:
             print 'Checking %s' % provider.name
             try:
@@ -45,10 +60,10 @@ class SNI_Tests(unittest.TestCase):
                     print 'SSLError on %s: %s' % (provider.name, ex(error.message))
                     raise
                 else:
-                    print  'Cannot verify certificate for %s' % provider.name
-            except Exception:
+                    print 'Cannot verify certificate for %s' % provider.name
+            except Exception:  # pylint: disable=broad-except
                 pass
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(SNI_Tests)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(SniTests)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
