5e933a8bab8a3996f683bb36df6944c2d18f5fe2
X O <xo@wvvw.me>
2015-11-29 14:38:11 +1030

    move metas to core

--- gui/slick/js/core.js
+++ gui/slick/js/core.js
@@ -22,6 +22,26 @@ var configSuccess = function(){
     $('#prowl_show').trigger('notify');
 };
 
+function metaToBool(pyVar){
+    var meta = $('meta[data-var="' + pyVar + '"]').data('content');
+    if(meta === undefined){
+        console.log(pyVar + ' is empty, did you forget to add this to main.mako?');
+        return meta;
+    } else {
+        meta = (isNaN(meta) ? meta.toLowerCase() : meta.toString());
+        return !(meta === 'false' || meta === 'none' || meta === '0');
+    }
+}
+
+function getMeta(pyVar){
+    return $('meta[data-var="' + pyVar + '"]').data('content');
+}
+
+function isMeta(pyVar, result){
+    var reg = new RegExp(result.length > 1 ? result.join('|') : result);
+    return (reg).test($('meta[data-var="' + pyVar + '"]').data('content'));
+}
+
 var SICKRAGE = {
     common: {
         init: function() {
