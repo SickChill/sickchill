d953f2de4f9ca3d5a4e92c87a5157fdd6cd6b9d4
medariox <dariovizz@hotmail.it>
2015-12-07 20:45:32 +0100

    Fixed two visual bugs in 'Missed Subtitle Management'

--- gui/slick/js/core.js
+++ gui/slick/js/core.js
@@ -2378,18 +2378,20 @@ var SICKRAGE = {
                 var row = '';
                 row += '<tr class="good show-' + indexerId + '">';
                 row += '<td align="center"><input type="checkbox" class="' + indexerId + '-epcheck" name="' + indexerId + '-' + season + 'x' + episode + '"' + (checked ? ' checked' : '') + '></td>';
-                row += '<td style="width: 1%;">' + season + 'x' + episode + '</td>';
-                row += '<td>' + name + '</td>';
-                if(subtitles.length > 0){
-                    row += '<td style="float: right;">';
+                row += '<td style="width: 2%;">' + season + 'x' + episode + '</td>';
+                if (subtitles.length > 0) {
+                    row += '<td style="width: 8%;">';
                     subtitles = subtitles.split(',');
                     for (var i in subtitles) {
                         if (subtitles.hasOwnProperty(i)) {
-                            row += '<img src="/images/subtitles/flags/' + subtitles[i] + '.png" width="16" height="11" alt="' + subtitles[i] + '" />&nbsp;';
+                            row += '<img src="' + srRoot + '/images/subtitles/flags/' + subtitles[i] + '.png" width="16" height="11" alt="' + subtitles[i] + '" />&nbsp;';
                         }
                     }
                     row += '</td>';
+                } else {
+                	row += '<td style="width: 8%;">None</td>';
                 }
+                row += '<td>' + name + '</td>';
                 row += '</tr>';
 
                 return row;
