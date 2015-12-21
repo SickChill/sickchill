e5f7b7f19c754b93b33c8e3faf5ff06796b62940
Valentin Tolmer <valentin.tolmer@gmail.com>
2015-12-22 00:04:34 +0100

    rtorrent: add timeout to hash resolution

--- sickbeard/clients/rtorrent_client.py
+++ sickbeard/clients/rtorrent_client.py
@@ -21,6 +21,7 @@ import traceback
 import sickbeard
 from sickbeard import logger
 from sickbeard.clients.generic import GenericClient
+from sickrage.helper.timeout import exit_after
 from rtorrent import RTorrent
 
 
@@ -51,6 +52,7 @@ class rTorrentAPI(GenericClient):
 
         return self.auth
 
+    @exit_after(60)
     def _add_torrent_uri(self, result):
 
         if not self.auth:
