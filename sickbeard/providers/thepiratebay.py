e1a2b1a453eb7b670619a3e0014f2059b4967629
Dario <dariovizz@hotmail.it>
2016-01-28 09:53:32 +0100

    Replace almost useless RSS query for TPB

--- sickbeard/providers/thepiratebay.py
+++ sickbeard/providers/thepiratebay.py
@@ -47,7 +47,7 @@ class ThePirateBayProvider(TorrentProvider):  # pylint: disable=too-many-instanc
         self.url = 'https://thepiratebay.se/'
         self.urls = {
             'search': self.url + 's/',
-            'rss': self.url + 'tv/latest'
+            'rss': self.url + 'browse/205'
         }
 
         self.custom_url = None
