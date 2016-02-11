1e3d8e288032ead8892d236774f143ccac39249a
medariox <dariovizz@hotmail.it>
2016-02-11 12:28:42 +0100

    Use .eu for podnapisi

--- lib/subliminal/providers/podnapisi.py
+++ lib/subliminal/providers/podnapisi.py
@@ -79,7 +79,7 @@ class PodnapisiSubtitle(Subtitle):
 class PodnapisiProvider(Provider):
     languages = ({Language('por', 'BR'), Language('srp', script='Latn')} |
                  {Language.fromalpha2(l) for l in language_converters['alpha2'].codes})
-    server_url = 'http://podnapisi.net/subtitles/'
+    server_url = 'http://podnapisi.eu/subtitles/'
 
     def initialize(self):
         self.session = Session()
