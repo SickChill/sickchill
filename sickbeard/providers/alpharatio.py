f3d2e1a8d7f2d8036dbb06f37b6926492365d6ef
miigotu <miigotu@gmail.com>
2016-02-14 05:47:52 -0800

    Alpharatio strings updates * Remove u prefix from strings * Normalize strings to use " throughout

--- sickbeard/providers/alpharatio.py
+++ sickbeard/providers/alpharatio.py
@@ -49,14 +49,14 @@ class AlphaRatioProvider(TorrentProvider):  # pylint: disable=too-many-instance-
         self.freeleech = None
 
         # URLs
-        self.url = 'http://alpharatio.cc'
+        self.url = "http://alpharatio.cc"
         self.urls = {
-            'login': urljoin(self.url, 'login.php'),
-            'search': urljoin(self.url, 'torrents.php'),
+            "login": urljoin(self.url, "login.php"),
+            "search": urljoin(self.url, "torrents.php"),
         }
 
         # Proper Strings
-        self.proper_strings = ['PROPER', 'REPACK']
+        self.proper_strings = ["PROPER", "REPACK"]
 
         # Cache
         self.cache = tvcache.TVCache(self)
@@ -66,19 +66,19 @@ class AlphaRatioProvider(TorrentProvider):  # pylint: disable=too-many-instance-
             return True
 
         login_params = {
-            'username': self.username,
-            'password': self.password,
-            'login': 'submit',
-            'remember_me': 'on',
+            "username": self.username,
+            "password": self.password,
+            "login": "submit",
+            "remember_me": "on",
         }
 
-        response = self.get_url(self.urls['login'], post_data=login_params, timeout=30, response='text')
+        response = self.get_url(self.urls["login"], post_data=login_params, timeout=30, response="text")
         if not response:
             logger.log("Unable to connect to provider", logger.WARNING)
             return False
 
-        if re.search('Invalid Username/password', response) \
-                or re.search('<title>Login :: AlphaRatio.cc</title>', response):
+        if re.search("Invalid Username/password", response) \
+                or re.search("<title>Login :: AlphaRatio.cc</title>", response):
             logger.log("Invalid username or password. Check your settings", logger.WARNING)
             return False
 
@@ -91,21 +91,21 @@ class AlphaRatioProvider(TorrentProvider):  # pylint: disable=too-many-instance-
 
         # Search Params
         search_params = {
-            'searchstr': '',
-            'filter_cat[1]': 1,
-            'filter_cat[2]': 1,
-            'filter_cat[3]': 1,
-            'filter_cat[4]': 1,
-            'filter_cat[5]': 1
+            "searchstr": "",
+            "filter_cat[1]": 1,
+            "filter_cat[2]": 1,
+            "filter_cat[3]": 1,
+            "filter_cat[4]": 1,
+            "filter_cat[5]": 1
         }
 
         # Units
-        units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+        units = ["B", "KB", "MB", "GB", "TB", "PB"]
 
         def process_column_header(td):
-            result = ''
+            result = ""
             if td.a and td.a.img:
-                result = td.a.img.get('title', td.a.get_text(strip=True))
+                result = td.a.img.get("title", td.a.get_text(strip=True))
             if not result:
                 result = td.get_text(strip=True)
             return result
@@ -115,60 +115,60 @@ class AlphaRatioProvider(TorrentProvider):  # pylint: disable=too-many-instance-
             logger.log("Search Mode: %s" % mode, logger.DEBUG)
 
             for search_string in search_strings[mode]:
-                if mode != 'RSS':
-                    logger.log(u"Search string: {search}".format
-                               (search=search_string.decode('utf-8')), logger.DEBUG)
+                if mode != "RSS":
+                    logger.log("Search string: {search}".format
+                               (search=search_string.decode("utf-8")), logger.DEBUG)
 
-                search_params['searchstr'] = search_string
-                search_url = self.urls['search']
-                data = self.get_url(search_url, params=search_params, response='text')
+                search_params["searchstr"] = search_string
+                search_url = self.urls["search"]
+                data = self.get_url(search_url, params=search_params, response="text")
                 if not data:
-                    logger.log(u"No data returned from provider", logger.DEBUG)
+                    logger.log("No data returned from provider", logger.DEBUG)
                     continue
 
-                with BS4Parser(data, 'html5lib') as html:
-                    torrent_table = html.find('table', id='torrent_table')
+                with BS4Parser(data, "html5lib") as html:
+                    torrent_table = html.find("table", id="torrent_table")
                     if not torrent_table:
                         continue
 
-                    torrent_rows = torrent_table.find_all('tr') if torrent_table else []
+                    torrent_rows = torrent_table.find_all("tr") if torrent_table else []
                     # Continue only if at least one Release is found
                     if len(torrent_rows) < 2:
-                        logger.log(u"Data returned from provider does not contain any torrents", logger.DEBUG)
+                        logger.log("Data returned from provider does not contain any torrents", logger.DEBUG)
                         continue
 
-                    # '', '', 'Name /Year', 'Files', 'Time', 'Size', 'Snatches', 'Seeders', 'Leechers'
-                    labels = [process_column_header(label) for label in torrent_rows[0].find_all('td')]
+                    # "", "", "Name /Year", "Files", "Time", "Size", "Snatches", "Seeders", "Leechers"
+                    labels = [process_column_header(label) for label in torrent_rows[0].find_all("td")]
 
                     # Skip column headers
                     for result in torrent_rows[1:]:
-                        cells = result.find_all('td')
+                        cells = result.find_all("td")
                         if len(cells) < len(labels):
                             continue
 
                         try:
-                            title = cells[labels.index('Name /Year')].find('a', dir='ltr').get_text(strip=True)
-                            download_url = urljoin(self.url, cells[labels.index('Name /Year')].find('a', title='Download')['href'])
+                            title = cells[labels.index("Name /Year")].find("a", dir="ltr").get_text(strip=True)
+                            download_url = urljoin(self.url, cells[labels.index("Name /Year")].find("a", title="Download")["href"])
                             if not all([title, download_url]):
                                 continue
 
-                            seeders = try_int(cells[labels.index('Seeders')].get_text(strip=True))
-                            leechers = try_int(cells[labels.index('Leechers')].get_text(strip=True))
+                            seeders = try_int(cells[labels.index("Seeders")].get_text(strip=True))
+                            leechers = try_int(cells[labels.index("Leechers")].get_text(strip=True))
 
                             # Filter unseeded torrent
                             if seeders < self.minseed or leechers < self.minleech:
-                                if mode != 'RSS':
-                                    logger.log(u"Discarding torrent because it doesn't meet the"
-                                               u" minimum seeders or leechers: {} (S:{} L:{})".format
+                                if mode != "RSS":
+                                    logger.log("Discarding torrent because it doesn't meet the"
+                                               " minimum seeders or leechers: {} (S:{} L:{})".format
                                                (title, seeders, leechers), logger.DEBUG)
                                 continue
 
-                            torrent_size = cells[labels.index('Size')].get_text(strip=True)
+                            torrent_size = cells[labels.index("Size")].get_text(strip=True)
                             size = convert_size(torrent_size, units=units) or -1
 
                             item = title, download_url, size, seeders, leechers
-                            if mode != 'RSS':
-                                logger.log(u"Found result: {} with {} seeders and {} leechers".format
+                            if mode != "RSS":
+                                logger.log("Found result: {} with {} seeders and {} leechers".format
                                            (title, seeders, leechers), logger.DEBUG)
 
                             items.append(item)
