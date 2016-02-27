3ec5b3bfb6f9c730739da364576067be9810e9c0
Erik Weber <terbolous@gmail.com>
2016-02-27 23:28:55 +0100

    rewrite, got rid of unnecessary methods and based the provider on abnormal.py

--- sickbeard/providers/norbits.py
+++ sickbeard/providers/norbits.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-'''A Norbits (https://norbits.net) provider, based on hdbits.py'''
+'''A Norbits (https://norbits.net) provider'''
 
 # URL: https://sickrage.github.io
 #
@@ -22,8 +22,6 @@ from requests.compat import urlencode
 
 from sickbeard import logger, tvcache
 
-from sickbeard.helpers import sanitizeSceneName
-from sickrage.helper.common import episode_num
 from sickrage.helper.exceptions import AuthException
 from sickrage.providers.torrent.TorrentProvider import TorrentProvider
 
@@ -33,7 +31,7 @@ except ImportError:
     import simplejson as json
 
 
-class NorbitsProvider(TorrentProvider):
+class NorbitsProvider(TorrentProvider): # pylint: disable=too-many-instance-attributes
     '''Main provider object'''
 
     def __init__(self):
@@ -43,14 +41,15 @@ class NorbitsProvider(TorrentProvider):
         self.username = None
         self.passkey = None
         self.ratio = None
+        self.minseed = None
+        self.minleech = None
 
         self.cache = tvcache.TVCache(self, min_time=20)  # only poll Norbits every 15 minutes max
 
-        self.urls = {'base_url': 'https://norbits.net',
-                     'search': 'https://norbits.net/api2.php?action=torrents',
-                     'download': 'https://norbits.net/download.php?'}
+        self.url = 'https://norbits.net'
+        self.urls = {'search': self.url + '/api2.php?action=torrents',
+                     'download': self.url + '/download.php?'}
 
-        self.url = self.urls['base_url']
         self.logger = logger.Logger()
 
     def _check_auth(self):
@@ -71,74 +70,75 @@ class NorbitsProvider(TorrentProvider):
 
         return True
 
-    def _get_season_search_strings(self, ep_obj):
-        season_search_string = [self._make_post_data_JSON(show=ep_obj.show, season=ep_obj)]
-        return season_search_string
-
-    def _get_episode_search_strings(self, ep_obj, add_string=''):
-        episode_search_string = [self._make_post_data_JSON(show=ep_obj.show, episode=ep_obj)]
-        return episode_search_string
-
-    def _get_title_and_url(self, item):
-        title = item.get('name', '').replace(' ', '.')
-        url = self.urls['download'] + urlencode({'id': item['id'], 'passkey': self.passkey})
-        return title, url
-
-    def search(self, search_params, age=0, ep_obj=None):
+    def search(self, search_params, age=0, ep_obj=None): # pylint: disable=too-many-locals
         ''' Do the actual searching and JSON parsing'''
 
         results = []
 
-        self.logger.log(u'Search string: %s' % search_params, logger.DEBUG)
+        for mode in search_params:
+            items = []
+            self.logger.log(u'Search Mode: {}'.format(mode), logger.DEBUG)
 
-        self._check_auth()
-        parsed_json = self.get_url(self.urls['search'], post_data=search_params, json=True)
-        if not parsed_json:
-            return []
+            for search_string in search_params[mode]:
+                if mode != 'RSS':
+                    self.logger.log('Search string: {}'.format(search_string.decode('utf-8')),
+                                    logger.DEBUG)
 
-        if self._checkAuthFromData(parsed_json):
-            if parsed_json and 'data' in parsed_json:
-                items = parsed_json['data']
-            else:
-                self.logger.log((u'Resulting JSON from provider is not correct, '
-                                 'not parsing it'), logger.ERROR)
-                items = []
-            if 'torrents' in items:
-                for item in items['torrents']:
-                    results.append(item)
-        self.logger.log(u'results: %s' % results, logger.DEBUG)
+                post_data = {
+                    'username': self.username,
+                    'passkey': self.passkey,
+                    'category': '2', # TV Category
+                    'search': search_string,
+                }
+
+                self._check_auth()
+                parsed_json = self.get_url(self.urls['search'],
+                                           post_data=json.dumps(post_data),
+                                           json=True)
+
+                if not parsed_json:
+                    return []
+
+                if self._checkAuthFromData(parsed_json):
+                    if parsed_json and 'data' in parsed_json:
+                        json_items = parsed_json['data']
+                    else:
+                        self.logger.log((u'Resulting JSON from provider is not correct, '
+                                         'not parsing it'), logger.ERROR)
+                    if 'torrents' in json_items:
+                        for item in json_items['torrents']:
+                            seeders = item['seeders']
+                            leechers = item['leechers']
+                            title = item['name']
+                            info_hash = item['info_hash']
+                            size = item['size']
+                            download_url = ('{}{}'.format(self.urls['download'],
+                                                          urlencode({'id': item['id'],
+                                                                     'passkey': self.passkey})))
+
+                            if seeders < self.minseed or leechers < self.minleech:
+                                self.logger.log((u'Discarding torrent because it does not meet '
+                                                 'the minimum seeders or leechers: '
+                                                 '{} (S:{} L:{})').format
+                                                (title, seeders, leechers), logger.DEBUG)
+                                continue
+                            else:
+                                item = title, download_url, size, seeders, leechers, info_hash
+                                if mode != "RSS":
+                                    self.logger.log((u'Found result: {} with {} seeders and {}'
+                                                     'leechers').format(title,
+                                                                        seeders,
+                                                                        leechers), logger.DEBUG)
+
+                                items.append(item)
+            # For each search mode sort all the items by seeders if available
+            items.sort(key=lambda tup: tup[3], reverse=True)
+
+            results += items
+
         return results
-
-    def _make_post_data_JSON(self, show=None, episode=None, season=None, search_term=None):  # pylint: disable=invalid-name
-        ''' Make post data for our JSON query'''
-        post_data = {
-            'username': self.username,
-            'passkey': self.passkey,
-            'category': '2',
-            # TV Category
-        }
-        if episode:
-            ep_num = episode_num(episode.scene_season, episode.scene_episode)
-            show_name = sanitizeSceneName(show.name)
-            search = '{}.{}'.format(show_name, ep_num)
-            post_data['search'] = search
-
-        if season:
-            self.logger.log(u'season: %s' % dir(season), logger.DEBUG)
-        if search_term:
-            post_data['search'] = search_term
-
-        return json.dumps(post_data)
 
     def seed_ratio(self):
         return self.ratio
-
-
-class NorbitsCache(tvcache.TVCache):
-    ''' Hold our cache'''
-
-    def _getRSSData(self):
-        ''' We don't want to do RSS searchs'''
-        pass
 
 provider = NorbitsProvider()  # pylint: disable=invalid-name
