cd4b972cd193a3e6ce6ef6edf4ee5b4cb8f6f000
miigotu <miigotu@gmail.com>
2016-02-27 20:21:37 -0800

    Change UA for statistical reasons

--- sickbeard/common.py
+++ sickbeard/common.py
@@ -47,7 +47,7 @@ from sickrage.recompiled import tags
 # This is disabled, was only added for testing, and has no config.ini or web ui setting. To enable, set SPOOF_USER_AGENT = True
 SPOOF_USER_AGENT = False
 INSTANCE_ID = str(uuid.uuid1())
-USER_AGENT = ('SickRage/(' + platform.system() + '; ' + platform.release() + '; ' + INSTANCE_ID + ')')
+USER_AGENT = ('Sick-Rage.CE.1/(' + platform.system() + '; ' + platform.release() + '; ' + INSTANCE_ID + ')')
 UA_SETTINGS.DB = ek(path.abspath, ek(path.join, ek(path.dirname, __file__), '../lib/fake_useragent/ua.json'))
 UA_POOL = UserAgent()
 if SPOOF_USER_AGENT:
