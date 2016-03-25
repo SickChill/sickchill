1847f03608e96cf80b9999233811ff58021ddb3e
miigotu <miigotu@gmail.com>
2016-03-24 17:07:18 -0700

    Use cookie auth for DSM Show error messages for error codes returned from DSM according to DSM docs Check DSM version, and fix the download location if it is absolute on DSM6

--- sickbeard/clients/download_station_client.py
+++ sickbeard/clients/download_station_client.py
@@ -21,9 +21,13 @@
 #
 # Uses the Synology Download Station API: http://download.synology.com/download/Document/DeveloperGuide/Synology_Download_Station_Web_API.pdf
 
+from __future__ import unicode_literals
+from requests.compat import urljoin
+import os
+import re
+
 import sickbeard
 from sickbeard.clients.generic import GenericClient
-from requests.compat import urljoin
 
 
 class DownloadStationAPI(GenericClient):
@@ -32,11 +36,47 @@ class DownloadStationAPI(GenericClient):
 
         super(DownloadStationAPI, self).__init__('DownloadStation', host, username, password)
 
-        self.url = urljoin(self.host, 'webapi/DownloadStation/task.cgi')
+        self.urls = {
+            'login': urljoin(self.host, 'webapi/auth.cgi'),
+            'task': urljoin(self.host, 'webapi/DownloadStation/task.cgi'),
+            'info': urljoin(self.host, '/webapi/DownloadStation/info.cgi'),
+            'dsminfo': urljoin(self.host, '/webapi/entry.cgi')
+        }
+
+        self.url = self.urls['task']
+
+        self.error_map = {
+            100: 'Unknown error',
+            101: 'Invalid parameter',
+            102: 'The requested API does not exist',
+            103: 'The requested method does not exist',
+            104: 'The requested version does not support the functionality',
+            105: 'The logged in session does not have permission',
+            106: 'Session timeout',
+            107: 'Session interrupted by duplicate login'
+        }
+        self.checked_destination = False
+        self.destination = sickbeard.TORRENT_PATH
+
+    def _check_response(self):
+        try:
+            jdata = self.response.json()
+        except ValueError:
+            self.session.cookies.clear()
+            self.auth = False
+            return self.auth
+
+        self.auth = jdata.get('success')
+        if not self.auth:
+            error_code = jdata.get('error', {}).get('code')
+            sickbeard.logger.log('{}'.format(self.error_map.get(error_code, jdata)))
+            self.session.cookies.clear()
+
+        return self.auth
 
     def _get_auth(self):
-
-        auth_url = urljoin(self.host, 'webapi/auth.cgi')
+        if self.session.cookies and self.auth:
+            return self.auth
 
         params = {
             'api': 'SYNO.API.Auth',
@@ -45,16 +85,19 @@ class DownloadStationAPI(GenericClient):
             'account': self.username.encode('utf-8') if isinstance(self.username, unicode) else self.username,
             'passwd': self.password.encode('utf-8') if isinstance(self.password, unicode) else self.password,
             'session': 'DownloadStation',
-            'format': 'sid'
+            'format': 'cookie'
         }
 
         try:
-            self.response = self.session.get(auth_url, params=params, verify=False)
-            self.auth = self.response.json()['data']['sid']
-        except Exception:
-            return None
+            self.response = self.session.get(self.urls['login'], params=params, verify=False)
+            self.response.raise_for_status()
+        except Exception as error:
+            sickbeard.helpers.handle_requests_exception(error)
+            self.session.cookies.clear()
+            self.auth = False
+            return self.auth
 
-        return self.auth
+        return self._check_response()
 
     def _add_torrent_uri(self, result):
 
@@ -63,16 +106,17 @@ class DownloadStationAPI(GenericClient):
             'version': '1',
             'method': 'create',
             'session': 'DownloadStation',
-            '_sid': self.auth,
             'uri': result.url
         }
+
+        if not self._check_destination():
+            return False
 
         if sickbeard.TORRENT_PATH:
             data['destination'] = sickbeard.TORRENT_PATH
 
         self._request(method='post', data=data)
-
-        return self.response.json()['success']
+        return self._check_response()
 
     def _add_torrent_file(self, result):
 
@@ -81,14 +125,87 @@ class DownloadStationAPI(GenericClient):
             'version': '1',
             'method': 'create',
             'session': 'DownloadStation',
-            '_sid': self.auth
         }
+
+        if not self._check_destination():
+            return False
 
         if sickbeard.TORRENT_PATH:
             data['destination'] = sickbeard.TORRENT_PATH
-        files = {'file': (result.name + '.torrent', result.content)}
-        self._request(method='post', data=data, files=files)
 
-        return self.response.json()['success']
+        files = {'file': (result.name + '.torrent', result.content)}
+
+        self._request(method='post', data=data, files=files)
+        return self._check_response()
+
+    def _check_destination(self):  # pylint: disable=too-many-return-statements, too-many-branches
+        if not (self.auth or self._get_auth()):
+            return False
+
+        if self.checked_destination and self.destination == sickbeard.TORRENT_PATH:
+            return True
+
+        params = {
+            'api': 'SYNO.DSM.Info',
+            'version': 2,
+            'method': 'getinfo',
+            'session': 'DownloadStation'
+        }
+
+        try:
+            self.response = self.session.get(self.urls['dsminfo'], params=params, verify=False, timeout=120)
+            self.response.raise_for_status()
+        except Exception as error:
+            sickbeard.helpers.handle_requests_exception(error)
+            self.session.cookies.clear()
+            self.auth = False
+            return False
+
+        destination = ''
+        if self._check_response():
+            jdata = self.response.json()
+            version_string = jdata.get('data', {}).get('version_string')
+            if not version_string:
+                sickbeard.logger.log('Could not get the version_string from DSM: {0}'.format(jdata))
+                return False
+
+            if version_string.startswith('DSM 6'):
+                #  This is DSM6, lets make sure the location is relative
+                if sickbeard.TORRENT_PATH:
+                    if os.path.isabs(sickbeard.TORRENT_PATH):
+                        sickbeard.TORRENT_PATH = re.sub(r'^/volume\d/', '', sickbeard.TORRENT_PATH).lstrip('/')
+                else:
+                    #  Since they didnt specify the location in the settings, lets make sure the default is relative,
+                    #  Or forcefully set the location setting in SickRage
+                    params.update({
+                        'method': 'getconfig',
+                        'version': 2
+                    })
+
+                    try:
+                        self.response = self.session.get(self.urls['info'], params=params, verify=False, timeout=120)
+                        self.response.raise_for_status()
+                    except Exception as error:
+                        sickbeard.helpers.handle_requests_exception(error)
+                        self.session.cookies.clear()
+                        self.auth = False
+                        return False
+
+                    if self._check_response():
+                        jdata = self.response.json()
+                        destination = jdata.get('data', {}).get('default_destination')
+                        if destination:
+                            if os.path.isabs(destination):
+                                sickbeard.TORRENT_PATH = re.sub(r'^/volume\d/', '', destination).lstrip('/')
+                        else:
+                            sickbeard.logger.log('default_destination could not be determined for DSM6: {0}'.format(jdata))
+                            return False
+
+        if destination or sickbeard.TORRENT_PATH:
+            sickbeard.logger.log('Destination is now {0}'.format(sickbeard.TORRENT_PATH or destination))
+
+        self.checked_destination = True
+        self.destination = sickbeard.TORRENT_PATH
+        return True
 
 api = DownloadStationAPI()
