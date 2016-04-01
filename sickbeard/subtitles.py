e28551717fa91afd305555cecbfa04f964e732c4
miigotu <miigotu@gmail.com>
2016-03-31 19:10:55 -0700

    zzz

--- sickbeard/subtitles.py
+++ sickbeard/subtitles.py
@@ -57,22 +57,34 @@ PROVIDER_URLS = {
     'tvsubtitles': 'http://www.tvsubtitles.net'
 }
 
+
 class SubtitleProviderPool(object):
     _lock = threading.Lock()
     _creation = None
     _instance = None
 
-    def __init_instance(self):
+    @staticmethod
+    def __init_instance():
         with SubtitleProviderPool._lock:
             providers = enabled_service_list()
-            provider_configs = {'addic7ed': {'username': sickbeard.ADDIC7ED_USER,
-                                             'password': sickbeard.ADDIC7ED_PASS},
-                                'itasa': {'username': sickbeard.ITASA_USER,
-                                          'password': sickbeard.ITASA_PASS},
-                                'legendastv': {'username': sickbeard.LEGENDASTV_USER,
-                                                   'password': sickbeard.LEGENDASTV_PASS},
-                                'opensubtitles': {'username': sickbeard.OPENSUBTITLES_USER,
-                                                  'password': sickbeard.OPENSUBTITLES_PASS}}
+            provider_configs = {
+                'addic7ed': {
+                    'username': sickbeard.ADDIC7ED_USER,
+                    'password': sickbeard.ADDIC7ED_PASS
+                },
+                'itasa': {
+                    'username': sickbeard.ITASA_USER,
+                    'password': sickbeard.ITASA_PASS
+                },
+                'legendastv': {
+                    'username': sickbeard.LEGENDASTV_USER,
+                    'password': sickbeard.LEGENDASTV_PASS
+                },
+                'opensubtitles': {
+                    'username': sickbeard.OPENSUBTITLES_USER,
+                    'password': sickbeard.OPENSUBTITLES_PASS
+                }
+            }
 
             SubtitleProviderPool._instance = ProviderPool(providers=providers, provider_configs=provider_configs)
 
