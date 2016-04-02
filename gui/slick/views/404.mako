d95491454ab53d9a6a0dc3ee4f4b5ffeb13ebe47
miigotu <miigotu@gmail.com>
2016-04-02 13:02:24 -0700

    Add setup.cfg, Add babel methods to setup.py, Add initial pot file in po/sickrage.pot and add one translation in the 404 page Remove BOM in torrentbytes These probably arent loaded yet, but a good place to start

--- gui/slick/views/404.mako
+++ gui/slick/views/404.mako
@@ -2,6 +2,6 @@
 <%block name="content">
 <h1 class="header">${header}</h1>
 <div class="align-center">
-You have reached this page by accident, please check the url.
+${_('You have reached this page by accident, please check the url.')}
 </div>
 </%block>
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ Use setup tools to install sickrage
 """
 import os
 from setuptools import find_packages, setup
+from babel.messages import frontend as babel
 
 ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
 
@@ -12,10 +13,21 @@ with open(os.path.join(ROOT, 'readme.md'), 'r') as r:
 setup(
     name="sickrage",
     version="0.0.1",
+
     description="Automatic Video Library Manager for TV Shows",
     long_description=long_description,
+
+    url='https://sickrage.github.io',
+    download_url='https://github.com/SickRage/SickRage.git',
+
+    author='miigotu',
+    author_email='miigotu@gmail.com',
+
+    license='GPLv2',
+
     packages=find_packages(),
     install_requires=[],
+
     test_suite="tests",
     tests_require=[
         'coveralls',
@@ -23,10 +35,24 @@ setup(
         'rednose',
         'mock',
     ],
+
     classifiers=[
-        'Development Status :: ???',
-        'Intended Audience :: Developers',
-        'Operating System :: POSIC :: LINUX',
-        'Topic :: ???',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Multimedia :: Video',
     ],
+
+    cmdclass={
+        'compile_catalog': babel.compile_catalog,
+        'extract_messages': babel.extract_messages,
+        'init_catalog': babel.init_catalog,
+        'update_catalog': babel.update_catalog
+    },
+
+    message_extractors={
+        'gui': [('**.mako', 'mako', {'input_encoding': 'utf-8'})],
+        'sickrage': [('**.py', 'python', None)],
+        'sickbeard': [('**.py', 'python', None)],
+    },
 )
