8efe16715dc807dc1762cc98fc3e15ca8fb18511
Clinton Collins <clcollins@deloitte.com>
2016-04-18 10:39:31 -0400

    Change to sickrage client IDs and fix lines longer than 80 characters

--- sickbeard/clients/putio_client.py
+++ sickbeard/clients/putio_client.py
@@ -24,28 +24,31 @@ from .generic import GenericClient
 # Base of our Put.io APIs
 PUTIO_BASE_URL='https://api.put.io/v2'
 # API Client ID that is registerd on put.io
-API_CLIENT_ID='2103'
+API_CLIENT_ID='2392'
 # API Client URL registered with put.io
-API_CLIENT_REGISTERED_URL='http://devclinton.com'
+API_CLIENT_REGISTERED_URL='https://sickrage.github.io'
 
 class PutioAPI(GenericClient):
     def __init__(self, host=None, username=None, password=None):
         super(PutioAPI, self).__init__('put_io', host, username, password)
-        self.url = '{}/oauth2/authenticate?client_id={}&response_type=token&redirect_uri={}'.format(PUTIO_BASE_URL,API_CLIENT_ID, urllib.urlencode(API_CLIENT_REGISTERED_URL))
+        self.url = '{}/oauth2/authenticate?client_id={}&response_type=token&redirect_uri={}'.format(
+            PUTIO_BASE_URL,API_CLIENT_ID, urllib.urlencode(API_CLIENT_REGISTERED_URL))
 
     def _get_auth(self):
 
         post_data = {
             'name': self.username,
             'password': self.password,
-            'next': '/v2/oauth2/authenticate?client_id={}&response_type=token&redirect_uri=http://devclinton.com'.format(API_CLIENT_ID)
+            'next': '/v2/oauth2/authenticate?client_id={}&response_type=token&redirect_uri={}'.format(
+                API_CLIENT_ID,API_CLIENT_REGISTERED_URL)
         }
         try:
             self.response = self.session.get(self.url, timeout=120)
             self.tok_response = self.session.post('https://api.put.io/login',
                                                   data=post_data, allow_redirects=False)
             self.tok_response = self.session.get(self.tok_response.headers['location'], allow_redirects=False)
-            self.auth = re.search('{}#access_token=(.*)'.format(API_CLIENT_REGISTERED_URL), self.tok_response.headers['location']).group(1)
+            self.auth = re.search('{}#access_token=(.*)'.format(re.escape(API_CLIENT_REGISTERED_URL)),
+                                  self.tok_response.headers['location']).group(1)
         except Exception:
             logger.log("Putio Auth Error: {}".format(Exception), logger.INFO)
             return None
@@ -70,7 +73,8 @@ class PutioAPI(GenericClient):
         post_data = { 'name': 'putio_torrent','parent': 0 }
         params={ 'oauth_token': self.auth }
 
-        self.response = self.session.post('{}/files/upload?oauth_token={}'.format(PUTIO_BASE_URL,self.access_token),  data=post_data, files=('putio_torrent',result.name), params=params)
+        self.response = self.session.post('{}/files/upload?oauth_token={}'.format(PUTIO_BASE_URL,self.access_token),
+                                          data=post_data, files=('putio_torrent',result.name), params=params)
         return self.response.json()['status'] == "OK"
 
 api = PutioAPI()
