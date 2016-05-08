c7b96f5976878ee7d1384b97a6ba92e53b58cd54
Michael Davies <a1studmuffin@gmail.com>
2016-05-08 20:16:01 +1000

    Added optional episode separator to name_parser 'bare' regex
    I noticed the post-processor was failing to rename episodes with the
    format:
    
    show.name.01e05.blah.avi
    
    I extended the name_parser 'bare' regex to add support for an optional
    episode separator, and also added a name_parser test case to verify the
    change has not broken any existing tests and also passes a new test for
    this case.

--- tests/name_parser_tests.py
+++ tests/name_parser_tests.py
@@ -74,6 +74,7 @@ SIMPLE_TEST_CASES = {
         'show.name.2010.123.source.quality.etc-group': parser.ParseResult(None, 'show name 2010', 1, [23], 'source.quality.etc', 'group'),
         'show.name.2010.222.123.source.quality.etc-group': parser.ParseResult(None, 'show name 2010.222', 1, [23], 'source.quality.etc', 'group'),
         'Show.Name.102': parser.ParseResult(None, 'Show Name', 1, [2]),
+        'Show.Name.01e02': parser.ParseResult(None, 'Show Name', 1, [2]),
         'the.event.401.hdtv-group': parser.ParseResult(None, 'the event', 4, [1], 'hdtv', 'group'),
         'show.name.2010.special.hdtv-blah': None,
         'show.ex-name.102.hdtv-group': parser.ParseResult(None, 'show ex-name', 1, [2], 'hdtv', 'group'),
