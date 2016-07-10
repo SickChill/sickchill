8a073f6cd2643a9e30b8538ca7a5986d5158da19
Kevin Richter <me@kevinrichter.nl>
2016-07-10 14:18:05 +0200

    Allow unlimited shows to be mass updated

--- gui/slick/js/massUpdate.js
+++ gui/slick/js/massUpdate.js
@@ -8,12 +8,11 @@ $(document).ready(function(){
 
         if(editArr.length === 0) { return; }
 
-        var url = srRoot + '/manage/massEdit?toEdit='+editArr.join('|');
-        if(url.length < 2083) {
-            window.location.href = url;
-        } else {
-            alert("You've selected too many shows, please uncheck some and try again. [" + url.length + "/2083 characters]");
-        }
+        var form = $(
+            "<form method='post' action='/manage/massEdit'>" +
+                "<input type='hidden' name='toEdit' value='" + editArr.join('|') + "'/>" +
+            "</form>"
+        ).submit();
     });
 
     $('.submitMassUpdate').on('click', function(){
