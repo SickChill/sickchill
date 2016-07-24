fb35cd2208e7f642e864adfedb81703dcd565729
Valentino Dell'Aica <elettronik@gmail.com>
2016-07-24 23:55:38 +0200

    Retry download subtitles: GUI changes

--- gui/slick/js/ajaxEpSubtitles.js
+++ gui/slick/js/ajaxEpSubtitles.js
@@ -42,4 +42,77 @@
             return false;
         });
     };
+
+    $.ajaxRetrySubtitlesSearch = {
+        defaults: {
+            size:               11,
+            loadingImage:       'loading16.gif',
+            noImage:            'no16.png'
+        }
+    };
+
+    $.fn.ajaxRetrySubtitlesSearch = function(options){
+        options = $.extend({}, $.ajaxRetrySubtitlesSearch.defaults, options);
+
+        function downloadSubtitles(){
+            var imageName, imageResult, htmlContent;
+
+            var parent = selectedEpisode.parent();
+
+            // Create var for anchor
+            var link = selectedEpisode;
+
+            // Create var for img under anchor and set options for the loading gif
+            var img = selectedEpisode.children('img');
+            img.prop('title','loading');
+            img.prop('alt','');
+            img.prop('src',srRoot + '/images/' + options.loadingImage);
+
+            var url = selectedEpisode.prop('href');
+            disableLink(link);
+
+            $.getJSON(url, function(data){
+
+                // if they failed then just put the red X
+                if (data.result.toLowerCase() === 'failure') {
+                    imageName = srRoot + '/images/' + options.noImage;
+                    imageResult = 'failed';
+                // if the subtitle was successfully downloaded then apply the corresponding image
+                } else {
+                    imageName = link.children('img').data('image-url');
+                    imageResult = 'success';
+
+                }
+
+                // put the corresponding image as the result of downloading subtitles
+                img.prop('title', imageResult);
+                img.prop('alt', imageResult);
+                img.prop('height', options.size);
+                img.prop('src', imageName);
+
+                //Enable back the link
+                enableLink(link);
+            });
+
+            // don't follow the link
+            return false;
+        }
+
+        $('.epRetrySubtitlesSearch').on('click', function(event){
+            event.preventDefault();
+
+            // Check if we have disabled the click
+            if ($(this).prop('enableClick') === '0') { return false; }
+
+            selectedEpisode = $(this);
+
+            $("#confirmSubtitleDownloadModal").modal('show');
+
+        });
+
+        $('#confirmSubtitleDownloadModal .btn.btn-success').on('click', function(){
+            downloadSubtitles();
+        });
+    };
+
 })();
