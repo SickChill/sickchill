3a1aeb1f48ab7e2f430563cb770c8ddc1a1ee82b
bobbysteel <github@robk.com>
2016-08-08 16:52:46 +0100

    Remove Torrentz

--- sickbeard/providers/__init__.py
+++ sickbeard/providers/__init__.py
@@ -22,7 +22,7 @@ from os import sys
 from random import shuffle
 
 import sickbeard
-from sickbeard.providers import btn, womble, thepiratebay, torrentleech, iptorrents, torrentz, \
+from sickbeard.providers import btn, womble, thepiratebay, torrentleech, iptorrents, \
     omgwtfnzbs, scc, hdtorrents, torrentday, hdbits, hounddawgs, speedcd, nyaatorrents, bluetigers, xthor, abnormal, torrentbytes, cpasbien,\
     freshontv, morethantv, t411, tokyotoshokan, shazbat, rarbg, alpharatio, tntvillage, binsearch, torrentproject, extratorrent, \
     scenetime, btdigg, transmitthenet, tvchaosuk, bitcannon, pretome, gftracker, hdspace, newpct, elitetorrent, bitsnoop, danishbits, hd4free, limetorrents, \
@@ -35,7 +35,7 @@ __all__ = [
     'morethantv', 't411', 'tokyotoshokan', 'alpharatio',
     'shazbat', 'rarbg', 'tntvillage', 'binsearch', 'bluetigers',
     'xthor', 'abnormal', 'scenetime', 'btdigg', 'transmitthenet', 'tvchaosuk',
-    'torrentproject', 'extratorrent', 'bitcannon', 'torrentz', 'pretome', 'gftracker',
+    'torrentproject', 'extratorrent', 'bitcannon', 'pretome', 'gftracker',
     'hdspace', 'newpct', 'elitetorrent', 'bitsnoop', 'danishbits', 'hd4free', 'limetorrents',
     'norbits', 'ilovetorrents'
 ]
