0583eb4bbbdb227fe0b850e43e0ecf83061bf3c2
DutchEngineer <ricky_leeuwtje@hotmail.com>
2016-10-11 12:32:50 +0200

    Update GenericProvider.py (#2461)
    Make black hole working again

--- sickrage/providers/GenericProvider.py
+++ sickrage/providers/GenericProvider.py
@@ -53,7 +53,7 @@ class GenericProvider(object):  # pylint: disable=too-many-instance-attributes
             'http://thetorrent.org/torrent/{torrent_hash}.torrent',
             'http://btdig.com/torrent/{torrent_hash}.torrent',
             # 'http://torrage.com/torrent/{torrent_hash}.torrent',
-            # 'http://itorrents.org/torrent/{torrent_hash}.torrent',
+            'http://itorrents.org/torrent/{torrent_hash}.torrent',
         ]
         self.cache = TVCache(self)
         self.enable_backlog = False
