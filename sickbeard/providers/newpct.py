59626a50fa78c4912fd680e5c0747ccf4601c135
snamds <snamds@users.noreply.github.com>
2016-10-25 10:50:09 +0200

    More cases covered

--- sickbeard/providers/newpct.py
+++ sickbeard/providers/newpct.py
@@ -187,27 +187,55 @@ class newpctProvider(TorrentProvider):
     def _processTitle(title, url):
         # Remove 'Mas informacion sobre ' literal from title
         title = title[22:]
-        title = re.sub(r'( ){2,}', ' ', title, flags=re.I)
+        title = re.sub(r'[ ]{2,}', ' ', title, flags=re.I)
 
         # Quality - Use re module to avoid case sensitive problems with replace
         title = re.sub(r'\[HDTV 1080p?[^\[]*]', '1080p HDTV x264', title, flags=re.I)
         title = re.sub(r'\[HDTV 720p?[^\[]*]', '720p HDTV x264', title, flags=re.I)
-        title = re.sub(r'\[ALTA DEFINICION 720p[^\[]*]', '720p HDTV x264', title, flags=re.I)
+        title = re.sub(r'\[ALTA DEFINICION 720p?[^\[]*]', '720p HDTV x264', title, flags=re.I)
         title = re.sub(r'\[HDTV]', 'HDTV x264', title, flags=re.I)
         title = re.sub(r'\[DVD[^\[]*]', 'DVDrip x264', title, flags=re.I)
-        title = re.sub(r'\[BluRay 1080p[^\[]*]', '1080p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[BluRay 1080p?[^\[]*]', '1080p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[BluRay Rip 1080p?[^\[]*]', '1080p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[BluRay Rip 720p?[^\[]*]', '720p BluRay x264', title, flags=re.I)
         title = re.sub(r'\[BluRay MicroHD[^\[]*]', '1080p BluRay x264', title, flags=re.I)
-        title = re.sub(r'\[MicroHD 1080p[^\[]*]', '1080p BluRay x264', title, flags=re.I)
+        title = re.sub(r'\[MicroHD 1080p?[^\[]*]', '1080p BluRay x264', title, flags=re.I)
         title = re.sub(r'\[BLuRay[^\[]*]', '720p BluRay x264', title, flags=re.I)
         title = re.sub(r'\[BRrip[^\[]*]', '720p BluRay x264', title, flags=re.I)
         title = re.sub(r'\[BDrip[^\[]*]', '720p BluRay x264', title, flags=re.I)
         
-        #detect hdtv 720 by url
-        #hdtv 720 example url: http://www.newpct.com/descargar-seriehd/foo/capitulo-26/hdtv-720p-ac3-5-1/
-        is_hdtv = re.search(r'hdtv', title, flags=re.I) and re.search(r'720p', title, flags=re.I)
-        if not is_hdtv:
-            if re.search(r'hdtv', url, flags=re.I) and re.search(r'720p', url, flags=re.I):
-                title += ' 720p HDTV x264'
+        #detect hdtv/bluray by url
+        #hdtv 1080p example url: http://www.newpct.com/descargar-seriehd/foo/capitulo-610/hdtv-1080p-ac3-5-1/
+        #hdtv 720p example url: http://www.newpct.com/descargar-seriehd/foo/capitulo-26/hdtv-720p-ac3-5-1/
+        #hdtv example url: http://www.newpct.com/descargar-serie/foo/capitulo-214/hdtv/
+        #bluray compilation example url: http://www.newpct.com/descargar-seriehd/foo/capitulo-11/bluray-1080p/
+        title_hdtv = re.search(r'HDTV', title, flags=re.I)
+        title_720p = re.search(r'720p', title, flags=re.I)
+        title_1080p = re.search(r'1080p', title, flags=re.I)
+        title_x264 = re.search(r'x264', title, flags=re.I)
+        title_bluray = re.search(r'bluray', title, flags=re.I)
+        title_serie_hd = re.search(r'descargar\-seriehd', title, flags=re.I)
+        url_hdtv = re.search(r'HDTV', url, flags=re.I)
+        url_720p = re.search(r'720p', url, flags=re.I)
+        url_1080p = re.search(r'1080p', url, flags=re.I)
+        url_bluray = re.search(r'bluray', url, flags=re.I)
+        
+        if not title_hdtv and url_hdtv:
+            title += ' HDTV'
+            if not title_x264:
+                title += ' x264'
+        if not title_bluray and url_bluray:
+            title += ' BluRay'
+            if not title_x264:
+                title += ' x264'
+        if not title_1080p and url_1080p:
+            title += ' 1080p'
+            title_1080p = True
+        if not title_720p and url_720p:
+            title += ' 720p'
+            title_720p = True
+        if not (title_720p or title_1080p) and title_serie_hd:
+            title += ' 720p'
 
         # Language
         title = re.sub(r'\[Spanish[^\[]*]', 'SPANISH AUDIO', title, flags=re.I)
