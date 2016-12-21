72873cee9bf5d259721f7fa2b8ba4f5a6f692be9
miigotu <miigotu@gmail.com>
2016-12-21 06:21:13 -0800

    Fix missed js in trendingShows and other issues, Fixes #2725

--- gui/slick/js/core.js
+++ gui/slick/js/core.js
@@ -3061,6 +3061,45 @@ var SICKRAGE = {
                     }
                 });
             };
+
+            $('#saveDefaultsButton').on('click', function () {
+                var anyQualArray = [];
+                var bestQualArray = [];
+                $('#anyQualities option:selected').each(function (i, d) {
+                    anyQualArray.push($(d).val());
+                });
+                $('#bestQualities option:selected').each(function (i, d) {
+                    bestQualArray.push($(d).val());
+                });
+
+                $.get(srRoot + '/config/general/saveAddShowDefaults', {
+                    defaultStatus: $('#statusSelect').val(),
+                    anyQualities: anyQualArray.join(','),
+                    bestQualities: bestQualArray.join(','),
+                    defaultFlattenFolders: $('#flatten_folders').prop('checked'),
+                    subtitles: $('#subtitles').prop('checked'),
+                    anime: $('#anime').prop('checked'),
+                    scene: $('#scene').prop('checked'),
+                    defaultStatusAfter: $('#statusSelectAfter').val()
+                });
+
+                $(this).attr('disabled', true);
+                new PNotify({
+                    title: 'Saved Defaults',
+                    text: 'Your "add show" defaults have been set to your current selections.',
+                    shadow: false
+                });
+            });
+
+            $('#statusSelect, #qualityPreset, #flatten_folders, #anyQualities, #bestQualities, #subtitles, #scene, #anime, #statusSelectAfter').change(function () {
+                $('#saveDefaultsButton').attr('disabled', false);
+            });
+
+            $('#qualityPreset').on('change', function() {
+                //fix issue #181 - force re-render to correct the height of the outer div
+                $('span.prev').click();
+                $('span.next').click();
+            });
         },
         index: function() {
 
