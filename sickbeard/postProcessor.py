28aa3d8ede82a03b1a0c4e99fe52a357ae77a3d8
h3llrais3r <pooh_beer_1@hotmail.com>
2017-02-21 21:32:06 +0100

    Rework on post processing logic
    Add separate flag to move associated files and to delete non associated
    files

--- sickbeard/postProcessor.py
+++ sickbeard/postProcessor.py
@@ -162,6 +162,7 @@ class PostProcessor(object):  # pylint: disable=too-many-instance-attributes
             return []
 
         file_path_list = []
+        extensions_to_allow = []
         extensions_to_delete = []
 
         if subfolders:
@@ -207,8 +208,15 @@ class PostProcessor(object):  # pylint: disable=too-many-instance-attributes
             if re.search(r'(^.+\.(rar|r\d+)$)', associated_file_path):
                 continue
 
-            # Add the extensions that the user doesn't allow to the 'extensions_to_delete' list
+            # Add the extensions that the user allows to the 'extensions_to_allow' list
             if sickbeard.MOVE_ASSOCIATED_FILES:
+                allowed_extensions = sickbeard.ALLOWED_EXTENSIONS.split(",")
+                if associated_file_path.rpartition('.')[2] in allowed_extensions:
+                    if ek(os.path.isfile, associated_file_path):
+                        extensions_to_allow.append(associated_file_path)
+
+            # Add the extensions that the user doesn't allow to the 'extensions_to_delete' list
+            if sickbeard.DELETE_NON_ASSOCIATED_FILES:
                 allowed_extensions = sickbeard.ALLOWED_EXTENSIONS.split(",")
                 if not associated_file_path.rpartition('.')[2] in allowed_extensions:
                     if ek(os.path.isfile, associated_file_path):
@@ -219,9 +227,13 @@ class PostProcessor(object):  # pylint: disable=too-many-instance-attributes
 
         if file_path_list:
             self._log(u"Found the following associated files for {0}: {1}".format(file_path, file_path_list), logger.DEBUG)
-            if extensions_to_delete:
+            if extensions_to_allow:
+                self._log(u"Associated files to allow for {0}: {1}".format(file_path, extensions_to_allow), logger.DEBUG)
                 # Rebuild the 'file_path_list' list only with the extensions the user allows
-                file_path_list = [associated_file for associated_file in file_path_list if associated_file not in extensions_to_delete]
+                file_path_list = extensions_to_allow
+            if extensions_to_delete:
+                self._log(u"Associated files to delete for {0}: {1}".format(file_path, extensions_to_delete), logger.DEBUG)
+                # Delete all extensions the user doesn't allow
                 self._delete(extensions_to_delete)
         else:
             self._log(u"No associated files for {0} were found during this pass".format(file_path), logger.DEBUG)
