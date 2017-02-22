441bdbffce032be72526537042ac654cf07f1ad6
Aikurn <aikurn@gmail.com>
2017-02-22 11:51:16 +0100

    Add "Scrambled" to removeWordsList
    Add the suffix "Scrambled" to the remove words list as it has become popular on Usenet files.

--- sickbeard/helpers.py
+++ sickbeard/helpers.py
@@ -158,7 +158,8 @@ def remove_non_release_groups(name):
         r'- \[ www\.torrentday\.com \]$': 'searchre',
         r'^\[ www\.TorrentDay\.com \] - ': 'searchre',
         r'\[NO-RAR\] - \[ www\.torrentday\.com \]$': 'searchre',
-        r'^www\.Torrenting\.com\.-\.': 'searchre'
+        r'^www\.Torrenting\.com\.-\.': 'searchre',
+        r'-Scrambled$': 'searchre'
     }
 
     _name = name
