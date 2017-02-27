3c5ee884a440622f1cd0220f416d3d62fc854f2a
sharkykh <sharkykh@gmail.com>
2017-02-27 22:21:55 +0200

    Fix pp_tests.py in order to fix Travis build.
    and remove old comment from list_associated_files

--- tests/pp_tests.py
+++ tests/pp_tests.py
@@ -106,6 +106,8 @@ class ListAssociatedFiles(unittest.TestCase):
         self.file_list = [os.path.join('Show Name', f) for f in file_names] + [os.path.join(self.test_tree, f) for f in file_names]
         self.post_processor = PostProcessor('Show Name')
         self.maxDiff = None
+        sickbeard.MOVE_ASSOCIATED_FILES = True
+        sickbeard.ALLOWED_EXTENSIONS = u''
 
     def setUp(self):
         make_dirs(self.test_tree)
@@ -116,12 +118,31 @@ class ListAssociatedFiles(unittest.TestCase):
         shutil.rmtree('Show Name')
 
     def test_subfolders(self):
+        # Test edge cases first:
+        self.assertEqual([], # empty file_path
+            self.post_processor.list_associated_files('', subfolders=True))
+        self.assertEqual([], # no file name
+                         self.post_processor.list_associated_files('\\Show Name\\.nomedia', subfolders=True))
+
         associated_files = self.post_processor.list_associated_files(self.file_list[0], subfolders=True)
 
         associated_files = sorted(file_name.lstrip('./') for file_name in associated_files)
         out_list = sorted(file_name for file_name in self.file_list[1:] if 'Non-Associated' not in file_name)
 
         self.assertEqual(out_list, associated_files)
+
+        # Test no associated files:
+        associated_files = self.post_processor.list_associated_files('Fools Quest.avi', subfolders=True)
+
+        # Test delete non-associated files
+        value_keep = sickbeard.DELETE_NON_ASSOCIATED_FILES
+        sickbeard.DELETE_NON_ASSOCIATED_FILES = True
+        sickbeard.ALLOWED_EXTENSIONS = u'srt'
+        associated_files = self.post_processor.list_associated_files(self.file_list[0], subfolders=True)
+        out_list = sorted(file_name for file_name in self.file_list[1:] if '.srt' in file_name and 'Non-Associated' not in file_name)
+        self.assertEqual(out_list, associated_files)
+        sickbeard.DELETE_NON_ASSOCIATED_FILES = value_keep
+        sickbeard.ALLOWED_EXTENSIONS = u''
 
     def test_no_subfolders(self):
         associated_files = self.post_processor.list_associated_files(self.file_list[0], subfolders=False)
