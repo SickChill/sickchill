e51e2ee8b92ce0544d63dadea76efedab8ce7030
sharkykh <sharkykh@gmail.com>
2017-03-14 05:04:23 +0200

    Honor sickbeard.SORT_ARTICLE in Mass Update and History tables.
    Moved massUpdate.js into core.js

--- gui/slick/js/core.js
+++ gui/slick/js/core.js
@@ -2983,7 +2983,7 @@ var SICKRAGE = {
                 widgets: ['zebra', 'filter', 'columnSelector'],
                 headers: {
                     0: { sorter: false, filter: false},
-                    1: { sorter: 'showNames'},
+                    1: { sorter: 'loadingNames'},
                     2: { sorter: 'network'},
                     3: { sorter: 'quality'},
                     4: { sorter: 'sports'},
@@ -3012,6 +3012,113 @@ var SICKRAGE = {
             }).on('shown.bs.popover', function () { // bootstrap popover event triggered when the popover opens
                 // call this function to copy the column selection code into the popover
                 $.tablesorter.columnSelector.attachTo( $('#massUpdateTable'), '#popover-target');
+            });
+
+            $('.submitMassEdit').on('click', function(){
+                var editArr = [];
+
+                $('.editCheck').each(function() {
+                    if(this.checked === true) { editArr.push($(this).attr('id').split('-')[1]); }
+                });
+
+                if(editArr.length === 0) { return; }
+
+                var submitForm = $(
+                    "<form method='post' action='" + srRoot + "/manage/massEdit'>" +
+                        "<input type='hidden' name='toEdit' value='" + editArr.join('|') + "'/>" +
+                    "</form>"
+                );
+                submitForm.appendTo('body');
+
+                submitForm.submit();
+            });
+
+            $('.submitMassUpdate').on('click', function(){
+                var updateArr = [];
+                var refreshArr = [];
+                var renameArr = [];
+                var subtitleArr = [];
+                var deleteArr = [];
+                var removeArr = [];
+                var metadataArr = [];
+
+                $('.updateCheck').each(function() {
+                    if(this.checked === true) { updateArr.push($(this).attr('id').split('-')[1]); }
+                });
+
+                $('.refreshCheck').each(function() {
+                    if(this.checked === true) { refreshArr.push($(this).attr('id').split('-')[1]); }
+                });
+
+                $('.renameCheck').each(function() {
+                    if(this.checked === true) { renameArr.push($(this).attr('id').split('-')[1]); }
+                });
+
+                $('.subtitleCheck').each(function() {
+                    if(this.checked === true) { subtitleArr.push($(this).attr('id').split('-')[1]); }
+                });
+
+                $('.removeCheck').each(function() {
+                    if(this.checked === true) { removeArr.push($(this).attr('id').split('-')[1]); }
+                });
+
+                var deleteCount = 0;
+
+                $('.deleteCheck').each(function() {
+                    if(this.checked === true) { deleteCount++; }
+                });
+
+                if(deleteCount >= 1) {
+                    $.confirm({
+                        title: "Delete Shows",
+                        text: "You have selected to delete " + deleteCount + " show(s).  Are you sure you wish to continue? All files will be removed from your system.",
+                        confirmButton: "Yes",
+                        cancelButton: "Cancel",
+                        dialogClass: "modal-dialog",
+                        post: false,
+                        confirm: function() {
+                            $('.deleteCheck').each(function() {
+                                if(this.checked === true) {
+                                    deleteArr.push($(this).attr('id').split('-')[1]);
+                                }
+                            });
+                            if(updateArr.length + refreshArr.length + renameArr.length + subtitleArr.length + deleteArr.length + removeArr.length + metadataArr.length === 0) {
+                                return false;
+                            }
+                            var url = srRoot + '/manage/massUpdate';
+                            var params = 'toUpdate='+updateArr.join('|')+'&toRefresh='+refreshArr.join('|')+'&toRename='+renameArr.join('|')+'&toSubtitle='+subtitleArr.join('|')+'&toDelete='+deleteArr.join('|')+'&toRemove='+removeArr.join('|')+'&toMetadata='+metadataArr.join('|');
+                            $.post(url, params, function() { location.reload(true); });
+                        }
+                    });
+                }
+                if(updateArr.length + refreshArr.length + renameArr.length + subtitleArr.length + deleteArr.length + removeArr.length + metadataArr.length === 0) {
+                    return false;
+                }
+                var url = srRoot + '/manage/massUpdate';
+                var params = 'toUpdate='+updateArr.join('|')+'&toRefresh='+refreshArr.join('|')+'&toRename='+renameArr.join('|')+'&toSubtitle='+subtitleArr.join('|')+'&toDelete='+deleteArr.join('|')+'&toRemove='+removeArr.join('|')+'&toMetadata='+metadataArr.join('|');
+                $.post(url, params, function() { location.reload(true); });
+            });
+
+            ['.editCheck', '.updateCheck', '.refreshCheck', '.renameCheck', '.deleteCheck', '.removeCheck'].forEach(function(name) {
+                var lastCheck = null;
+
+                $(name).on('click', function(event) {
+                    if(!lastCheck || !event.shiftKey) {
+                        lastCheck = this;
+                        return;
+                    }
+
+                    var check = this;
+                    var found = 0;
+
+                    $(name).each(function() {
+                        switch (found) {
+                            case 2: return false;
+                            case 1: if(!this.disabled) { this.checked = lastCheck.checked; }
+                        }
+                        if(this === check || this === lastCheck) { found++; }
+                    });
+                });
             });
         },
         backlogOverview: function() {
@@ -3251,6 +3358,7 @@ var SICKRAGE = {
                     if(isMeta('sickbeard.HISTORY_LAYOUT', ['detailed'])){
                         return {
                             0: { sorter: 'realISODate' },
+                            1: { sorter: 'loadingNames' },
                             4: { sorter: 'quality' },
                             5: { sorter: false, filter: false}
                         };
@@ -3258,6 +3366,7 @@ var SICKRAGE = {
                         if(isMeta('sickbeard.USE_SUBTITLES', ['True'])) {
                             return {
                                 0: { sorter: 'realISODate' },
+                                1: { sorter: 'loadingNames' },
                                 4: { sorter: false },
                                 5: { sorter: 'quality' },
                                 6: { sorter: false, filter: false }
@@ -3265,6 +3374,7 @@ var SICKRAGE = {
                         } else {
                             return {
                                 0: { sorter: 'realISODate' },
+                                1: { sorter: 'loadingNames' },
                                 4: { sorter: 'quality' },
                                 5: { sorter: false, filter: false }
                             };
