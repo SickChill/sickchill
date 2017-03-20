f2e84c185a15c6b42839dda48cf34c5e7b40048c
miigotu <miigotu@gmail.com>
2017-03-20 11:18:59 -0700

    Fix auto pushing changelog commit
    Signed-off-by: miigotu <miigotu@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -267,8 +267,7 @@ module.exports = function(grunt) {
                         grunt.fatal('Missing file path.');
                     }
                     var path = file.substr(0, file.lastIndexOf('/', file.length-12)); // get sickrage.github.io folder
-                    return 'cd ' + path +
-                           ' && git commit -asm "Update changelog"';
+                    return 'cd ' + path + ' && git commit -asm "Update changelog" && git push origin master';
                 },
                 stdout: true
             }
@@ -292,7 +291,7 @@ module.exports = function(grunt) {
         }
         grunt.config('changesmd_file', file);
         grunt.task.run(['exec:git_get_last_tag', 'exec:git_list_tags', '_genchanges',
-                        'exec:commit_changelog', 'exec:git_push:origin:master']);
+                        'exec:commit_changelog']);
     });
 
     /****************************************
