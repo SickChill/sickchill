8b368875c395b3279e0e68248e9e1c5f2c351828
sharkykh <sharkykh@gmail.com>
2017-03-22 10:47:14 +0200

    Revert more bad bad consecutive ifs fixes

--- sickbeard/providers/newznab.py
+++ sickbeard/providers/newznab.py
@@ -346,8 +346,8 @@ class NewznabProvider(NZBProvider):  # pylint: disable=too-many-instance-attribu
                                 elif validators.url(item.link.next.strip(), require_tld=False):
                                     download_url = item.link.next.strip()
 
-                            if all([not download_url, item.enclosure,
-                                validators.url(item.enclosure.get('url', '').strip(), require_tld=False)]):
+                            if (not download_url, item.enclosure and
+                                    validators.url(item.enclosure.get('url', '').strip(), require_tld=False)):
                                 download_url = item.enclosure.get('url', '').strip()
 
                             if not (title and download_url):
