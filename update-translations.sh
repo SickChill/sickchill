3bda0d1b6056b75f1e53a6c9fc3afd90214ff4fe
sharkykh <sharkykh@gmail.com>
2017-03-31 10:50:42 +0300

    This might be better,
    Tabs -> Spaces

--- update-translations.sh
+++ update-translations.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" == "develop" ]; then # either the pushed branch or the pull request base branch
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" != "master" ]; then
+
   if [ -z "$CROWDIN_API_KEY" ]; then
     echo -e "\$CROWDIN_API_KEY is undefined or empty"
     # exit 1
   fi
-  
+
   echo -e "[ 1/10] Installing babel, mako and crowdin-cli-py"
   pip install --upgrade babel mako crowdin-cli-py > /dev/null
   if [ $? != 0 ]; then
     echo -e "-!- An error has occurred."
-	exit 1
+    exit 1
   fi
-  
+
   echo -e "[ 2/10] Configuring git..."
   git config --global user.name "SickRage"
   git config --global user.email sickrage2@gmail.com
@@ -22,48 +23,48 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then # either the pushed branch or the pul
   python setup.py extract_messages > /dev/null
   if [ $? != 0 ]; then
     echo -e "-!- An error has occurred."
-	exit 1
+    exit 1
   fi
-  
+
   echo -e "[ 4/10] Updating translations..."
   python setup.py update_catalog > /dev/null
   if [ $? != 0 ]; then
     echo -e "-!- An error has occurred."
-	exit 1
+    exit 1
   fi
-  
+
   echo -e "[ 5/10] Uploading to Crowdin..."
   crowdin-cli-py upload sources #> /dev/null
   if [ $? != 0 ]; then
     echo -e "-!- An error has occurred."
-	# exit 1
+    exit 1
   fi
   
   echo -e "[ 6/10] Downloading Crowdin..."
   crowdin-cli-py download #> /dev/null
   if [ $? != 0 ]; then
     echo -e "-!- An error has occurred."
-	# exit 1
+    exit 1
   fi
   
   echo -e "[ 7/10] Compiling translations..."
   python setup.py compile_catalog > /dev/null
   if [ $? != 0 ]; then
     echo -e "-!- An error has occurred."
-	exit 1
+    exit 1
   fi
   
   echo -e "[ 8/10] Converting translations to json..."
   grunt po2json > /dev/null
   if [ $? != 0 ]; then
     echo -e "-!- An error has occurred."
-	exit 1
+    exit 1
   fi
 
   git diff-index --quiet HEAD -- locale/ # check if locale files have actually changed
   if [ $? == 0 ]; then # check return value, 0 is clean, otherwise is dirty
-	echo -e "No need to update translations."
-	exit 1
+    echo -e "No need to update translations."
+    exit 1
   fi
 
   echo -e "[ 9/10] Commiting translations..."
@@ -73,16 +74,11 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then # either the pushed branch or the pul
   # git commit -q -m "Update translations (build $TRAVIS_BUILD_NUMBER)" -- locale/
   git commit --dry-run -m "Update translations (build $TRAVIS_BUILD_NUMBER)" -- locale/
 
-  if [ "$TRAVIS_PULL_REQUEST" == "true" ] && [ "$TRAVIS_PULL_REQUEST_SLUG" == "SickRage/SickRage" ]; then
-    echo -e "[10/10] Pushing translations to $TRAVIS_PULL_REQUEST_BRANCH branch"
-    git push --dry-run -f origin $TRAVIS_PULL_REQUEST_BRANCH > /dev/null
-  else
-    echo -e "[10/10] Pushing translations to develop branch"
-    git push --dry-run -f origin develop > /dev/null
-  fi
+  echo -e "[10/10] Pushing translations to develop branch"
+  git push --dry-run -f origin develop > /dev/null
   if [ $? != 0 ]; then
     echo -e "-!- An error has occurred."
-	exit 1
+    exit 1
   fi
   
   echo -e "Done!"
