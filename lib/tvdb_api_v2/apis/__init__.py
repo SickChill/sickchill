c080570032e92ffbd5ea0ecdde95ad4f52151ff9
sharkykh <sharkykh@gmail.com>
2017-04-01 03:18:35 +0300

    Initial commit for tvdb_api_v2 (original tests included)

---
+++ lib/tvdb_api_v2/apis/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .authentication_api import AuthenticationApi
+from .episodes_api import EpisodesApi
+from .languages_api import LanguagesApi
+from .search_api import SearchApi
+from .series_api import SeriesApi
+from .updates_api import UpdatesApi
+from .users_api import UsersApi
