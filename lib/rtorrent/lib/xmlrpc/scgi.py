2ebbd0c739c13b93606abac5904215ed3b6bc7dd
josef radinger <cheese@nosuchhost.net>
2017-05-01 20:51:46 +0200

    FSF address fixed

--- lib/rtorrent/lib/xmlrpc/scgi.py
+++ lib/rtorrent/lib/xmlrpc/scgi.py
@@ -36,7 +36,7 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
 #
 # In addition, as a special exception, the copyright holders give
 # permission to link the code of portions of this program with the
