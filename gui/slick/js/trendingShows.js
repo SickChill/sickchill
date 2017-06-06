7b4984d7f3c5c9d24a1592c6e704a7b71f547d39
Alexis Tyler <xo@wvvw.me>
2017-06-06 14:41:18 +0930

    lint trendingShows

--- gui/slick/js/trendingShows.js
+++ gui/slick/js/trendingShows.js
@@ -1,36 +1,31 @@
-$(document).ready(function(){
-    // initialise combos for dirty page refreshes
+$(document).ready(() => {
+    // Initialise combos for dirty page refreshes
     $('#showsort').val('original');
     $('#showsortdirection').val('asc');
 
-    var $container = [$('#container')];
-    $.each($container, function () {
-        this.isotope({
-            itemSelector: '.trakt_show',
-            sortBy: 'original-order',
-            layoutMode: 'fitRows',
-            getSortData: {
-                name: function(itemElem) {
-                    var name = $(itemElem).attr('data-name') || '';
-                    return (metaToBool('sickbeard.SORT_ARTICLE') ? name : name.replace(/^(The|A|An)\s/i, '')).toLowerCase();
-                },
-                rating: '[data-rating] parseInt',
-                votes: '[data-votes] parseInt'
-            }
-        });
+    $('#container').isotope({
+        itemSelector: '.trakt_show',
+        sortBy: 'original-order',
+        layoutMode: 'fitRows',
+        getSortData: {
+            name(itemElem) {
+                const name = $(itemElem).attr('data-name') || '';
+                return (metaToBool('sickbeard.SORT_ARTICLE') ? name : name.replace(/^(The|A|An)\s/i, '')).toLowerCase();
+            },
+            rating: '[data-rating] parseInt',
+            votes: '[data-votes] parseInt'
+        }
     });
 
     $('#showsort').on('change', function() {
-        var sortCriteria;
+        let sortCriteria;
         switch (this.value) {
             case 'original':
                 sortCriteria = 'original-order';
                 break;
             case 'rating':
-                /* randomise, else the rating_votes can already
-                 * have sorted leaving this with nothing to do.
-                 */
-                $('#container').isotope({ sortBy: 'random' });
+                // Randomise, else the rating_votes can already have sorted leaving this with nothing to do.
+                $('#container').isotope({sortBy: 'random'});
                 sortCriteria = 'rating';
                 break;
             case 'rating_votes':
@@ -43,12 +38,12 @@ $(document).ready(function(){
                 sortCriteria = 'name';
                 break;
         }
-        $('#container').isotope({ sortBy: sortCriteria });
+        $('#container').isotope({sortBy: sortCriteria});
     });
 
     $('#showsortdirection').on('change', function() {
         $('#container').isotope({
-            sortAscending: ('asc' === this.value)
+            sortAscending: this.value === 'asc'
         });
     });
 });
