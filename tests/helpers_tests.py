SickChill/sickchill

commit 8c4278a52bf30a02914aa85c9b9ba5ad61021bea

Restore `bencode` library to vanilla state (#3858)
* Restore lib `bencode` to vanilla state

* Update libs list

* Handle 'data after valid prefix' exception where bencode is used

sickbeard/clients/generic.py
@@ -214,10 +214,15 @@ class GenericClient(object):  # pylint: disable=too-many-instance-attributes
             try:
                 torrent_bdecode = bencode.bdecode(result.content)
             except (bencode.BTL.BTFailure, Exception) as error:
-                logger.log('Unable to bdecode torrent', logger.ERROR)
-                logger.log('Error is: {0}'.format(error), logger.DEBUG)
-                # logger.log('Torrent bencoded data: {0!r}'.format(result.content), logger.DEBUG)
-                raise
+                if (isinstance(error, bencode.BTL.BTFailure) and 'data after valid prefix' in error.message):
+                    # Skip this specific exception
+                    pass
+                else:
+                    logger.log('Unable to bdecode torrent', logger.ERROR)
+                    logger.log('Error is: {0}'.format(error), logger.DEBUG)
+                    # logger.log('Torrent bencoded data: {0!r}'.format(result.content), logger.DEBUG)
+                    raise
+
             try:
                 info = torrent_bdecode[b'info']
             except Exception:

sickbeard/providers/rsstorrent.py
@@ -174,8 +174,12 @@ class TorrentRssProvider(TorrentProvider):  # pylint: disable=too-many-instance-
                 try:
                     bencode.bdecode(torrent_file)
                 except (bencode.BTL.BTFailure, Exception) as error:
-                    self.dumpHTML(torrent_file)
-                    return False, 'Torrent link is not a valid torrent file: {0}'.format(error)
+                    if (isinstance(error, bencode.BTL.BTFailure) and 'data after valid prefix' in error.message):
+                        # Skip this specific exception
+                        pass
+                    else:
+                        self.dumpHTML(torrent_file)
+                        return False, 'Torrent link is not a valid torrent file: {0}'.format(error)
 
             return True, 'RSS feed Parsed correctly'
 

lib/bencode/__init__.py
@@ -66,8 +66,8 @@ def bdecode(x):
         r, l = decode_func[x[0]](x, 0)
     except (IndexError, KeyError, ValueError):
         raise BTFailure("not a valid bencoded string")
-    # if l != len(x):
-    #     raise BTFailure("invalid bencoded value (data after valid prefix)")
+    if l != len(x):
+        raise BTFailure("invalid bencoded value (data after valid prefix)")
     return r
 
 from types import StringType, IntType, LongType, DictType, ListType, TupleType

commit 56a11d19ab9be1f35068c8c959ddf2a7a94a4e2d

Fix bdecode regression (#3871)
* Fix bencode.bdecode regression from #3858

* Add tests

sickbeard/clients/generic.py
@@ -212,16 +212,12 @@ class GenericClient(object):  # pylint: disable=too-many-instance-attributes
                 raise Exception('Torrent without content')
 
             try:
-                torrent_bdecode = bencode.bdecode(result.content)
+                torrent_bdecode = helpers.bdecode(result.content, True)
             except (bencode.BTL.BTFailure, Exception) as error:
-                if (isinstance(error, bencode.BTL.BTFailure) and 'data after valid prefix' in error.message):
-                    # Skip this specific exception
-                    pass
-                else:
-                    logger.log('Unable to bdecode torrent', logger.ERROR)
-                    logger.log('Error is: {0}'.format(error), logger.DEBUG)
-                    # logger.log('Torrent bencoded data: {0!r}'.format(result.content), logger.DEBUG)
-                    raise
+                logger.log('Unable to bdecode torrent', logger.ERROR)
+                logger.log('Error is: {0}'.format(error), logger.DEBUG)
+                # logger.log('Torrent bencoded data: {0!r}'.format(result.content), logger.DEBUG)
+                raise
 
             try:
                 info = torrent_bdecode[b'info']

sickbeard/helpers.py
@@ -45,6 +45,7 @@ from contextlib import closing
 from itertools import cycle, izip
 
 import adba
+import bencode
 import certifi
 import cfscrape
 import rarfile
@@ -1906,3 +1907,19 @@ def manage_torrents_url(reset=False):
     sickbeard.CLIENT_WEB_URLS['torrent'] = ('', torrent_ui_url)[test_exists(torrent_ui_url)]
 
     return sickbeard.CLIENT_WEB_URLS.get('torrent')
+
+
+def bdecode(x, allow_extra_data=False):
+    """
+    Custom bdecode function to ignore the 'data after valid prefix' exception.
+
+    :param allow_extra_data: Set to True to allow extra data after valid prefix
+    :return: bdecoded data
+    """
+    try:
+        r, l = bencode.decode_func[x[0]](x, 0)
+    except (IndexError, KeyError, ValueError):
+        raise bencode.BTL.BTFailure("not a valid bencoded string")
+    if not allow_extra_data and l != len(x):
+        raise bencode.BTL.BTFailure("invalid bencoded value (data after valid prefix)")
+    return r

sickbeard/providers/rsstorrent.py
@@ -24,8 +24,8 @@ import io
 import os
 import re
 
-import bencode
 import sickbeard
+from bencode.BTL import BTFailure
 from requests.utils import add_dict_to_cookiejar
 from sickbeard import helpers, logger, tvcache
 from sickrage.helper.encoding import ek
@@ -172,14 +172,10 @@ class TorrentRssProvider(TorrentProvider):  # pylint: disable=too-many-instance-
             else:
                 torrent_file = self.get_url(url, returns='content')
                 try:
-                    bencode.bdecode(torrent_file)
-                except (bencode.BTL.BTFailure, Exception) as error:
-                    if (isinstance(error, bencode.BTL.BTFailure) and 'data after valid prefix' in error.message):
-                        # Skip this specific exception
-                        pass
-                    else:
-                        self.dumpHTML(torrent_file)
-                        return False, 'Torrent link is not a valid torrent file: {0}'.format(error)
+                    helpers.bdecode(torrent_file, True)
+                except (BTFailure, Exception) as error:
+                    self.dumpHTML(torrent_file)
+                    return False, 'Torrent link is not a valid torrent file: {0}'.format(error)
 
             return True, 'RSS feed Parsed correctly'
 

tests/helpers_tests.py
@@ -79,6 +79,9 @@ Public Methods:
     pretty_time_delta
     is_file_locked
     disk_usage
+    sortable_name
+    manage_torrents_url
+    bdecode
 Private Methods:
     _check_against_names
     _setUpSession
@@ -95,6 +98,8 @@ from shutil import rmtree
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib')))
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import sickbeard
+from bencode.BTL import BTFailure
 from sickbeard import helpers
 from sickrage.helper import MEDIA_EXTENSIONS, SUBTITLE_EXTENSIONS
 
@@ -761,6 +766,40 @@ class HelpersMiscTests(unittest.TestCase):
         Test pretty_time_delta
         """
         pass
+
+    def test_sortable_name(self):
+        """
+        Test that sortable_name returns the correct show name
+        """
+        cases = [
+            # raw_name, SORT_ARTICLE, expected
+            ('The Big Bang Theory', False, 'big bang theory'),
+            ('A Big World', False, 'big world'),
+            ('An Unexpected Journey', False, 'unexpected journey'),
+            ('The Big Bang Theory', True, 'the big bang theory'),
+            ('A Big World', True, 'a big world'),
+            ('An Unexpected Journey', True, 'an unexpected journey'),
+        ]
+        for raw_name, option, expected in cases:
+            sickbeard.SORT_ARTICLE = option
+            self.assertEqual(helpers.sortable_name(raw_name), expected)
+
+    @unittest.skip('Not yet implemented')
+    def test_manage_torrents_url(self):
+        """
+        Test manage_torrents_url
+        """
+        pass
+
+    def test_bdecode(self):
+        """
+        Test the custom bdecode function
+        """
+        bencoded_data = b'd5:hello5:world7:numbersli1ei2eeeEXTRA_DATA_HERE'
+        self.assertEqual(helpers.bdecode(bencoded_data, True), {'hello': b'world', 'numbers': [1, 2]})
+        self.assertRaisesRegexp(BTFailure, 'data after valid prefix', helpers.bdecode, bencoded_data, False)
+        self.assertRaisesRegexp(BTFailure, 'not a valid bencoded string', helpers.bdecode, b'Heythere', False)
+
 
 if __name__ == '__main__':
     print("==================")