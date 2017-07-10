20237230f7132c3f7be1e141c3f1cd96363261c3
Alexis Tyler <xo@wvvw.me>
2017-07-10 17:29:15 +0930

    lint ajaxEpSearch.js

--- gui/slick/js/ajaxEpSearch.js
+++ gui/slick/js/ajaxEpSearch.js
@@ -1,7 +1,7 @@
-var searchStatusUrl = srRoot + '/home/getManualSearchStatus';
-var failedDownload = false;
-var qualityDownload = false;
-var selectedEpisode = '';
+const searchStatusUrl = srRoot + '/home/getManualSearchStatus';
+let failedDownload = false;
+let qualityDownload = false;
+let selectedEpisode = '';
 PNotify.prototype.options.maxonscreen = 5;
 
 $.fn.manualSearches = [];
@@ -9,104 +9,102 @@ $.fn.manualSearches = [];
 function enableLink(link) {
     link.on('click.disabled', false);
     link.prop('enableClick', '1');
-    link.fadeTo("fast", 1);
+    link.fadeTo('fast', 1);
 }
 
 function disableLink(link) {
     link.off('click.disabled');
     link.prop('enableClick', '0');
-    link.fadeTo("fast", 0.5);
+    link.fadeTo('fast', 0.5);
 }
 
 function updateImages(data) {
-    $.each(data.episodes, function (name, ep) {
+    $.each(data.episodes, (name, ep) => {
         // Get td element for current ep
-        var loadingClass = 'loading-spinner16';
-        var queuedClass = 'displayshow-icon-clock';
-        var searchClass = 'displayshow-icon-search';
+        const loadingClass = 'loading-spinner16';
+        const queuedClass = 'displayshow-icon-clock';
+        const searchClass = 'displayshow-icon-search';
 
-        //Try to get the <a> Element
-        var link = $('a[id=' + ep.show + 'x' + ep.season + 'x' + ep.episode+']');
+        // Try to get the <a> Element
+        const link = $('a[id=' + ep.show + 'x' + ep.season + 'x' + ep.episode + ']');
         if (link) {
-            var icon = link.children('span');
-            var parent = link.parent();
+            const icon = link.children('span');
+            const parent = link.parent();
 
-            var rSearchTerm = '';
-            var htmlContent = '';
+            let rSearchTerm = '';
+            let htmlContent = '';
 
             if (ep.searchstatus.toLowerCase() === 'searching') {
                 icon.prop('class', loadingClass);
-                icon.prop('title','Searching');
-                icon.prop('alt','Searching');
+                icon.prop('title', 'Searching');
+                icon.prop('alt', 'Searching');
 
                 disableLink(link);
                 htmlContent = ep.searchstatus.title;
-
             } else if (ep.searchstatus.toLowerCase() === 'queued') {
-                icon.prop('class', queuedClass );
-                icon.prop('title','Queued');
-                icon.prop('alt','Queued');
+                icon.prop('class', queuedClass);
+                icon.prop('title', 'Queued');
+                icon.prop('alt', 'Queued');
 
                 disableLink(link);
                 htmlContent = ep.searchstatus;
-
             } else if (ep.searchstatus.toLowerCase() === 'finished') {
                 icon.prop('class', searchClass);
-                if (ep.quality !== "N/A") {
-                    link.prop('class','epRetry');
+                if (ep.quality !== 'N/A') {
+                    link.prop('class', 'epRetry');
                 }
-                icon.prop('title','Search');
-                icon.prop('alt','Search');
+                icon.prop('title', 'Search');
+                icon.prop('alt', 'Search');
 
                 enableLink(link);
 
                 // Update Status and Quality
                 rSearchTerm = /(\w+)\s\((.+?)\)/;
-                htmlContent = ep.status.replace(rSearchTerm,"$1"+' <span class="quality '+ep.quality+'">'+"$2"+'</span>');
-                parent.closest('tr').prop("class", ep.overview + " season-" + ep.season + " seasonstyle");
+                htmlContent = ep.status.replace(rSearchTerm, '$1 <span class="quality ' + ep.quality + '">$2</span>');
+                parent.closest('tr').prop('class', ep.overview + ' season-' + ep.season + ' seasonstyle');
             }
-            // update the status column if it exists
+            // Update the status column if it exists
             parent.siblings('.col-status').html(htmlContent);
-            // and location
+            // And location
             parent.siblings('.location').html(ep.location);
-            // and size
+            // And size
             parent.siblings('.size').html(ep.size);
-            // and qtip location
+            // And qtip location
             if (ep.location) {
-                parent.siblings('.episode').html('<span title="' + ep.location + '" class="addQTip">' + ep.episode + "</span>");
+                parent.siblings('.episode').html('<span title="' + ep.location + '" class="addQTip">' + ep.episode + '</span>');
             }
         }
-        var elementCompleteEpisodes = $('a[id=forceUpdate-' + ep.show + 'x' + ep.season + 'x' + ep.episode+']');
-        var spanCompleteEpisodes = elementCompleteEpisodes.children('span');
+        const elementCompleteEpisodes = $('a[id=forceUpdate-' + ep.show + 'x' + ep.season + 'x' + ep.episode + ']');
+        const spanCompleteEpisodes = elementCompleteEpisodes.children('span');
         if (elementCompleteEpisodes) {
             if (ep.searchstatus.toLowerCase() === 'searching') {
                 spanCompleteEpisodes.prop('class', loadingClass);
-                spanCompleteEpisodes.prop('title','Searching');
-                spanCompleteEpisodes.prop('alt','Searching');
+                spanCompleteEpisodes.prop('title', 'Searching');
+                spanCompleteEpisodes.prop('alt', 'Searching');
                 disableLink(elementCompleteEpisodes);
             } else if (ep.searchstatus.toLowerCase() === 'queued') {
                 spanCompleteEpisodes.prop('class', queuedClass);
-                spanCompleteEpisodes.prop('title','Queued');
-                spanCompleteEpisodes.prop('alt','Queued');
+                spanCompleteEpisodes.prop('title', 'Queued');
+                spanCompleteEpisodes.prop('alt', 'Queued');
                 disableLink(elementCompleteEpisodes);
             } else if (ep.searchstatus.toLowerCase() === 'finished') {
                 spanCompleteEpisodes.prop('class', searchClass);
-                spanCompleteEpisodes.prop('title','Search');
-                spanCompleteEpisodes.prop('alt','Search');
+                spanCompleteEpisodes.prop('title', 'Search');
+                spanCompleteEpisodes.prop('alt', 'Search');
                 if (ep.overview.toLowerCase() === 'snatched') {
                     // Find Banner or Poster
-                    var actionElement = elementCompleteEpisodes.closest('div.ep_listing');
+                    let actionElement = elementCompleteEpisodes.closest('div.ep_listing');
                     if (actionElement.length === 0) {
                         if (elementCompleteEpisodes.closest('table.calendarTable').length === 0) {
                             // List view
                             actionElement = elementCompleteEpisodes.closest('tr');
                         }
-                        // else - Calendar view is ignored
+                        // Else - Calendar view is ignored
                     }
 
                     if (actionElement.length) {
-                        // remove any listing-* classes and add listing-snatched (keeping non listing-* classes)
-                        actionElement.attr('class', function (i, value) {
+                        // Remove any listing-* classes and add listing-snatched (keeping non listing-* classes)
+                        actionElement.attr('class', (i, value) => {
                             return value.replace(/(^|\s)listing-\S+/g, '');
                         }).addClass('listing-snatched');
                     }
@@ -118,12 +116,12 @@ function updateImages(data) {
 }
 
 function checkManualSearches() {
-    var pollInterval = 5000;
-    var showId = $('#showID').val();
-    var url = showId !== undefined ? searchStatusUrl + '?show=' + showId : searchStatusUrl ;
+    let pollInterval = 5000;
+    const showId = $('#showID').val();
+    const url = showId !== undefined ? searchStatusUrl + '?show=' + showId : searchStatusUrl;
     $.ajax({
-        url: url,
-        success: function (data) {
+        url,
+        success(data) {
             if (data.episodes) {
                 pollInterval = 5000;
             } else {
@@ -131,73 +129,74 @@ function checkManualSearches() {
             }
             updateImages(data);
         },
-        error: function () {
+        error() {
             pollInterval = 30000;
         },
-        type: "GET",
-        dataType: "json",
-        complete: function () {
+        type: 'GET',
+        dataType: 'json',
+        complete() {
             setTimeout(checkManualSearches, pollInterval);
         },
-        timeout: 15000 // timeout every 15 secs
+        timeout: 15000 // Timeout every 15 secs
     });
 }
 
-$(document).ready(function () {
+$(document).ready(() => {
     checkManualSearches();
 });
 
-(function(){
-    var stupidOptions;
-    function manualSearch(){
-        var parent = selectedEpisode.parent();
+(function() {
+    let stupidOptions;
+    function manualSearch() {
+        const parent = selectedEpisode.parent();
 
         // Create var for anchor
-        var link = selectedEpisode;
+        const link = selectedEpisode;
 
         // Create var for img under anchor and set options for the loading gif
-        var icon = selectedEpisode.children('span');
-        icon.prop('title', _('Loading') );
-        icon.prop('alt', _('Loading') );
+        const icon = selectedEpisode.children('span');
+        icon.prop('title', _('Loading'));
+        icon.prop('alt', _('Loading'));
         icon.prop('class', stupidOptions.loadingClass);
 
-        var url = selectedEpisode.prop('href');
+        let url = selectedEpisode.prop('href');
 
         if (failedDownload === false) {
-            url = url.replace("retryEpisode", "searchEpisode");
+            url = url.replace('retryEpisode', 'searchEpisode');
         }
 
-        url = url + "&downCurQuality=" + (qualityDownload ? '1' : '0');
+        url = url + '&downCurQuality=' + (qualityDownload ? '1' : '0');
 
-        $.getJSON(url, function(data){
-            var imageName, imageResult;
-            // if they failed then just put the red X
+        $.getJSON(url, data => {
+            let imageName = null;
+            let imageResult = null;
+            // If they failed then just put the red X
             if (data.result.toLowerCase() === 'failure') {
                 imageName = stupidOptions.noImage;
                 imageResult = 'Failed';
             } else {
                 imageName = stupidOptions.loadingImage;
                 imageResult = 'Success';
-                // color the row
+                // Color the row
                 if (stupidOptions.colorRow) {
                     parent.parent().removeClass('skipped wanted qual good unaired').addClass('snatched');
                 }
-                // applying the quality class
-                var rSearchTerm = /(\w+)\s\((.+?)\)/;
-                var htmlContent = data.result.replace(rSearchTerm,"$1"+' <span class="quality '+data.quality+'">'+"$2"+'</span>');
-                // update the status column if it exists
+                // Applying the quality class
+                const rSearchTerm = /(\w+)\s\((.+?)\)/;
+                const htmlContent = data.result.replace(rSearchTerm, '$1 <span class="quality ' + data.quality + '">$2</span>');
+                // Update the status column if it exists
                 parent.siblings('.col-status').html(htmlContent);
                 // Only if the queuing was successful, disable the onClick event of the loading image
                 disableLink(link);
             }
 
-            // put the corresponding image as the result of queuing of the manual search
+            // Put the corresponding image as the result of queuing of the manual search
             // icon.prop('title', imageResult);
             // icon.prop('alt', imageResult);
             // icon.prop('class', imageName);
         });
 
-        // don't follow the link
+        // Don't follow the link
         return false;
     }
 
@@ -212,33 +211,34 @@ $(document).ready(function () {
         }
     };
 
-    $.fn.ajaxEpSearch = function(options){
+    $.fn.ajaxEpSearch = function(options) {
         stupidOptions = $.extend({}, $.ajaxEpSearch.defaults, options);
 
-        $('.epSearch, .epRetry').on('click', function(event){
+        $('.epSearch, .epRetry').on('click', function(event) {
             event.preventDefault();
 
             // Check if we have disabled the click
-            if ($(this).prop('enableClick') === '0') { return false; }
+            if ($(this).prop('enableClick') === '0') {
+                return false;
+            }
 
             selectedEpisode = $(this);
 
-            if ($(this).hasClass("epRetry")){
-                $("#manualSearchModalFailed").modal('show');
-            }
-            else if ($(this).parent().parent().children(".col-status").children(".quality").length) {
-                $("#manualSearchModalQuality").modal('show');
+            if ($(this).hasClass('epRetry')) {
+                $('#manualSearchModalFailed').modal('show');
+            } else if ($(this).parent().parent().children('.col-status').children('.quality').length) {
+                $('#manualSearchModalQuality').modal('show');
             } else {
                 manualSearch();
             }
         });
 
-        $('#manualSearchModalFailed .btn').on('click', function(){
+        $('#manualSearchModalFailed .btn').on('click', function() {
             failedDownload = ($(this).text().toLowerCase() === 'yes');
-            $("#manualSearchModalQuality").modal('show');
+            $('#manualSearchModalQuality').modal('show');
         });
 
-        $('#manualSearchModalQuality .btn').on('click', function(){
+        $('#manualSearchModalQuality .btn').on('click', function() {
             qualityDownload = ($(this).text().toLowerCase() === 'yes');
             manualSearch();
         });
