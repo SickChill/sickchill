212cba93ee4d4e84c35a9578b9906d7064cca9dc
Alexis Tyler <xo@wvvw.me>
2017-07-11 00:00:22 +0930

    Swap jshint with xo (+2 more small fixes) (#3916)
    This includes fixes for the API Builder command search feature,
    and a fix for the provider images not showing in /config/providers
    
    Commits:
    -----------
    
    * remove jshint and unneeded files
    
    * lint browser.js
    
    * lint parsers.js
    
    * lint ajaxEpSearch.js
    
    * fix conflicts
    
    * lint browser.js and replace arrow with normal function
    
    * lint js, remove old mocha files
    
    * lint rootDirs and remove no-console for now
    
    * lint blackwhite
    
    * lint testRename
    
    * lint configProviders
    
    * lint js files
    
    * switch mocha+jshint to xo for ci
    
    * remove sass from Grunt, use npm test with appveyor
    
    * Fix build
    
    * use longform for objects
    
    * add XO code style badge
    
    * Update comment in editorconfig
    
    * Review changes
    
    * Fix images in config/providers
    
    One forward slash can ruin everyone's day
    
    * Fix apibuilder
    
    * Add scroll to command feature
    
    * Fix lint

--- gui/slick/js/ajaxNotifications.js
+++ gui/slick/js/ajaxNotifications.js
@@ -1,8 +1,7 @@
-var message_url = srRoot + '/ui/get_messages',
-    test = !1;
+const test = false;
 
 PNotify.prototype.options.addclass = 'stack-bottomright';
-PNotify.prototype.options.buttons.closer_hover = !1;
+PNotify.prototype.options.buttons.closer_hover = !1; // eslint-disable-line camelcase
 PNotify.prototype.options.delay = 5000;
 PNotify.prototype.options.hide = !0;
 PNotify.prototype.options.history = !1;
@@ -13,34 +12,32 @@ PNotify.prototype.options.width = '340px';
 PNotify.desktop.permission();
 PNotify.prototype.options.desktop = {desktop: !0, icon: srRoot + '/images/ico/favicon-196.png'};
 
-
 function displayPNotify(type, title, message, id) {
-    new PNotify({
+    new PNotify({ // eslint-disable-line no-new
         desktop: {
             tag: id
         },
         type: type,
         title: title,
-        text: message.replace(/<br[\s\/]*(?:\s[^>]*)?>/ig, "\n")
-            .replace(/<[\/]?b(?:\s[^>]*)?>/ig, '*')
-            .replace(/<i(?:\s[^>]*)?>/ig, '[').replace(/<[\/]i>/ig, ']')
-            .replace(/<(?:[\/]?ul|\/li)(?:\s[^>]*)?>/ig, '').replace(/<li(?:\s[^>]*)?>/ig, "\n" + '* ')
+        text: message.replace(/<br[\s/]*(?:\s[^>]*)?>/ig, '\n')
+            .replace(/<[/]?b(?:\s[^>]*)?>/ig, '*')
+            .replace(/<i(?:\s[^>]*)?>/ig, '[').replace(/<[/]i>/ig, ']')
+            .replace(/<(?:[/]?ul|\/li)(?:\s[^>]*)?>/ig, '').replace(/<li(?:\s[^>]*)?>/ig, '\n* ')
     });
 }
 
-function check_notifications() {
-    $.getJSON(message_url, function (data) {
-        $.each(data, function (name, data) {
+function checkNotifications() {
+    $.getJSON(srRoot + '/ui/get_messages', function(data) {
+        $.each(data, function(name, data) {
             displayPNotify(data.type, data.title, data.message, data.hash);
         });
     });
-    setTimeout(function () {
-        "use strict";
-        check_notifications();
-    }, 3000);
+    setTimeout(checkNotifications, 3000);
 }
 
-$(document).ready(function(){
-    check_notifications();
-    if(test) displayPNotify('notice', 'test', 'test<br><i class="test-class">hello <b>world</b></i><ul><li>item 1</li><li>item 2</li></ul>', 'notification-test');
+$(document).ready(function() {
+    checkNotifications();
+    if (test) {
+        displayPNotify('notice', 'test', 'test<br><i class="test-class">hello <b>world</b></i><ul><li>item 1</li><li>item 2</li></ul>', 'notification-test');
+    }
 });
