ed264bf2eb4b2358ce6ed05dcabdc732655af121
barraudf <barraudf@gmail.com>
2017-08-23 22:16:55 +0200

    New provider : Elite Tracker ( https://elite-tracker.net/ )

--- sickbeard/providers/__init__.py
+++ sickbeard/providers/__init__.py
@@ -36,7 +36,7 @@ __all__ = [
     'horriblesubs', 'hounddawgs', 'ilcorsaronero', 'immortalseed', 'iptorrents', 'limetorrents', 'morethantv',
     'ncore', 'nebulance', 'newpct', 'norbits', 'nyaa', 'omgwtfnzbs', 'pretome', 'rarbg', 'scc', 'scenetime',
     'shazbat', 'skytorrents', 'speedcd', 'thepiratebay', 'tntvillage', 'tokyotoshokan', 'torrent9',
-    'torrentbytes', 'torrentday', 'torrentleech', 'torrentproject', 'torrentz', 'tvchaosuk', 'xthor'
+    'torrentbytes', 'torrentday', 'torrentleech', 'torrentproject', 'torrentz', 'tvchaosuk', 'xthor', 'elitetrackerfr'
 ]
 
 
