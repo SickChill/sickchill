3fe0766974ddf05a55157218a57f5f41d843b017
snamds <snamds@gmail.com>
2017-11-06 13:19:35 +0100

    New web address
    -New web address
    -New torrent download address format

--- sickbeard/providers/elitetorrent.py
+++ sickbeard/providers/elitetorrent.py
@@ -24,9 +24,8 @@ import re
 import time
 import traceback
 
-import six
-
 import sickbeard
+import six
 from sickbeard import logger, tvcache
 from sickbeard.bs4_parser import BS4Parser
 from sickbeard.common import cpu_presets
@@ -46,10 +45,10 @@ class EliteTorrentProvider(TorrentProvider):
         self.cache = tvcache.TVCache(self)  # Only poll EliteTorrent every 20 minutes max
 
         self.urls = {
-            'base_url': 'http://www.elitetorrent.net',
-            'search': 'http://www.elitetorrent.net/torrents.php'
+            'base_url': 'https://www.elitetorrent.eu',
+            'search': 'https://www.elitetorrent.eu/torrents.php'
         }
-
+        
         self.url = self.urls['base_url']
 
     def search(self, search_strings, age=0, ep_obj=None):  # pylint: disable=too-many-locals, too-many-branches
@@ -58,7 +57,7 @@ class EliteTorrentProvider(TorrentProvider):
 
         """
         Search query:
-        http://www.elitetorrent.net/torrents.php?cat=4&modo=listado&orden=fecha&pag=1&buscar=fringe
+        https://www.elitetorrent.eu/torrents.php?cat=4&modo=listado&orden=fecha&pag=1&buscar=fringe
 
         cat = 4 => Shows
         modo = listado => display results mode
@@ -110,6 +109,16 @@ class EliteTorrentProvider(TorrentProvider):
                         for row in torrent_rows[1:]:
                             try:
                                 download_url = self.urls['base_url'] + row.find('a')['href']
+                                
+                                """
+                                Transform from
+                                https://elitetorrent.eu/torrent/40142/la-zona-1x02
+                                to
+                                https://elitetorrent.eu/get-torrent/40142
+                                """
+                    
+                                download_url = re.sub(r'/torrent/(\d*)/.*', r'/get-torrent/\1', download_url)
+                                
                                 """
                                 Trick for accents for this provider.
 
