6b62cd4003cab6cfe484e0877c01b082a109a5f1
DarkVaDehors <darkvadehors@users.noreply.github.com>
2017-12-10 13:00:57 +0100

    Update Torrent9 URL

--- sickbeard/providers/torrent9.py
+++ sickbeard/providers/torrent9.py
@@ -37,7 +37,7 @@ class Torrent9Provider(TorrentProvider):
         self.public = True
         self.minseed = None
         self.minleech = None
-        self.url = "http://www.torrent9.cc"
+        self.url = "http://www.torrent9.pe"
 
         self.proper_strings = ['PROPER', 'REPACK']
         self.cache = tvcache.TVCache(self)
