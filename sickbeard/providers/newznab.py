8bee6c1ac7134c4c43b0bd3c11f084dadf517404
miigotu <miigotu@gmail.com>
2017-12-16 02:24:02 -0800

    Fix Jackett (#4197)
    * Fix issue with jackett sending torrents to nzb downloader
    
    * Fix issue with adding custom providers (jackett) and providers disappearing. Fix being able to change categories with built in nzb providers
    
    * ESLint fixes

--- sickbeard/providers/newznab.py
+++ sickbeard/providers/newznab.py
@@ -282,7 +282,6 @@ class NewznabProvider(NZBProvider):  # pylint: disable=too-many-instance-attribu
                 return results
 
         for mode in search_strings:
-            torznab = False
             search_params = {
                 't': ('search', 'tvsearch')[bool(self.use_tv_search)],
                 'limit': 100,
@@ -330,9 +329,9 @@ class NewznabProvider(NZBProvider):  # pylint: disable=too-many-instance-attribu
                         break
 
                     try:
-                        torznab = 'xmlns:torznab' in html.rss.attrs
+                        self.torznab = 'xmlns:torznab' in html.rss.attrs
                     except AttributeError:
-                        torznab = False
+                        self.torznab = False
 
                     for item in html('item'):
                         try:
@@ -362,7 +361,7 @@ class NewznabProvider(NZBProvider):  # pylint: disable=too-many-instance-attribu
                                     seeders = try_int(attr['value']) if attr['name'] == 'seeders' else seeders
                                     leechers = try_int(attr['value']) if attr['name'] == 'peers' else leechers
 
-                            if not item_size or (torznab and (seeders is None or leechers is None)):
+                            if not item_size or (self.torznab and (seeders is None or leechers is None)):
                                 continue
 
                             size = convert_size(item_size) or -1
@@ -377,7 +376,7 @@ class NewznabProvider(NZBProvider):  # pylint: disable=too-many-instance-attribu
                 if 'tvdbid' in search_params:
                     break
 
-            if torznab:
+            if self.torznab:
                 results.sort(key=lambda d: try_int(d.get('seeders', 0)), reverse=True)
             results += items
 
