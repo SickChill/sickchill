b00d9852101b3684d5dfcd38afdfa8d71a71e62d
miigotu <miigotu@gmail.com>
2018-03-16 23:14:51 -0700

    Add coding declaration and license docstring to sickbeard.routes.py

--- sickbeard/routes.py
+++ sickbeard/routes.py
@@ -1,4 +1,23 @@
-""" Custom fast routes """
+# coding=utf-8
+# URL: https://sickrage.github.io
+#
+# This file is part of SickRage.
+#
+# SickRage is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickRage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SickRage. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
 import tornado.web
 
 route_list = []
