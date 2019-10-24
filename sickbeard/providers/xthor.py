acde9c6115bf90dcff9ef962e6c4d50adc18396f
rsabile <sabile.remy@gmail.com>
2019-10-24 15:19:04 +0200

    Update xthor.py

--- sickbeard/providers/xthor.py
+++ sickbeard/providers/xthor.py
@@ -32,7 +32,7 @@ class XThorProvider(TorrentProvider):
         TorrentProvider.__init__(self, 'XThor')
 
         self.url = 'https://xthor.to'
-        self.urls = {'search': 'https://api.xthor.to'}
+        self.urls = {'search': 'https://api.xthor.tk'}
 
         self.freeleech = None
         self.api_key = None
