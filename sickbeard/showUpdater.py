a54e3e749bfdf5c0d92fa86b299c01ecaff177ea
Wally73 <Wally73@users.noreply.github.com>
2019-11-30 08:41:59 +0100

    Update tvdb api to v3  (credits to @BenjV)
    update ShowUpdater to use api v3 from the tvdb 
    made by @BenjV (i only edited the right api key)

--- sickbeard/showUpdater.py
+++ sickbeard/showUpdater.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 # Author: Nic Wolfe <nic@wolfeden.ca>
+# 2019-11-29 : Updated by Benj to comply with Tvdb API V3
 # URL: https://sickchill.github.io
 #
 # This file is part of SickChill.
@@ -22,32 +23,61 @@ from __future__ import unicode_literals
 import datetime
 import threading
 import time
+import json
 
 import sickbeard
 from sickbeard import db, helpers, logger, network_timezones, ui
 from sickbeard.indexers.indexer_config import INDEXER_TVDB, INDEXER_TVRAGE
 from sickchill.helper.exceptions import CantRefreshShowException, CantUpdateShowException, ex
 
-try:
-    import xml.etree.cElementTree as etree
-except ImportError:
-    import xml.etree.ElementTree as etree
-
-
-
 
 class ShowUpdater(object):  # pylint: disable=too-few-public-methods
     def __init__(self):
         self.lock = threading.Lock()
         self.amActive = False
-
+        self.base = 'https://api.thetvdb.com'
+        self.apikey = json.dumps({'apikey':"F9C450E78D99172E"})
+        self.login = self.base + '/login'
+        self.refresh = self.base + '/refresh'
+        self.update = self.base + '/updated/query?fromTime='
         self.session = helpers.make_session()
+            # set the correct headers for the requests calls to tvdb
+        self.session.headers.update({'Accept': 'application/json','Content-Type': 'application/json','Authorization': ''})
+        self.timeout = 12.1 #General timeout for the requests calls to prevent a hang if tvdb does not fespond
+
+    def _gettoken(self):
+        try:
+            if self.session.headers.get('Authorization'):
+                    # There is a token in the header so we try to refresh it
+                resp = self.session.get(self.refresh, timeout=self.timeout)
+                logger.log('Try to refresh a tvdb token')
+                if resp.ok:
+                    Token = json.loads(resp.text).get('token','')
+                    self.session.headers.update({"Authorization": "Bearer " + Token})
+                    logger.log('Tvdb token refreshed')
+                    return True
+                # there was no token in the header or the token was expired so we login to tvdb with the apikey to get a new token
+            logger.log('Try to login to tvdb to get a token')
+            resp = self.session.post(self.login, self.apikey, timeout=self.timeout)
+            if resp.ok:
+                Token = json.loads(resp.text).get('token','')
+                self.session.headers.update({"Authorization": "Bearer " + Token})
+                logger.log('Logged in on tvdb and got a token.')
+                return True
+            else:
+                logger.log('Failed to get a token from tvdb')
+                return False
+        except Exception as error:
+            logger.log('Could not get a token from Tvdb. Reason: %s' %resp.reason)
+        return False
 
     def run(self, force=False):  # pylint: disable=unused-argument, too-many-locals, too-many-branches, too-many-statements
-
+        logger.log('ShowUpdater for tvdb Api V3 starting')
         if self.amActive:
             return
-
+            # get a tvdb api token and put it in the requests session header
+        if not self._gettoken():
+            return
         self.amActive = True
 
         update_timestamp = time.mktime(datetime.datetime.now().timetuple())
@@ -60,28 +90,32 @@ class ShowUpdater(object):  # pylint: disable=too-few-public-methods
             cache_db_con.action('INSERT INTO lastUpdate (provider, `time`) VALUES (?, ?)', ['theTVDB', last_update])
 
         network_timezones.update_network_dict()
-
-        url = 'http://thetvdb.com/api/Updates.php?type=series&time={0}'.format(last_update)
-        data = helpers.getURL(url, session=self.session, returns='text', hooks={'response': self.request_hook})
-        if not data:
-            logger.log('Could not get the recently updated show data from {0}. Retrying later. Url was: {1}'.format(sickbeard.indexerApi(INDEXER_TVDB).name, url))
+        try:
+            logger.log('Tvdb Cmd:' + self.update + str(last_update))
+            resp = self.session.get(self.update + str(last_update), timeout=self.timeout)
+        except Exception as error:
+            self.amActive = False
+            logger.log('Could not get show updates from tvdb. Reason: %s' %resp.reason)
+            return
+        TvdbData = json.loads(resp.text)
+        if TvdbData.get('Error'):
+            logger.log(TvdbData['Error'])
             self.amActive = False
             return
-
-        updated_shows = set()
-        try:
-            tree = etree.fromstring(data)
-            for show in tree.findall('Series'):
-                updated_shows.add(int(show.text))
-        except SyntaxError:
-            update_timestamp = last_update
+        else:
+                # Use a list comprehension to put all the updated serie id's from the dict into a single list
+            updated_shows = [i['id'] for i in TvdbData.get('data',[])]
+            logger.log('Updates for: %s' % updated_shows )
+        if not updated_shows:
+            logger.log('No updated shows found on tvdb')
+            self.amActive = False
+            return
 
         pi_list = []
         for cur_show in sickbeard.showList:
             if int(cur_show.indexer) in [INDEXER_TVRAGE]:
                 logger.log('Indexer is no longer available for show [{0}] '.format(cur_show.name), logger.WARNING)
                 continue
-
             try:
                 cur_show.nextEpisode()
                 if sickbeard.indexerApi(cur_show.indexer).name == 'theTVDB':
