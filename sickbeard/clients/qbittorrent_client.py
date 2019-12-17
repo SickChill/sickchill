f80c5186644348160e04852ad625835c6e65b5ba
tchab <dev@chabs.name>
2019-12-17 22:40:48 +0100

    Switch to qBittorrent v2 REST API

--- sickbeard/clients/qbittorrent_client.py
+++ sickbeard/clients/qbittorrent_client.py
@@ -36,6 +36,7 @@ class qbittorrentAPI(GenericClient):
         super(qbittorrentAPI, self).__init__('qbittorrent', host, username, password)
 
         self.url = self.host
+        self.api_prefix = 'api/v2'
         self.session.auth = HTTPDigestAuth(self.username, self.password)
         self.session.headers.update({
             'Origin': self.host,
@@ -45,33 +46,24 @@ class qbittorrentAPI(GenericClient):
     @property
     def api(self):
         try:
-            self.url = urljoin(self.host, 'version/api')
+            self.url = urljoin(self.host, self.api_prefix, 'app/webapiVersion')
             response = self.session.get(self.url, verify=sickbeard.TORRENT_VERIFY_CERT)
             if response.status_code == 401:
                 version = None
             else:
-                version = int(response.content)
+                version = float(response.content)
         except Exception:
-            version = 1
+            version = 2
         return version
 
     def _get_auth(self):
         if self.api is None:
             return None
 
-        if self.api > 1:
-            self.url = urljoin(self.host, 'login')
-            data = {'username': self.username, 'password': self.password}
-            try:
-                self.response = self.session.post(self.url, data=data)
-            except Exception:
-                return None
-
-        else:
-            try:
-                self.response = self.session.get(self.host, verify=sickbeard.TORRENT_VERIFY_CERT)
-            except Exception:
-                return None
+        try:
+            self.response = self.session.get(self.host, verify=sickbeard.TORRENT_VERIFY_CERT)
+        except Exception:
+            return None
 
         self.session.cookies = self.response.cookies
         self.auth = self.response.content
@@ -82,18 +74,7 @@ class qbittorrentAPI(GenericClient):
 
         data = {'urls': result.url}
 
-        if self.api > 6:
-            if sickbeard.TORRENT_PATH:
-                data['savepath'] = sickbeard.TORRENT_PATH
-
-            label = sickbeard.TORRENT_LABEL
-            if result.show.is_anime:
-                label = sickbeard.TORRENT_LABEL_ANIME
-
-            if label:
-                data['category'] = label.replace(' ', '_')
-
-        self.url = urljoin(self.host, 'command/download')
+        self.url = urljoin(self.host, self.api_prefix, 'torrents/add')
         if self._request(method='post', data=data, cookies=self.session.cookies):
             sleep(2)  # The client always needs to fetch the torrent/magnet
             return self._verify_added(result.hash)
@@ -104,18 +85,8 @@ class qbittorrentAPI(GenericClient):
         files = {'torrents': (result.name + '.torrent', result.content)}
 
         data = {}
-        if self.api > 6:
-            if sickbeard.TORRENT_PATH:
-                data['savepath'] = sickbeard.TORRENT_PATH
 
-            label = sickbeard.TORRENT_LABEL
-            if result.show.is_anime:
-                label = sickbeard.TORRENT_LABEL_ANIME
-
-            if label:
-                data['category'] = label.replace(' ', '_')
-
-        self.url = urljoin(self.host, 'command/upload')
+        self.url = urljoin(self.host, self.api_prefix, 'torrents/add')
         if self._request(method='post', files=files, data=data, cookies=self.session.cookies):
             return self._verify_added(result.hash)
         return False
@@ -126,38 +97,30 @@ class qbittorrentAPI(GenericClient):
         if result.show.is_anime:
             label = sickbeard.TORRENT_LABEL_ANIME
 
-        if 6 < self.api < 10 and label:
-            self.url = urljoin(self.host, 'command/setLabel')
-            data = {'hashes': result.hash.lower(), 'label': label.replace(' ', '_')}
-            return self._request(method='post', data=data, cookies=self.session.cookies)
-
-        elif self.api >= 10 and label:
-            self.url = urljoin(self.host, 'command/setCategory')
-            data = {'hashes': result.hash.lower(), 'category': label.replace(' ', '_')}
-            return self._request(method='post', data=data, cookies=self.session.cookies)
-
-        return True
+        self.url = urljoin(self.host, self.api_prefix, 'torrents/setCategory')
+        data = {'hashes': result.hash.lower(), 'category': label.replace(' ', '_')}
+        return self._request(method='post', data=data, cookies=self.session.cookies)
 
     def _set_torrent_priority(self, result):
 
-        self.url = urljoin(self.host, 'command/decreasePrio')
+        self.url = urljoin(self.host, self.api_prefix, 'torrents/decreasePrio')
         if result.priority == 1:
-            self.url = urljoin(self.host, 'command/increasePrio')
+            self.url = urljoin(self.host, self.api_prefix, 'torrents/increasePrio')
 
         data = {'hashes': result.hash.lower()}
         return self._request(method='post', data=data, cookies=self.session.cookies)
 
     def _set_torrent_pause(self, result):
 
-        self.url = urljoin(self.host, 'command/resume')
+        self.url = urljoin(self.host, self.api_prefix, 'torrents/resume')
         if sickbeard.TORRENT_PAUSED:
-            self.url = urljoin(self.host, 'command/pause')
+            self.url = urljoin(self.host, self.api_prefix, 'torrents/pause')
 
         data = {'hash': result.hash.lower()}
         return self._request(method='post', data=data, cookies=self.session.cookies)
 
     def _verify_added(self, torrent_hash, attempts=10):
-        self.url = urljoin(self.host, 'query/propertiesGeneral/{}'.format(torrent_hash.lower()))
+        self.url = urljoin(self.host, self.api_prefix, 'torrents/info/{}'.format(torrent_hash.lower()))
         for i in range(attempts):
             if self._request(method='get', cookies=self.session.cookies):
                 if self.response.json()['piece_size'] != -1:
