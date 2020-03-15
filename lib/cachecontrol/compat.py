f3e6e2a93e83e10d6a3a50ca4636470194c9afd3
miigotu <miigotu@gmail.com>
2020-03-15 05:49:10 -0400

    Update some libs and update the docker packaging script (#6106)
    * Fix build cache sharing and simplify yml
    
    * Skip docker cache, handle build for PR's from outside Org
    
    Signed-off-by: miigotu <miigotu@gmail.com>
    
    * Update some libs
    
    Signed-off-by: miigotu <miigotu@gmail.com>

--- lib/cachecontrol/compat.py
+++ lib/cachecontrol/compat.py
@@ -21,3 +21,9 @@ try:
     from requests.packages.urllib3.util import is_fp_closed
 except ImportError:
     from urllib3.util import is_fp_closed
+
+# Replicate some six behaviour
+try:
+    text_type = unicode
+except NameError:
+    text_type = str
