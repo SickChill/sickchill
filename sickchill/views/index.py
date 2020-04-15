82bb7d340c6cd38b81737bfb7a16b7d13b61b74e
Valdemar Jakobsen <jamver@users.noreply.github.com>
2020-04-15 21:10:26 +1000

    Workaround for SickChill#6199 to support RFC 4007 IPv6 Scoped Literal Addresses in remote_ip

--- sickchill/views/index.py
+++ sickchill/views/index.py
@@ -169,7 +169,8 @@ class BaseHandler(RequestHandler):
 
         else:
             # Local network
-            return helpers.is_ip_private(self.request.remote_ip)
+            # strip <scope id> / <zone id> (%value/if_name) from remote_ip IPv6 scoped literal IP Addresses (RFC 4007) until phihag/ipaddress is updated tracking cpython 3.9.
+            return helpers.is_ip_private(self.request.remote_ip.rsplit('%')[0])
 
     def get_user_locale(self):
         return sickbeard.GUI_LANG or None
