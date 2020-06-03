06cab14b8b4eeb4bb014aacbe602e1e73c5d36a2
miigotu <miigotu@gmail.com>
2020-06-03 14:11:26 -0400

    Allow cross origin for api if the correct api key is given. Fixes #6475

--- sickchill/views/api/webapi.py
+++ sickchill/views/api/webapi.py
@@ -87,8 +87,12 @@ class ApiHandler(RequestHandler):
     def __init__(self, *args, **kwargs):
         super(ApiHandler, self).__init__(*args, **kwargs)
 
-    # def set_default_headers(self):
-    #     self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+    def set_default_headers(self):
+        self.set_header("Content-Type", "application/json")
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "content-type")
+        self.set_header('Access-Control-Allow-Methods', 'GET')
+        # self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
 
     def get(self, *args, **kwargs):
         kwargs = self.request.arguments
