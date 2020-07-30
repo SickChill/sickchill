e18a82d79d837b326f618977cd311e70f48ec442
miigotu <miigotu@gmail.com>
2020-07-30 17:37:53 -0400

    zzz
    Signed-off-by: miigotu <miigotu@gmail.com>

--- SickChill.py
+++ SickChill.py
@@ -116,7 +116,8 @@ class SickChill(object):
         settings.MY_FULLNAME = os.path.normpath(os.path.abspath(__file__))
         settings.MY_NAME = os.path.basename(settings.MY_FULLNAME)
         settings.PROG_DIR = os.path.dirname(settings.MY_FULLNAME).replace('EGG-INFO{sep}scripts{sep}'.format(sep=os.path.sep), '').replace('Scripts{sep}'.format(sep=os.path.sep), '')
-
+        if os.name == 'nt':
+            settings.PROG_DIR += 'site-packages'
         settings.DATA_DIR = settings.PROG_DIR
         if 'site-packages' in settings.DATA_DIR:
             settings.DATA_DIR = str(Path.home().joinpath('sickchill').absolute())
