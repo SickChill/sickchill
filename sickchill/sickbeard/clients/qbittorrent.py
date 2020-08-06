ecd7870ff3f78228aaa9c04d9291b25a46c84cdc
miigotu <miigotu@gmail.com>
2020-08-05 21:42:23 -0400

    Use qbittorrent-api library
    Signed-off-by: miigotu <miigotu@gmail.com>

--- sickchill/sickbeard/clients/qbittorrent.py
+++ sickchill/sickbeard/clients/qbittorrent.py
@@ -1,85 +1,40 @@
+from urllib.parse import splitport
 
-# Stdlib Imports
+import qbittorrentapi
 
-# Stdlib Imports
-from urllib.parse import urljoin
-
-# Third Party Imports
-from requests.auth import HTTPDigestAuth
-
-# First Party Imports
-from sickchill import settings
+from sickchill import settings, sickbeard
 from sickchill.sickbeard.clients.generic import GenericClient
 
 
 class Client(GenericClient):
-
     def __init__(self, host=None, username=None, password=None):
         super(Client, self).__init__('qBittorrent', host, username, password)
-        self.url = self.host
-        self.api_prefix = 'api/v2/'
-        self.session.auth = HTTPDigestAuth(self.username, self.password)
-        self.session.headers.update({
-            'Origin': self.host,
-            'Referer': self.host
-        })
-
-    @property
-    def api(self):
-        try:
-            self.url = urljoin(self.host, self.api_prefix + 'app/webapiVersion')
-            response = self.session.get(self.url, verify=settings.TORRENT_VERIFY_CERT)
-            if response.status_code == 401:
-                version = None
-            else:
-                version = float(response.content)
-        except Exception:
-            version = 2
-        return version
+        self.host, self.port = splitport(self.host)
+        self.api = qbittorrentapi.Client(host=self.host, port=self.port or 8080, username=self.username, password=self.password)
 
     def _get_auth(self):
-        if self.api is None:
-            return None
-        self.url = urljoin(self.host, self.api_prefix + 'auth/login')
-        data = {'username': self.username, 'password': self.password}
         try:
-            self.response = self.session.post(self.url, data=data)
-        except Exception:
+            self.api.auth_log_in()
+        except qbittorrentapi.LoginFailed as error:
+            sickbeard.logger.info(error)
             return None
-        self.session.cookies = self.response.cookies
-        self.auth = self.response.content
-        return (None, self.auth)[self.response.status_code != 403]
+        return True
+
+    @staticmethod
+    def __torrent_args(result):
+        return dict(
+            save_path=settings.TORRENT_DIR or None,
+            category=(settings.TORRENT_LABEL, settings.TORRENT_LABEL_ANIME)[result.show.is_anime] or None,
+            is_paused=settings.TORRENT_PAUSED
+        )
 
     def _add_torrent_uri(self, result):
-        data = {'urls': result.url}
-        self.url = urljoin(self.host, self.api_prefix + 'torrents/add')
-        return self._request(method='post', data=data, cookies=self.session.cookies)
+        return self.api.torrents_add(urls=[result.url], **self.__torrent_args(result))
 
     def _add_torrent_file(self, result):
-        files = {'torrents': (result.name + '.torrent', result.content)}
-        self.url = urljoin(self.host, self.api_prefix + 'torrents/add')
-        return self._request(method='post', files=files, cookies=self.session.cookies)
-
-    def _set_torrent_label(self, result):
-        label = settings.TORRENT_LABEL
-        if result.show.is_anime:
-            label = settings.TORRENT_LABEL_ANIME
-        self.url = urljoin(self.host, self.api_prefix + 'torrents/setCategory')
-        data = {'hashes': result.hash.lower(), 'category': label.replace(' ', '_')}
-        return self._request(method='post', data=data, cookies=self.session.cookies)
+        return self.api.torrents_add(torrent_files=[result.content], **self.__torrent_args(result))
 
     def _set_torrent_priority(self, result):
-        self.url = urljoin(self.host, self.api_prefix + 'app/preferences')
-        self._request(method='get', cookies=self.session.cookies)
-        json = self.response.json()
-        if json and json.get('queueing_enabled'):
-            self.url = urljoin(self.host, self.api_prefix + 'torrents/decreasePrio?hashes={}'.format(result.hash.lower))
-            if result.priority == 1:
-                self.url = urljoin(self.host, self.api_prefix + 'torrents/increasePrio?hashes={}'.format(result.hash.lower))
-            return self._request(method='get', cookies=self.session.cookies)
-
-    def _set_torrent_pause(self, result):
-        self.url = urljoin(self.host, self.api_prefix + 'torrents/resume?hashes={}'.format(result.hash.lower()))
-        if settings.TORRENT_PAUSED:
-            self.url = urljoin(self.host, self.api_prefix + 'torrents/pause?hashes={}'.format(result.hash.lower()))
-        return self._request(method='get', cookies=self.session.cookies)
+        if not self.api.app_preferences.queueing_enabled:
+            return True
+        return (self.api.torrents_decrease_priority, self.api.torrents_increase_priority)[result.priority == 1](result.hash.lower)
