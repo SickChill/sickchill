ec2edacd630f1ab9ad5aad25907ced5a87d3b3ad
miigotu <miigotu@gmail.com>
2020-08-18 06:44:29 -0400

    Remove old db upgrades. We will only support db 44+ now. Improves speedup and code.

--- sickchill/oldbeard/databases/cache.py
+++ sickchill/oldbeard/databases/cache.py
@@ -10,12 +10,15 @@ class InitialSchema(db.SchemaUpgrade):
         queries = (
             ("CREATE TABLE lastUpdate (provider TEXT, time NUMERIC);",),
             ("CREATE TABLE lastSearch (provider TEXT, time NUMERIC);",),
-            ("CREATE TABLE scene_exceptions (exception_id INTEGER PRIMARY KEY, indexer_id INTEGER, show_name TEXT, season NUMERIC DEFAULT -1, custom NUMERIC DEFAULT 0);",),
+            ("CREATE TABLE scene_exceptions ("
+             "exception_id INTEGER PRIMARY KEY, indexer_id INTEGER, show_name TEXT, season NUMERIC DEFAULT -1, custom NUMERIC DEFAULT 0);",),
             ("CREATE TABLE scene_names (indexer_id INTEGER, name TEXT);",),
             ("CREATE TABLE network_timezones (network_name TEXT PRIMARY KEY, timezone TEXT);",),
             ("CREATE TABLE scene_exceptions_refresh (list TEXT PRIMARY KEY, last_refreshed INTEGER);",),
             ("CREATE TABLE db_version (db_version INTEGER);",),
-            ("CREATE TABLE results (provider TEXT, name TEXT, season NUMERIC, episodes TEXT, indexerid NUMERIC, url TEXT, time NUMERIC, quality TEXT, release_group TEXT, version NUMERIC, seeders INTEGER DEFAULT 0, leechers INTEGER DEFAULT 0, size INTEGER DEFAULT -1, status INTEGER DEFAULT 0, failed INTEGER DEFAULT 0, added TEXT DEFAULT CURRENT_TIMESTAMP);",),
+            ("CREATE TABLE results (provider TEXT, name TEXT, season NUMERIC, episodes TEXT, indexerid NUMERIC, url TEXT, time NUMERIC, quality TEXT, "
+             "release_group TEXT, version NUMERIC, seeders INTEGER DEFAULT 0, leechers INTEGER DEFAULT 0, size INTEGER DEFAULT -1, status INTEGER DEFAULT 0, "
+             "failed INTEGER DEFAULT 0, added TEXT DEFAULT CURRENT_TIMESTAMP);",),
             ("INSERT INTO db_version(db_version) VALUES (1);",),
             ("CREATE UNIQUE INDEX IF NOT EXISTS idx_url ON results (url);",),
             ("CREATE INDEX IF NOT EXISTS provider ON results (provider);",),
@@ -28,102 +31,16 @@ class InitialSchema(db.SchemaUpgrade):
                 self.connection.action(query[0], query[1:])
 
 
-class AddSceneExceptions(InitialSchema):
-    def test(self):
-        return self.has_table("scene_exceptions")
-
-    def execute(self):
-        self.connection.action(
-            "CREATE TABLE scene_exceptions (exception_id INTEGER PRIMARY KEY, indexer_id INTEGER, show_name TEXT);")
-
-
-class AddSceneNameCache(AddSceneExceptions):
-    def test(self):
-        return self.has_table("scene_names")
-
-    def execute(self):
-        self.connection.action("CREATE TABLE scene_names (indexer_id INTEGER, name TEXT);")
-
-
-class AddNetworkTimezones(AddSceneNameCache):
-    def test(self):
-        return self.has_table("network_timezones")
-
-    def execute(self):
-        self.connection.action("CREATE TABLE network_timezones (network_name TEXT PRIMARY KEY, timezone TEXT);")
-
-
-class AddLastSearch(AddNetworkTimezones):
-    def test(self):
-        return self.has_table("lastSearch")
-
-    def execute(self):
-        self.connection.action("CREATE TABLE lastSearch (provider TEXT, time NUMERIC);")
-
-
-class AddSceneExceptionsSeasons(AddLastSearch):
-    def test(self):
-        return self.has_column("scene_exceptions", "season")
-
-    def execute(self):
-        self.add_column("scene_exceptions", "season", "NUMERIC", -1)
-
-
-class AddSceneExceptionsCustom(AddSceneExceptionsSeasons):  # pylint:disable=too-many-ancestors
-    def test(self):
-        return self.has_column("scene_exceptions", "custom")
-
-    def execute(self):
-        self.add_column("scene_exceptions", "custom", "NUMERIC", 0)
-
-
-class AddSceneExceptionsRefresh(AddSceneExceptionsCustom):  # pylint:disable=too-many-ancestors
-    def test(self):
-        return self.has_table("scene_exceptions_refresh")
-
-    def execute(self):
-        self.connection.action(
-            "CREATE TABLE scene_exceptions_refresh (list TEXT PRIMARY KEY, last_refreshed INTEGER);")
-
-
-class ConvertSceneExeptionsToIndexerScheme(AddSceneExceptionsRefresh):  # pylint:disable=too-many-ancestors
-    def test(self):
-        return self.has_column("scene_exceptions", "indexer_id")
-
-    def execute(self):
-        self.connection.action("DROP TABLE IF EXISTS tmp_scene_exceptions;")
-        self.connection.action("ALTER TABLE scene_exceptions RENAME TO tmp_scene_exceptions;")
-        self.connection.action("CREATE TABLE scene_exceptions (exception_id INTEGER PRIMARY KEY, indexer_id INTEGER, show_name TEXT, season NUMERIC DEFAULT -1, custom NUMERIC DEFAULT 0);")
-        # language=TEXT
-        self.connection.action("INSERT INTO scene_exceptions SELECT exception_id, tvdb_id as indexer_id, show_name, season, custom FROM tmp_scene_exceptions;")
-        # language=TEXT
-        self.connection.action("DROP TABLE tmp_scene_exceptions;")
-
-
-class ConvertSceneNamesToIndexerScheme(AddSceneExceptionsRefresh):  # pylint:disable=too-many-ancestors
-    def test(self):
-        return self.has_column("scene_names", "indexer_id")
-
-    def execute(self):
-        self.connection.action("DROP TABLE IF EXISTS tmp_scene_names;")
-        self.connection.action("ALTER TABLE scene_names RENAME TO tmp_scene_names;")
-        self.connection.action("CREATE TABLE scene_names (indexer_id INTEGER, name TEXT);")
-        # language=TEXT
-        self.connection.action("INSERT INTO scene_names SELECT * FROM tmp_scene_names;")
-        # language=TEXT
-        self.connection.action("DROP TABLE tmp_scene_names;")
-
-
-class ResultsTable(ConvertSceneExeptionsToIndexerScheme):
+class ResultsTable(InitialSchema):
     def test(self):
         return self.has_table('results')
 
     def execute(self):
         import sickchill.settings
-        assert len(sickchill.settings.providerList) != 0
         self.connection.action(
             "CREATE TABLE results (provider TEXT, name TEXT, season NUMERIC, episodes TEXT, indexerid NUMERIC, url TEXT, time NUMERIC, quality TEXT,"
-            "release_group TEXT, version NUMERIC, seeders INTEGER DEFAULT 0, leechers INTEGER DEFAULT 0, size INTEGER DEFAULT -1, status INTEGER DEFAULT 0, failed INTEGER DEFAULT 0, added TEXT DEFAULT CURRENT_TIMESTAMP);")
+            "release_group TEXT, version NUMERIC, seeders INTEGER DEFAULT 0, leechers INTEGER DEFAULT 0, size INTEGER DEFAULT -1, status INTEGER DEFAULT 0, "
+            "failed INTEGER DEFAULT 0, added TEXT DEFAULT CURRENT_TIMESTAMP);")
 
         for provider in sickchill.settings.providerList:
             provider_id = provider.get_id()
@@ -139,13 +56,7 @@ class ResultsTable(ConvertSceneExeptionsToIndexerScheme):
 
                 # language=TEXT
                 self.connection.action(
-                    "INSERT INTO results SELECT provider, name , season, episodes, indexerid, url, time, quality, release_group, version, seeders, leechers, size, status, failed, added FROM {}".format(provider_id))
+                    "INSERT INTO results SELECT provider, name , season, episodes, indexerid, url, time, quality,"
+                    "release_group, version, seeders, leechers, size, status, failed, added FROM ?", [provider_id]
+                )
                 self.connection.action('DROP TABLE {}'.format(provider_id))
-
-
-class LastUpdate(ResultsTable):
-    def test(self):
-        return self.has_table('lastUpdate')
-
-    def execute(self):
-        self.connection.action("CREATE TABLE lastUpdate (provider TEXT, time NUMERIC)")
