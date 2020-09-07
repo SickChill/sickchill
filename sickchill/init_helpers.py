c4ecb5cd449a8d3e1fa762ab4e5793bcabafa6fe
miigotu <miigotu@gmail.com>
2020-09-07 16:23:54 -0400

    Fix failure to start due to translations by byassing if it fails (hacky) Try to fix an empty environment variable for language Fixes #6702
    Signed-off-by: miigotu <miigotu@gmail.com>

--- sickchill/init_helpers.py
+++ sickchill/init_helpers.py
@@ -22,9 +22,10 @@ def locale_dir():
 
 def setup_gettext(language=None):
     languages = [language] if language else None
-    if not [key for key in ('LANGUAGE', 'LC_ALL', 'LC_MESSAGES', 'LANG') if key in os.environ]:
+    if not [key for key in ('LANGUAGE', 'LC_ALL', 'LC_MESSAGES', 'LANG') if os.environ.get(key)]:
         os.environ['LC_MESSAGES'] = 'en_US.UTF-8'
-    gt = gettext.translation('messages', locale_dir(), languages=languages)
+    print(locale_dir())
+    gt = gettext.translation('messages', locale_dir(), languages=languages, fallback=True)
     gt.install(names=["ngettext"])
 
 
