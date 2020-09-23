15c20f02008f97f003a40a78ca2b9b88c79b683a
miigotu <miigotu@gmail.com>
2020-09-23 05:18:16 -0400

    Rework pushover notifier to use requests, and be able to verify the server https certificate
    Signed-off-by: miigotu <miigotu@gmail.com>

--- sickchill/oldbeard/notifiers/pushover.py
+++ sickchill/oldbeard/notifiers/pushover.py
@@ -1,25 +1,26 @@
 # Author: Marvin Pinto <me@marvinp.ca>
 # Author: Dennis Lutter <lad1337@gmail.com>
-import http.client
 import time
-import urllib.error
-import urllib.parse
+
+import requests
 
 from sickchill import logger, settings
 from sickchill.oldbeard.common import (NOTIFY_DOWNLOAD, NOTIFY_GIT_UPDATE, NOTIFY_GIT_UPDATE_TEXT, NOTIFY_LOGIN, NOTIFY_LOGIN_TEXT, NOTIFY_SNATCH,
                                        NOTIFY_SUBTITLE_DOWNLOAD, notifyStrings)
+from sickchill.oldbeard.helpers import make_session
 
 API_URL = "https://api.pushover.net/1/messages.json"
 
 
 class Notifier(object):
     def __init__(self):
-        pass
+        self.api_url = "https://api.pushover.net/1/messages.json"
+        self.session = make_session()
 
     def test_notify(self, userKey=None, apiKey=None):
-        return self._notifyPushover("This is a test notification from SickChill", 'Test', userKey=userKey, apiKey=apiKey, force=True)
+        return self._notify_pushover("This is a test notification from SickChill", 'Test', userKey=userKey, apiKey=apiKey, force=True)
 
-    def _sendPushover(self, msg, title, sound=None, userKey=None, apiKey=None, priority=None):
+    def _send_pushover(self, msg, title, sound=None, userKey=None, apiKey=None, priority=None):
         """
         Sends a pushover notification to the address provided
 
@@ -31,120 +32,94 @@ class Notifier(object):
         returns: True if the message succeeded, False otherwise
         """
 
-        if userKey is None:
-            userKey = settings.PUSHOVER_USERKEY
+        userKey = userKey or settings.PUSHOVER_USERKEY
+        apiKey = apiKey or settings.PUSHOVER_APIKEY
+        sound = sound or settings.PUSHOVER_SOUND
+        priority = priority or settings.PUSHOVER_PRIORITY
 
-        if apiKey is None:
-            apiKey = settings.PUSHOVER_APIKEY
-
-        if sound is None:
-            sound = settings.PUSHOVER_SOUND
-
-        if priority is None:
-            priority = settings.PUSHOVER_PRIORITY
+        if not (userKey and apiKey):
+            logger.warning('You must set a user key and api key to use pushover')
+            return False
 
         logger.debug("Pushover API KEY in use: " + apiKey)
 
-        # build up the URL and parameters
-        msg = msg.strip()
-
-        # send the request to pushover
         try:
-            if settings.PUSHOVER_SOUND != "default":
-                args = {
-                    "token": apiKey,
-                    "user": userKey,
-                    "title": title,
-                    "message": msg,
-                    "timestamp": int(time.time()),
-                    "retry": 60,
-                    "expire": 3600,
-                    "sound": sound,
-                    "priority": priority,
-                }
-            else:
-                # sound is default, so don't send it
-                args = {
-                    "token": apiKey,
-                    "user": userKey,
-                    "title": title,
-                    "message": msg,
-                    "timestamp": int(time.time()),
-                    "retry": 60,
-                    "expire": 3600,
-                    "priority": priority,
-                }
+            data = {
+                "token": apiKey,
+                "user": userKey,
+                "title": title,
+                "message": msg.strip(),
+                "timestamp": int(time.time()),
+                "retry": 60,
+                "expire": 3600,
+                "priority": priority,
+            }
+
+            if sound and sound != "default":
+                data["sound"] = sound
 
             if settings.PUSHOVER_DEVICE:
-                args["device"] = settings.PUSHOVER_DEVICE
+                data["device"] = settings.PUSHOVER_DEVICE
 
-            conn = http.client.HTTPSConnection("api.pushover.net:443")
-            conn.request("POST", "/1/messages.json",
-                         urllib.parse.urlencode(args), {"Content-type": "application/x-www-form-urlencoded"})
+            response = self.session.post(self.api_url, data=data)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as error:
+            if error.response:
+                print(error.response)
 
-        except urllib.error.HTTPError as e:
-            # if we get an error back that doesn't have an error code then who knows what's really happening
-            if not hasattr(e, 'code'):
-                logger.exception("Pushover notification failed." + str(e))
-                return False
-            else:
-                logger.exception("Pushover notification failed. Error code: " + str(e.code))
+                # HTTP status 404 if the provided email address isn't a Pushover user.
+                if error.response.status_code == 404:
+                    logger.warning("Username is wrong/not a pushover email. Pushover will send an email to it")
+                # For HTTP status code 401's, it is because you are passing in either an invalid token, or the user has not added your service.
+                elif error.response.status_code == 401:
+                    # HTTP status 401 if the user doesn't have the service added
+                    subscribe_note = self._send_pushover(msg, title, sound=sound, userKey=userKey, apiKey=apiKey)
+                    if subscribe_note:
+                        logger.debug("Subscription sent")
+                        return True
+                    else:
+                        logger.warning("Subscription could not be sent")
+                # If you receive an HTTP status code of 400, it is because you failed to send the proper parameters
+                elif error.response.status_code == 400:
+                    logger.warning("Wrong data sent to pushover")
 
-            # HTTP status 404 if the provided email address isn't a Pushover user.
-            if e.code == 404:
-                logger.warning("Username is wrong/not a pushover email. Pushover will send an email to it")
-                return False
+                # If you receive a HTTP status code of 429, it is because the message limit has been reached (free limit is 7,500)
+                elif error.response.status_code == 429:
+                    logger.warning("Pushover API message limit reached - try a different API key")
 
-            # For HTTP status code 401's, it is because you are passing in either an invalid token, or the user has not added your service.
-            elif e.code == 401:
-
-                # HTTP status 401 if the user doesn't have the service added
-                subscribeNote = self._sendPushover(msg, title, sound=sound, userKey=userKey, apiKey=apiKey)
-                if subscribeNote:
-                    logger.debug("Subscription sent")
-                    return True
-                else:
-                    logger.exception("Subscription could not be sent")
-                    return False
-
-            # If you receive an HTTP status code of 400, it is because you failed to send the proper parameters
-            elif e.code == 400:
-                logger.exception("Wrong data sent to pushover")
                 return False
 
-            # If you receive a HTTP status code of 429, it is because the message limit has been reached (free limit is 7,500)
-            elif e.code == 429:
-                logger.exception("Pushover API message limit reached - try a different API key")
-                return False
+            logger.warning(f"Pushover notification failed. {error}")
+            return False
 
         logger.info("Pushover notification successful.")
         return True
 
     def notify_snatch(self, ep_name, title=notifyStrings[NOTIFY_SNATCH]):
         if settings.PUSHOVER_NOTIFY_ONSNATCH:
-            self._notifyPushover(title, ep_name)
+            self._notify_pushover(title, ep_name)
 
     def notify_download(self, ep_name, title=notifyStrings[NOTIFY_DOWNLOAD]):
         if settings.PUSHOVER_NOTIFY_ONDOWNLOAD:
-            self._notifyPushover(title, ep_name)
+            self._notify_pushover(title, ep_name)
 
     def notify_subtitle_download(self, ep_name, lang, title=notifyStrings[NOTIFY_SUBTITLE_DOWNLOAD]):
         if settings.PUSHOVER_NOTIFY_ONSUBTITLEDOWNLOAD:
-            self._notifyPushover(title, ep_name + ": " + lang)
+            self._notify_pushover(title, ep_name + ": " + lang)
 
     def notify_git_update(self, new_version="??"):
         if settings.USE_PUSHOVER:
             update_text = notifyStrings[NOTIFY_GIT_UPDATE_TEXT]
             title = notifyStrings[NOTIFY_GIT_UPDATE]
-            self._notifyPushover(title, update_text + new_version)
+            self._notify_pushover(title, update_text + new_version)
 
     def notify_login(self, ipaddress=""):
         if settings.USE_PUSHOVER:
             update_text = notifyStrings[NOTIFY_LOGIN_TEXT]
             title = notifyStrings[NOTIFY_LOGIN]
-            self._notifyPushover(title, update_text.format(ipaddress))
+            self._notify_pushover(title, update_text.format(ipaddress))
 
-    def _notifyPushover(self, title, message, sound=None, userKey=None, apiKey=None, force=False):
+    def _notify_pushover(self, title, message, sound=None, userKey=None, apiKey=None, force=False):
         """
         Sends a pushover notification based on the provided info or SR config
 
@@ -162,4 +137,4 @@ class Notifier(object):
 
         logger.debug("Sending notification for " + message)
 
-        return self._sendPushover(message, title, sound=sound, userKey=userKey, apiKey=apiKey)
+        return self._send_pushover(message, title, sound=sound, userKey=userKey, apiKey=apiKey)
