3077f5195b859bf5bf849f678d9e8ccf96f8233d
miigotu <miigotu@gmail.com>
2021-02-14 23:42:27 -0500

    Fix getting api key using getkey endpoint Fixes #7006

--- sickchill/views/api/authentication.py
+++ sickchill/views/api/authentication.py
@@ -13,7 +13,7 @@ class KeyHandler(RequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def get(self):
+    def get(self, *args, **kwargs):
         if self.get_query_argument('u', None) == settings.WEB_USERNAME and self.get_query_argument('p', None) == settings.WEB_PASSWORD:
             if not len(settings.API_KEY or ''):
                 settings.API_KEY = helpers.generateApiKey()
