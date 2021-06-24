5b62ef3e19620a04e201b35a19ab9dc745256ea9
miigotu <miigotu@gmail.com>
2021-06-24 01:00:17 -0400

    Update helpers.py

--- sickchill/oldbeard/helpers.py
+++ sickchill/oldbeard/helpers.py
@@ -691,14 +691,12 @@ def sanitizeSceneName(name, anime=False):
     :return: A string containing the scene version of the show name given.
     """
 
-    # assert isinstance(name, unicode), name + ' is not unicode'
-
     if not name:
         return ""
 
     bad_chars = ",:()!?\u2019"
     if not anime:
-        bad_chars += "'’"
+        bad_chars += "'’`"
 
     # strip out any bad chars
     for x in bad_chars:
