376e73403105879d691657d9445bc49067757e7a
miigotu <miigotu@gmail.com>
2021-09-09 12:39:21 -0400

    Fix grunt and ava
    Signed-off-by: miigotu <miigotu@gmail.com>

--- tests/js/helpers/setup-browser-env.js
+++ tests/js/helpers/setup-browser-env.js
@@ -1,8 +1,1 @@
-import browserEnv from 'browser-env';
-import test from 'ava';
-
-browserEnv();
-
-test('1', t => {
-    t.is(1, 1);
-});
+require('browser-env')(); // eslint-disable-line unicorn/prefer-module
