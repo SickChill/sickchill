2b6fa8b7a7606607bff7f1a9a5f9a8899462256e
miigotu <miigotu@gmail.com>
2022-07-20 12:21:31 -0400

    Make sure VIRTUAL_ENV is set when we restart into a virtualenv
    Signed-off-by: miigotu <miigotu@gmail.com>

--- sickchill/init_helpers.py
+++ sickchill/init_helpers.py
@@ -405,6 +405,9 @@ def make_virtualenv_and_rerun(location: Path) -> None:
                     # add original arguments to this re-call
                     new_argv = [str(place)] + sys.argv
 
+                    if "VIRTUAL_ENV" not in os.environ:
+                        os.environ["VIRTUAL_ENV"] = str(location)
+
                     logger.info(f"Restarting SickChill with {new_argv}")
                     return os.execvp(new_argv[0], new_argv)
 
