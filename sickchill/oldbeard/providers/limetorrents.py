7da4b32bcead16e0d06e4d9309ce5e608781135a
miigotu <miigotu@gmail.com>
2022-08-21 02:19:36 -0400

    Update limetorrents.py

--- sickchill/oldbeard/providers/limetorrents.py
+++ sickchill/oldbeard/providers/limetorrents.py
@@ -1,11 +1,10 @@
 import re
 import traceback
 
-from bs4 import BeautifulSoup
-
 from sickchill import logger, settings
 from sickchill.helper.common import convert_size, try_int
 from sickchill.oldbeard import tvcache
+from sickchill.oldbeard.bs4_parser import BS4Parser
 from sickchill.providers.torrent.TorrentProvider import TorrentProvider
 
 
@@ -52,61 +51,56 @@ class Provider(TorrentProvider):
                         logger.info("Expected xml but got something else, is your mirror failing?")
                         continue
 
-                    data = BeautifulSoup(data, "xml")
+                    with BS4Parser(data, language="xml") as parser:
+                        for item in parser("item"):
+                            try:
+                                title = item.title.text
+                                download_url = item.enclosure["url"]
+                                torrent_hash = re.match(r"(.*)([A-F0-9]{40})(.*)", download_url, re.I).group(2)
 
-                    entries = data("item")
-                    if not entries:
-                        logger.info("Returned xml contained no results")
-                        continue
+                                if settings.TORRENT_METHOD != "blackhole" and "magnet:?" not in download_url:
+                                    download_url = "magnet:?xt=urn:btih:" + torrent_hash + "&dn=" + title + self._custom_trackers
 
-                    for item in entries:
-                        try:
-                            title = item.title.text
-                            download_url = item.enclosure["url"]
-                            torrent_hash = re.match(r"(.*)([A-F0-9]{40})(.*)", download_url, re.I).group(2)
+                                if not (title and download_url):
+                                    continue
 
-                            if settings.TORRENT_METHOD != "blackhole" and "magnet:?" not in download_url:
-                                download_url = "magnet:?xt=urn:btih:" + torrent_hash + "&dn=" + title + self._custom_trackers
+                                # seeders and leechers are presented diferently when doing a search and when looking for newly added
+                                if mode == "RSS":
+                                    # <![CDATA[
+                                    # Category: <a href="http://www.limetorrents.info/browse-torrents/TV-shows/">TV shows</a><br /> Seeds: 1<br />Leechers: 0<br />Size: 7.71 GB<br /><br /><a href="http://www.limetorrents.info/Owen-Hart-of-Gold-Djon91-torrent-7180661.html">More @ limetorrents.info</a><br />
+                                    # ]]>
+                                    description = item.find("description")
+                                    seeders = try_int(description("br")[0].next_sibling.strip().lstrip("Seeds: "))
+                                    leechers = try_int(description("br")[1].next_sibling.strip().lstrip("Leechers: "))
+                                else:
+                                    # <description>Seeds: 6982 , Leechers 734</description>
+                                    description = item.find("description").text.partition(",")
+                                    seeders = try_int(description[0].lstrip("Seeds: ").strip())
+                                    leechers = try_int(description[2].lstrip("Leechers ").strip())
 
-                            if not (title and download_url):
+                                torrent_size = item.find("size").text
+
+                                size = convert_size(torrent_size) or -1
+
+                            except (AttributeError, TypeError, KeyError, ValueError):
                                 continue
 
-                            # seeders and leechers are presented diferently when doing a search and when looking for newly added
-                            if mode == "RSS":
-                                # <![CDATA[
-                                # Category: <a href="http://www.limetorrents.info/browse-torrents/TV-shows/">TV shows</a><br /> Seeds: 1<br />Leechers: 0<br />Size: 7.71 GB<br /><br /><a href="http://www.limetorrents.info/Owen-Hart-of-Gold-Djon91-torrent-7180661.html">More @ limetorrents.info</a><br />
-                                # ]]>
-                                description = item.find("description")
-                                seeders = try_int(description("br")[0].next_sibling.strip().lstrip("Seeds: "))
-                                leechers = try_int(description("br")[1].next_sibling.strip().lstrip("Leechers: "))
-                            else:
-                                # <description>Seeds: 6982 , Leechers 734</description>
-                                description = item.find("description").text.partition(",")
-                                seeders = try_int(description[0].lstrip("Seeds: ").strip())
-                                leechers = try_int(description[2].lstrip("Leechers ").strip())
-
-                            torrent_size = item.find("size").text
-
-                            size = convert_size(torrent_size) or -1
-
-                        except (AttributeError, TypeError, KeyError, ValueError):
-                            continue
-
                             # Filter unseeded torrent
-                        if seeders < self.minseed or leechers < self.minleech:
-                            if mode != "RSS":
-                                logger.debug(
-                                    "Discarding torrent because it doesn't meet the minimum seeders or leechers: {0} (S:{1} L:{2})".format(
-                                        title, seeders, leechers
+                            if seeders < self.minseed or leechers < self.minleech:
+                                if mode != "RSS":
+                                    logger.debug(
+                                        f"Discarding torrent because it doesn't meet the minimum seeders or leechers: {title} (S:{seeders} L:{leechers})"
                                     )
-                                )
+                                continue
+
+                            item = {"title": title, "link": download_url, "size": size, "seeders": seeders, "leechers": leechers, "hash": torrent_hash}
+                            if mode != "RSS":
+                                logger.debug("Found result: {0} with {1} seeders and {2} leechers".format(title, seeders, leechers))
+
+                            items.append(item)
+                        else:
+                            logger.info("Returned xml contained no results")
                             continue
-
-                        item = {"title": title, "link": download_url, "size": size, "seeders": seeders, "leechers": leechers, "hash": torrent_hash}
-                        if mode != "RSS":
-                            logger.debug("Found result: {0} with {1} seeders and {2} leechers".format(title, seeders, leechers))
-
-                        items.append(item)
 
                 except (AttributeError, TypeError, KeyError, ValueError):
                     logger.exception("Failed parsing provider. Traceback: {0!r}".format(traceback.format_exc()))
