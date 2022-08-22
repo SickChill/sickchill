676f6d2c9eb20440c1bf16e62508548481a07582
pigsyn <soulard.morgan@gmail.com>
2022-08-22 17:08:52 +0200

    Update cpasbien.py
    Fix torrent table class and table cells

--- sickchill/oldbeard/providers/cpasbien.py
+++ sickchill/oldbeard/providers/cpasbien.py
@@ -105,7 +105,7 @@ class Provider(TorrentProvider):
                     continue
 
                 with BS4Parser(data, "html5lib") as html:
-                    torrent_table = html.find("table", {"class": "table-hover"})
+                    torrent_table = html.find("table", {"class": "table-corps"})
                     if torrent_table:
                         torrent_rows = torrent_table.findAll("tr")
                     else:
@@ -122,15 +122,15 @@ class Provider(TorrentProvider):
                             if not all([title, download_url]):
                                 continue
 
-                            seeders = try_int(result.find_all("td")[2].get_text(strip=True))
-                            leechers = try_int(result.find_all("td")[3].get_text(strip=True))
+                            seeders = try_int(result.find(class_="seed_ok").get_text(strip=True))
+                            leechers = try_int(result.find(class_="down").get_text(strip=True))
                             if seeders < self.minseed or leechers < self.minleech:
                                 if mode != "RSS":
                                     logger.debug(f"Discarding torrent because it doesn't meet the minimum seeders or "
                                                  f"leechers: {title} (S:{seeders} L:{leechers})")
                                 continue
 
-                            torrent_size = result.find_all("td")[1].get_text(strip=True)
+                            torrent_size = result.find(class_="poid").get_text(strip=True)
 
                             units = ["o", "Ko", "Mo", "Go", "To", "Po"]
                             size = convert_size(torrent_size, units=units) or -1
