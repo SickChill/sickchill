38171aa54796296e73d7df9a0352557f0811af25
miigotu <miigotu@gmail.com>
2022-09-20 23:10:22 -0400

    Add skeleton for testing web api calls, needs flesh
    Signed-off-by: miigotu <miigotu@gmail.com>

---
+++ tests/sickchill_tests/views/api/test_webapi.py
@@ -0,0 +1,21 @@
+import logging
+
+from pytest import fixture
+
+from sickchill.views.api import webapi
+
+
+@fixture(params=set(webapi.function_mapper.values()))
+def api_command(request, caplog):
+    with caplog.at_level(logging.WARNING, logger="root"):
+        caplog.set_level(logging.DEBUG, logger="sickchill")
+        caplog.set_level(logging.WARNING, logger="cachecontrol")
+        caplog.set_level(logging.WARNING, logger="urllib3")
+        yield request.param
+
+
+def test_api_calls(api_command, capsys):
+    with capsys.disabled():
+        print(api_command._help)
+
+    # TODO: test the api calls, this method will be called once for each api command automatically because of the fixture
