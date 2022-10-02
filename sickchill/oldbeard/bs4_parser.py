09db254ac60ca60051371b7ed8561023f45cb513
miigotu <miigotu@gmail.com>
2022-10-02 02:46:48 -0400

    Pull ParserBeautifulSoup from subliminal into our code, pin cryptography to < 35.0.0
    Signed-off-by: miigotu <miigotu@gmail.com>

--- sickchill/oldbeard/bs4_parser.py
+++ sickchill/oldbeard/bs4_parser.py
@@ -1,7 +1,37 @@
 import warnings
 
-from bs4 import FeatureNotFound
-from subliminal.providers import ParserBeautifulSoup
+from bs4 import BeautifulSoup, FeatureNotFound
+
+
+class ParserBeautifulSoup(BeautifulSoup):
+    """A ``bs4.BeautifulSoup`` that picks the first parser available in `parsers`.
+
+    :param markup: markup for the ``bs4.BeautifulSoup``.
+    :param list parsers: parser names, in order of preference.
+
+    Snagged from subliminal, because of course it was.
+    """
+
+    def __init__(self, markup, parsers, **kwargs):
+        # reject features
+        if set(parsers).intersection({"fast", "permissive", "strict", "xml", "html", "html5"}):
+            raise ValueError("Features not allowed, only parser names")
+
+        # reject some kwargs
+        if "features" in kwargs:
+            raise ValueError("Cannot use features kwarg")
+        if "builder" in kwargs:
+            raise ValueError("Cannot use builder kwarg")
+
+        # pick the first parser available
+        for parser in parsers:
+            try:
+                super(ParserBeautifulSoup, self).__init__(markup, parser, **kwargs)
+                return
+            except FeatureNotFound:
+                pass
+
+        raise FeatureNotFound
 
 
 class BS4Parser(object):
