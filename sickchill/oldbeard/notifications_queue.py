4ac07e55dacd21385421bcdda594062df25bb6e4
BKSteve <stephen.rieger@gmail.com>
2023-03-26 20:37:18 +0700

    Add traceback.format_exc()

--- sickchill/oldbeard/notifications_queue.py
+++ sickchill/oldbeard/notifications_queue.py
@@ -145,7 +145,7 @@ class DiscordTask(generic_queue.QueueItem):
             return False
         except requests.exceptions.RequestException as error:
             if error.response.status_code != 429 or int(error.response.headers.get("X-RateLimit-Remaining")) != 0:
-                raise error
+                raise traceback.format_exc()
 
             logger.info("Discord rate limiting, retrying after {} seconds".format(error.response.headers.get("X-RateLimit-Reset-After")))
             time.sleep(int(error.response.headers.get("X-RateLimit-Reset-After")) + 1)
@@ -154,7 +154,7 @@ class DiscordTask(generic_queue.QueueItem):
             )
             r.raise_for_status()
         except Exception as error:
-            logger.exception(f"Error Sending Discord message: {error}")
+            logger.exception(f"Error Sending Discord message: {traceback.format_exc()}")
 
             return False
 
