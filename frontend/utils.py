734be5ff8f58b59aae2d95eb5e564be70bb63b3a
miigotu <miigotu@gmail.com>
2023-11-03 19:11:44 -0400

    Update frontend/utils.py
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/utils.py
+++ frontend/utils.py
@@ -17,8 +17,22 @@ def build_blueprint(module_location: Path, module_import: str) -> Blueprint:
     templates_path = module_path.joinpath("templates")
     static_path = frontend_path.joinpath("static").joinpath(module_name)
 
+def build_blueprint(module_location: Path, module_import: str) -> Blueprint:
+    """Generate a blueprint for a frontend module"""
+
+    parent_path = Path(module_location).parent
+    module_name = parent_path.name
+    module_path = parent_path.resolve()
+    frontend_path = module_path.parent.resolve()
+
+    templates_path = module_path.joinpath("templates")
+    static_path = frontend_path.joinpath("static", module_name)
+
     blueprint = Blueprint(module_name, module_import, static_folder=static_path, template_folder=templates_path)
-    logger.debug(
-        f"Paths for {blueprint.name}\n\ttemplates: {blueprint.template_folder}\n\tstatic: {blueprint.static_folder}\n\turl_prefix: {blueprint.url_prefix}"
-    )
+    logger.debug(f"Blueprint name: {blueprint.name}")
+    logger.debug(f"Templates path: {blueprint.template_folder}")
+    logger.debug(f"Static path: {blueprint.static_folder}")
+    logger.debug(f"URL prefix: {blueprint.url_prefix}")
+
+    return blueprint
     return blueprint
