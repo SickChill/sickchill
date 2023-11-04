468b3a99561c794f1aef8709d53064f35f1c37f6
miigotu <miigotu@gmail.com>
2023-11-03 21:15:29 -0400

    chore: update docstring in frontent.config
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/config/__init__.py
+++ frontend/config/__init__.py
@@ -1,5 +1,5 @@
 """
-This module implements the web interface for the sickchill configurationt
+This module implements the web interface for the SickChill configuration. It provides a blueprint for handling configuration-related requests.
 """
 
 from .views import blueprint
