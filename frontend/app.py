0d77a18c0a47246717612a9d8925f42c78687611
miigotu <miigotu@gmail.com>
2023-11-05 00:16:52 +0000

    chore: fix python formatting
    Signed-off-by: miigotu <miigotu@gmail.com>

--- frontend/app.py
+++ frontend/app.py
@@ -70,14 +70,14 @@ class DevelopmentConfig(BaseConfig):
 
 if __name__ == "__main__":
     dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
-            }
-        },
-        "handlers": {"wsgi": {"class": "logging.StreamHandler", "stream": "ext://flask.logging.wsgi_errors_stream", "formatter": "default"}},
-        "root": {"level": "DEBUG", "handlers": ["wsgi"]},
-    }
+        {
+            "version": 1,
+            "formatters": {
+                "default": {
+                    "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+                }
+            },
+            "handlers": {"wsgi": {"class": "logging.StreamHandler", "stream": "ext://flask.logging.wsgi_errors_stream", "formatter": "default"}},
+            "root": {"level": "DEBUG", "handlers": ["wsgi"]},
+        }
     )
