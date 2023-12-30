621070b1c6b4ecdb4ef77ceab2e8dca421d9f398
BKSteve <stephen.rieger@gmail.com>
2023-12-30 13:08:25 +0700

    ratio as float

--- tests/js/index.js
+++ tests/js/index.js
@@ -1,11 +1,10 @@
 const test = require('ava');
 
-test.failing('getMeta', t => {
-    const meta = document.createElement('meta', {
-        'data-var': 'scRoot',
-        content: '/sickchill',
+test.failing('getMeta',
+    t => {
+        const meta = document.createElement('meta');
+        meta.datavar = 'scRoot';
+        meta.content = '/sickchill';
+        document.body.appendChild(meta);
+        t.is(getMeta('scRoot'), '/sickchill');
     });
-    document.body.append(meta);
-
-    t.is(getMeta('scRoot'), '/sickchill');
-});
