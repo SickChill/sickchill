0e895efefff71fb3efd62a230e97e7f88b83ba0f
BKSteve <stephen.rieger@gmail.com>
2023-12-31 15:46:00 +0700

    index js

--- tests/js/index.js
+++ tests/js/index.js
@@ -1,10 +1,10 @@
 const test = require('ava');
 
-test.failing('getMeta',
-    t => {
-        const meta = document.createElement('meta');
-        meta.datavar = 'scRoot';
-        meta.content = '/sickchill';
-        document.body.appendChild(meta);
-        t.is(getMeta('scRoot'), '/sickchill');
-    });
+test('getMeta', t => {
+    const meta = document.createElement('meta');
+    meta.setAttribute('data-var', 'scRoot');
+    meta.setAttribute('content', '/sickchill');
+    document.head.appendChild(meta);
+
+    t.is(getMeta('scRoot'), '/sickchill');
+});
