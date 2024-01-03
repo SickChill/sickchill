fef732d6dd9ca8c6745c464582be14fd9041d856
BKSteve <stephen.rieger@gmail.com>
2024-01-03 17:41:45 +0700

    no idea

--- tests/js/index.js
+++ tests/js/index.js
@@ -1,11 +1,11 @@
+const { getMeta } = require("/sickchill/gui/slick/js/core");
+
 const test = require("ava");
 
-test.failing('getMeta', t => {
-    const meta = document.createElement('meta', {
-        'data-var': 'scRoot',
-        content: '/sickchill',
-    });
-    document.body.append(meta);
+test.failing("getMeta", async t => {
+    const meta = document.createElement("meta");
+    meta.setAttribute("data-var", "scRoot");
+    meta.setAttribute("content", "/sickchill");
 
-    t.is(getMeta('scRoot'), '/sickchill');
+    t.is(getMeta("scRoot"), "/sickchill");
 });
