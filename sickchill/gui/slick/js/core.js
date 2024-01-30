668b972fb88154756e7ad26e9b390f7c9d5dbede
BKSteve <stephen.rieger@gmail.com>
2024-01-30 23:08:24 +0700

    TTS typo and 1:0

--- sickchill/gui/slick/js/core.js
+++ sickchill/gui/slick/js/core.js
@@ -711,12 +711,14 @@ const SICKCHILL = {
                     notifyModal('Please fill in the webhook address');
                     return;
                 }
+
                 const discord = {
                     webhook: discordWebhook.val(),
                     name: $('#discord_name').val(),
                     avatar: $('#discord_avatar_url').val(),
                     tts: $('#discord_tts').is(':checked') ? 1 : 0
                 }
+
                 $('#testDiscord').prop('disabled', true);
                 $('#testDiscord-result').html(loading);
                 $.post(scRoot + '/home/testDiscord', discord).done(data => {
