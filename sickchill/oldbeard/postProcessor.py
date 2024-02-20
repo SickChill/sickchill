02c498073331370e5d882db9a25defbdc7dda26e
miigotu <miigotu@gmail.com>
2024-02-20 15:16:57 -0500

    fix: episode.refreshSubtitles was renamed to refresh_subtitles
    Signed-off-by: miigotu <miigotu@gmail.com>

--- sickchill/oldbeard/postProcessor.py
+++ sickchill/oldbeard/postProcessor.py
@@ -1169,7 +1169,7 @@ class PostProcessor(object):
                 cur_ep.location = os.path.join(dest_path, new_filename)
                 # download subtitles
                 if settings.USE_SUBTITLES and episode_object.show.subtitles and (cur_ep.season != 0 or settings.SUBTITLES_INCLUDE_SPECIALS):
-                    cur_ep.refreshSubtitles()
+                    cur_ep.refresh_subtitles()
                     cur_ep.download_subtitles()
                 sql_l.append(cur_ep.get_sql())
 
