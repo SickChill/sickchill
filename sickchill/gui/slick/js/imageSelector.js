e317d7773ab92fc8029315683729ae850e22fb60
BKSteve <stephen.rieger@gmail.com>
2026-05-16 23:56:29 +0700

    Poster & Banner ratio tolerance and little ruff adjustments (#8940)
    * Banner ratio fix and little specific ruff adjustments
    
    * Banner ratio fix clean js
    
    * Poster ratio tolerance
    
    * init imp

--- sickchill/gui/slick/js/imageSelector.js
+++ sickchill/gui/slick/js/imageSelector.js
@@ -2,21 +2,33 @@
     const imageTypeSizes = {
         poster: {
             minHeight: 269,
-            ratio: 40 / 57,
+            targetRatio: 40 / 57,
+            tolerance: 0.02,
             validate(img) {
-                return img.height >= this.minHeight
-                    && img.width / img.height === this.ratio;
+                if (img.height < this.minHeight) {
+                    return false;
+                }
+
+                const actualRatio = img.width / img.height;
+                const diff = Math.abs(actualRatio - this.targetRatio);
+                return diff <= this.tolerance;
             },
-            errorMsg: 'The height of the poster can not be lower than 269 pixels and the aspect ratio should be 40:57.',
+            errorMsg: 'Poster must have aspect ratio ≈ 40:57 (≈0.7018) and height ≥ 269px.',
         },
         banner: {
             minHeight: 37,
-            ratio: 200 / 37,
+            targetRatio: 200 / 37,
+            tolerance: 0.03,
             validate(img) {
-                return img.height >= this.minHeight
-                    && img.width / img.height === this.ratio;
+                if (img.height < this.minHeight) {
+                    return false;
+                }
+
+                const actualRatio = img.width / img.height;
+                const diff = Math.abs(actualRatio - this.targetRatio);
+                return diff <= this.tolerance;
             },
-            errorMsg: 'The height of the banner can not be lower than 37 pixels and the aspect ratio should be 200:37.',
+            errorMsg: 'Banner must have aspect ratio ≈ 200:37 (5.405:1) and height ≥ 37px.',
         },
         fanart: {
             minHeight: 200,
